4c14f5a6f282e3158ed10356472f232da1c77ede
akaszynski <akascap@gmail.com>
2017-11-27 10:05:57 +0100

    removed numpy from install requirements

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ import sys
 from io import open as io_open
 
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.build_ext import build_ext
+
+# from setuptools.command.build_ext import build_ext as _build_ext
+# # Create a build class that includes numpy directory
+# class build_ext(_build_ext):
+#     def finalize_options(self):
+#         _build_ext.finalize_options(self)
+#         # Prevent numpy from thinking it is still in its setup process:
+#         __builtins__.__NUMPY_SETUP__ = False
+#         import numpy
+#         self.include_dirs.append(numpy.get_include())
 
 
-# Create a build class that includes numpy directory
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
-
-# Check compiler and assign compile arguments accordingly
 def compilerName():
+    """ Check compiler and assign compile arguments accordingly """
     import re
     import distutils.ccompiler
     comp = distutils.ccompiler.get_default_compiler()
@@ -131,7 +131,6 @@ setup(
                  ],
 
     keywords='vtk ANSYS cdb full rst',
-
     package_data={'pyansys.examples': ['TetBeam.cdb', 'HexBeam.cdb',
                                        'file.rst', 'file.full']},
 
