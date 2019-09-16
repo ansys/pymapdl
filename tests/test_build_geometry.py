2f0fa8a8ee37f63e64811fad6f64f43752ca0838
natter1 <52835756+natter1@users.noreply.github.com>
2019-09-16 16:07:48 +0200

    #138 pull out parse_al() from mapdl_functions.py (#139)
    * #131 useful return values for Mapdl.k/l/a/al
    
    * update fork to be similar to master
    
    * #138 fix mapdl.al()
    also fix return value for mapdl.k(1,0,0) (command is not available in parse_k)

--- tests/test_build_geometry.py
+++ tests/test_build_geometry.py
@@ -32,11 +32,24 @@ def test_a():
     k1 = mapdl.k("", 1, 0, 0)
     k2 = mapdl.k("", 1, 1, 0)
     k3 = mapdl.k("", 0, 1, 0)
+    a0 = mapdl.a(k0, k1, k2, k3)
+    assert a0 is 1
+
+
+@pytest.mark.skipif(not pyansys.has_ansys, reason="Requires ANSYS installed")
+def test_al():
+    mapdl.finish()
+    mapdl.clear()
+    mapdl.prep7()
+    k0 = mapdl.k("", 0, 0, 0)
+    k1 = mapdl.k("", 1, 0, 0)
+    k2 = mapdl.k("", 1, 1, 0)
+    k3 = mapdl.k("", 0, 1, 0)
     l0 = mapdl.l(k0, k1)
     l1 = mapdl.l(k1, k2)
     l2 = mapdl.l(k2, k3)
     l3 = mapdl.l(k3, k0)
-    a0 = mapdl.a(l0, l1, l2, l3)
+    a0 = mapdl.al(l0, l1, l2, l3)
     assert a0 is 1
 
 
