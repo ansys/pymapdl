aa677bd3ae38fe9eac8063562ae4409357b42128
natter1 <njoehrmann@gmail.com>
2019-10-09 16:19:42 +0200

    add tests for Mapdl.et()

---
+++ tests/test_build_element.py
@@ -0,0 +1,25 @@
+import pytest
+import pyansys
+
+if pyansys.has_ansys:
+    mapdl = pyansys.Mapdl(override=True)
+    mapdl.prep7()
+
+
+@pytest.mark.skipif(not pyansys.has_ansys, reason="Requires ANSYS installed")
+def test_et():
+    mapdl.finish()
+    mapdl.clear()
+    mapdl.prep7()
+    n_plane183 = mapdl.et("", "PLANE183")
+    assert n_plane183 == 1
+    n_compare = int(mapdl.get_float("ETYP", 0, "NUM", "MAX"))
+    assert n_plane183 == n_compare
+    n_plane183 = mapdl.et(17, "PLANE183")
+    assert n_plane183 == 17
+
+
+@pytest.mark.skipif(not pyansys.has_ansys, reason="Requires ANSYS installed")
+def test_invalid():
+    with pytest.raises(Exception):
+        mapdl.a(0, 0, 0, 0)
