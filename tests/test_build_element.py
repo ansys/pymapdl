80ccb544dbbe08a66c92a7f775e5335b1e9a021f
natter1 <52835756+natter1@users.noreply.github.com>
2019-10-14 19:38:31 +0200

    added return values and tests for v(), n() and e() (#155)
    * #131 useful return values for Mapdl.k/l/a/al
    
    * update fork to be similar to master
    
    * #138 fix mapdl.al()
    also fix return value for mapdl.k(1,0,0) (command is not available in parse_k)
    
    * add return value to Mapdl.bsplin
    
    * #146
    add Mapdl.read_float_parameter
    add Mapdl.read_float_from_inline_function
    
    * #150 add return value for Mapdl.et()
    
    * add tests for Mapdl.et()
    
    * Revert "add tests for Mapdl.et()"
    
    This reverts commit aa677bd3
    
    * add tests for Mapdl.et()
    
    * removed test_invalid()
    
    * add return values for:
    Mapdl.e(), Mapdl.v(), Mapdl.n()
    add tests
    
    * Delete element_commands.py
    
    * Delete test_build_element.py
    
    * updated conflicting element_commands.py and test_build_element.py

--- tests/test_build_element.py
+++ tests/test_build_element.py
@@ -3,14 +3,31 @@ import pyansys
 
 if pyansys.has_ansys:
     mapdl = pyansys.Mapdl(override=True)
+
+
+@pytest.fixture(scope='function')
+def cleared():
+    mapdl.clear()
     mapdl.prep7()
+    yield
 
 
 @pytest.mark.skipif(not pyansys.has_ansys, reason="Requires ANSYS installed")
-def test_et():
-    mapdl.finish()
-    mapdl.clear()
-    mapdl.prep7()
+def test_e(cleared):
+    mapdl.et("", 183)
+    n0 = mapdl.n("", 0, 0, 0)
+    n1 = mapdl.n("", 1, 0, 0)
+    n2 = mapdl.n("", 1, 1, 0)
+    n3 = mapdl.n("", 0, 1, 1)
+    n4 = mapdl.n("", 0, 1, -1)
+    e0 = mapdl.e(n0, n1, n2, n3)
+    assert e0 == 1
+    e1 = mapdl.e(n0, n1, n2, n4)
+    assert e1 == 2
+
+
+@pytest.mark.skipif(not pyansys.has_ansys, reason="Requires ANSYS installed")
+def test_et(cleared):
     n_plane183 = mapdl.et("", "PLANE183")
     assert n_plane183 == 1
     n_compare = int(mapdl.get_float("ETYP", 0, "NUM", "MAX"))
