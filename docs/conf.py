2ca344af8054d1f20e8c01c345491d8d1dccfe18
Alex Kaszynski <akascap@gmail.com>
2019-10-14 11:39:11 -0600

    cleaned up docs

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,6 @@ sphinx_gallery_conf = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme='classic'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
