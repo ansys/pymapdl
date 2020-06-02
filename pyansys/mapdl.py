97c49feebf4b0bc82ed475f181d0f8ab7d399e8c
Alex Kaszynski <akascap@gmail.com>
2020-06-03 00:37:06 +0200

    fixed corba issue with batch mode

--- pyansys/mapdl.py
+++ pyansys/mapdl.py
@@ -676,10 +676,6 @@ class Mapdl(_MapdlCommands, _DeprecCommands):
         """
         if (int(self.version) < 170 and os.name == 'posix') or self.prefer_pexpect:
             self._open_process(self.nproc, self.start_timeout, additional_switches)
-        elif int(self.version) >= 200 and os.name == 'posix':
-            raise RuntimeError('`ansys_corba` is not supported on Linux for '
-                               'MAPDL v20.0 and greater.  Please use:\n'
-                               '    prefer_pexpect=True')
         else:  # use corba
             self.open_corba(self.nproc, self.start_timeout, additional_switches)
 
@@ -1299,7 +1295,9 @@ class Mapdl(_MapdlCommands, _DeprecCommands):
         self.log.info('Connecting to ANSYS via CORBA')
 
         # command must include "aas" flag to start MAPDL server
-        command = '"%s" -j %s -aas -i tmp.inp -o out.txt -b -np %d %s' % (self.exec_file, self._jobname, nproc, additional_switches)
+        # command = '"%s" -j %s -aas -i tmp.inp -o out.txt -b -np %d %s' % (self.exec_file, self._jobname, nproc, additional_switches)
+        command = '"%s" -j %s -aas -np %d %s' % (self.exec_file, self._jobname,
+                                                 nproc, additional_switches)
 
         # remove the broadcast file if it exists:
         if os.path.isfile(self.broadcast_file):
@@ -1350,17 +1348,18 @@ class Mapdl(_MapdlCommands, _DeprecCommands):
         except:
             pip_cmd = 'pip install ansys_corba'
             raise ImportError('Missing ansys_corba.\n' +
-                              'This feature does not support MAC OS.\n' +\
+                              'This feature does not support MAC OS.\n' + \
                               'Otherwise, please install with "%s"' % pip_cmd)
 
         orb = CORBA.ORB_init()
         self.mapdl = orb.string_to_object(key)
 
-        # quick test
+        # must be the very first command
         try:
+            text = self.mapdl.executeCommandToString('/batch')
             self.mapdl.getComponentName()
         except:
-            raise Exception('Unable to connect to APDL server')
+            raise RuntimeError('Unable to connect to APDL server')
 
         self.using_corba = True
         self.log.debug('Connected to ANSYS using CORBA interface')
