96bd80c6ce6e8c0252be473160298cdc7ba40410
Alex Kaszynski <akascap@gmail.com>
2020-06-12 22:15:47 +0200

    using strtod

--- pyansys/cython/reader.c
+++ pyansys/cython/reader.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* #include <errno.h> */
+
 
 //=============================================================================
 // Fast string to interger convert to ANSYS formatted intergers 
@@ -26,72 +26,24 @@ __inline int fast_atoi(char * raw, int intsz, int *i){
     return val;
 }
 
-__inline int fast_atoi2(char * raw, int intsz, int *i){
-    int val;
-    int c;
+__inline int fast_atoi2(char * raw, int intsz){
 
-    val = 0;
-    for (c=0; c<intsz; ++c){
-        // Seek throug white space
-        if (raw[*(i)] == ' '){
-            ++*(i);
-            continue;
-        }
+  int val;
+  int c;
 
-        val = val*10 + (raw[*(i)] - '0');
-        ++*(i);
+  val = 0;
+  for (c=0; c<intsz; ++c){
+    // Seek through white space
+    if (raw[0] == ' '){
+      ++raw;
+      continue;
     }
 
-    // Pass counter position back to file position counter
-//    *(i) = c;
+    val = val*10 + (raw[0] - '0');
+    ++raw;
+  }
 
-    return val;
-}
-
-
-__inline int verbose_fast_atoi(char * raw, int intsz, int *i){
-    int val;
-    int c;
-
-    val = 0;
-    for (c=0; c<intsz; ++c){
-        ++*(i);
-        printf("%c", raw[*(i)]);
-
-        // Seek throug white space
-        if (raw[*(i)] == ' ') continue;
-
-        val = val*10 + (raw[*(i)] - '0');
-    }
-
-    // Pass counter position back to file position counter
-//    *(i) = c;
-
-    return val;
-}
-
-
-__inline int verbose_fast_atoi2(char * raw, int intsz, int *i){
-    int val;
-    int c;
-
-    val = 0;
-    for (c=0; c<intsz; ++c){
-        printf("%c", raw[*(i)]);
-        // Seek throug white space
-        if (raw[*(i)] == ' '){
-            ++*(i);
-            continue;
-        }
-
-        val = val*10 + (raw[*(i)] - '0');
-        ++*(i);
-    }
-
-    // Pass counter position back to file position counter
-//    *(i) = c;
-
-    return val;
+  return val;
 }
 
 
@@ -112,243 +64,45 @@ __inline int checkneg(char * raw, int intsz, int *i){
     return found;
 }
 
-__inline int verbose_checkneg(char * raw, int intsz, int *i){
-    int c;
-    int found = 0;
-    for (c=0; c<intsz; ++c){
-        printf("%c", raw[*(i)]);
-        // Seek throug white space
-        if (raw[*(i)] == '-'){
-            found = 1;
-        }
-        ++*(i);
-    }
-
-    return found;
-}
-
-
-
-
-void main(){
-    //
-}
-
-
 //=============================================================================
-// Fast string to flot converter for ANSYS formatted floats
-//=============================================================================
-__inline double fast_atof(char *raw, int fltsz, int*i){
-
-    int ivalue, j, nread;
-    double dvalue, sign, scale, pow;
-    char tempstr[100];
-
-    int c = *(i);
-    *(i) += fltsz;
-
-    //init value
-    dvalue = 1.0;
-
-    // check sign
-    if (raw[c] == '-') {
-        sign = -1.0;
-    }
-    else{
-        sign = 1.0;
-    }
-
-    // read first interger
-    ++c;            
-    dvalue *= raw[c] - '0';
-
-    // next is always a '.', skip it
-    ++c;
-
-    nread = fltsz - 7; // sign, first int, point, xxx, scinot (4)
-    pow = 0.1;
-    for (j=0; j<nread; ++j){
-        ++c;
-        dvalue += (raw[c] - '0')*pow;
-        pow *= 0.1;
-
-    }
-
-    // apply sign
-    dvalue *= sign;
-
-    // Read exponent
-    // Skip the E
-    ++c; 
-
-    // store sign of exponent
-    ++c;
-    tempstr[0] = raw[c];
-    
-    // read exponent
-    ++c;
-    ivalue = 10*(raw[c] - '0');
-    ++c;
-    ivalue += raw[c] - '0';
-
-    ++c;
-    if (ivalue == 0) {
-        // Store value
-        return dvalue;
-    }
-
-    scale = 1.0;
-    while (ivalue > 0) { scale *= 10.0; ivalue -=  1; }
-
-    if (tempstr[0] == '+'){
-        return dvalue *= scale;
-    }
-    else{
-        return dvalue /= scale;
-    }
-   
-}
-
-
-//=============================================================================
-// Reads NBLOCK from ANSYS.  Raw string is from Python reader and file is
+// reads nblock from ANSYS.  Raw string is from Python reader and file is
 // positioned at the start of the data of NBLOCK
 //=============================================================================
 int read_nblock(char *raw, int *nnum, double *nodes, int nnodes, int* intsz,
 		int fltsz, int *n, int EOL, int nexp){
 
+  // set to start of the NBLOCK
+  raw += n[0];
+  int len_orig = strlen(raw);
+  int j;
 
-    char tempstr[100];
-    int i, j, k, nread, t, actual_nread;
-    double dvalue, sign, pow, scale;
-
-    int ivalue;
-    int found;
-
-    // set file position
-    i = *(n);
-
-    /* Read node data */
-    nread = fltsz - 5 - nexp; // sign, first int, point, xxx, scinot (4)
-    for (k=0; k<nnodes; ++k){
-        // Starts assuming file is positioned on node number
-
-        // Read node number
-        nnum[k] = fast_atoi2(raw, intsz[0], &i);
-
-        // skip fields 2 and 3
-        i += intsz[1];
-	i += intsz[2];
-
-        // Read next 6 fields
-        for (t=0; t<7; ++t){
-            
-            // Check if end of line character
-	    found = 0;
-	    while (raw[i] == '\r' || raw[i] == '\n' ){
-	      ++i;
-	      found = 1;
-	    }
-	    if (found) break;
-
-            //init value
-            dvalue = 1.0;
-
-            // check sign
-            if (raw[i] == '-') {
-                sign = -1.0;
-            }
-            else{
-                sign = 1.0;
-            }
-
-	    // verify not a sign
-	    // certain versions of workbench outputted archive files
-	    // have different float formats
-            ++i;
-            if (raw[i] == '-') {
-                sign = -1.0;
-		actual_nread = nread - 1;
-            }
-            else if (raw[i] != ' '){
-	        --i;
-		actual_nread = nread;
-            }
-	    else{
-	      actual_nread = nread - 1;
-            }
-
-	    // read first interger
-	    ++i;
-            dvalue *= raw[i] - '0';
-
-            // next is always a '.', skip it
-            ++i;
-
-            pow = 0.1;
-            for (j=0; j<actual_nread; ++j){
-                ++i;
-                dvalue += (raw[i] - '0')*pow;
-                pow *= 0.1;
-            }
-
-            // apply sign
-            dvalue *= sign;
-
-            // Read exponent
-            // Skip the E
-            ++i;
-
-
-            // store sign of exponent
-            ++i;
-            tempstr[0] = raw[i];
-            
-            // read exponent
-	    pow = 1;
-	    for (j=1; j<nexp; ++j){
-	      pow *= 10;
-	    }
-
-	    ivalue = 0;
-	    for (j=nexp; j>0; --j){
-	      ++i;
-	      ivalue += pow*(raw[i] - '0');
-	      pow /= 10;
-	    }
-
-            ++i;
-            if (ivalue == 0) {
-                // Store value
-                nodes[k*6 + t] = dvalue;
-                continue;
-            }
-
-            scale = 1.0;
-            while (ivalue > 0) { scale *= 10.0; ivalue -=  1; }
-
-            if (tempstr[0] == '+'){
-                dvalue *= scale;
-            }
-            else{
-                dvalue /= scale;
-            }
-
-            // Store value
-            nodes[k*6 + t] = dvalue;
-
-        }
-
-        // make empty fields 0.0
-        for (j=t; j<6; ++j){
-            nodes[k*6 + j] = 0.0;
-        }
-
+  for (int i=0; i<nnodes; i++){
+    nnum[i] = fast_atoi2(raw, intsz[0]);
+    raw += intsz[0];
+    raw += intsz[1];
+    raw += intsz[2];
+    
+    for (j=0; j<6; j++){
+      if (raw[0] == '\r' || raw[0] == '\n'){
+	break;
+      }
+      nodes[6*i + j] = strtod(raw, &raw);
     }
 
-    // return file position
-    return i;
-    
+    // remaining are zeros
+    for (; j<6; j++){
+      nodes[6*i + j] = 0;
+    }
+
+    while (raw[0] == '\r' || raw[0] == '\n'){
+      ++raw;
+    }
+  }
+
+  // return file position
+  int fpos = len_orig - strlen(raw) + n[0];
+  return fpos;
+
 }
 
 
