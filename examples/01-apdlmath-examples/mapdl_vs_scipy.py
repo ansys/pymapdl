a140fdb0cd590d6dfab6bb1c6d78ecfa03cad8d1
Alex Kaszynski <akascap@gmail.com>
2021-01-19 18:30:12 -0700

    additional flow cleanup

--- examples/01-apdlmath-examples/mapdl_vs_scipy.py
+++ examples/01-apdlmath-examples/mapdl_vs_scipy.py
@@ -22,7 +22,7 @@ import math
 import matplotlib.pylab as plt
 import numpy as np
 import scipy
-from scipy.sparse.linalg import eigs, eigsh
+from scipy.sparse.linalg import eigsh
 
 from ansys.mapdl.core import launch_mapdl
 from ansys.mapdl.core import examples
@@ -39,9 +39,10 @@ mm = mapdl.math
 
 ###############################################################################
 # APDLMath EigenSolve
-# First ask MAPDL to load the input file
+# First load the input file using MAPDL.
 #
 print(mapdl.input(examples.examples.wing_model))
+
 
 ###############################################################################
 # Plot and mesh using the ``eplot`` method.
@@ -49,8 +50,9 @@ mapdl.eplot()
 
 
 ###############################################################################
-# Next, setup a modal Analysis and ask for the $K$ and $M$ matrices to
-# be formed. MAPDL store these matrices in a ``.FULL`` file.
+# Next, setup a modal Analysis and request the :math:`K` and math:`M`
+# matrices to be formed. MAPDL stores these matrices in a ``.FULL``
+# file.
 
 print(mapdl.slashsolu())
 print(mapdl.antype(antype='MODAL'))
@@ -62,154 +64,153 @@ output = mapdl.solve()
 
 
 ###############################################################################
-# Read Sparse Matrices using pyMapdl
+# Read the sparse matrices using PyMapdl.
 #
 mapdl.finish()
 mm.free()
-K = mm.stiff(fname='file.full')
+k = mm.stiff(fname='file.full')
 M = mm.mass(fname='file.full')
 
 
 ###############################################################################
-# PyMapdl solves the Eigenproblem
-# 
-nev = 10
-A = mm.mat( K.nrow, nev)
-
-print( '\nCalling MAPDL to solve the eigenproblem .. \n')
-t1 = time.time()
-ev = mm.eigs( nev, K, M, phi=A, fmin=1.)
-t2 = time.time()
-MapdlElapsedTime = t2-t1
-print( '\nElapsed time to solve this problem : ', MapdlElapsedTime)
-
-###############################################################################
-# Print EigenFrequencies && Accuracy
-# 
-# Accuracy : $\frac{||(K-\lambda.M).\phi||_2}{||K.\phi||_2}$
-# 
-MapdlAcc = np.zeros(nev)
-
-for i in range(0, nev):
-    F = ev[i]                        # Eigenfrequency (Hz)
-    omega = 2*np.pi*F                # omega = 2.pi.Frequency
-    lam = omega**2                   # lambda = omega^2
-    
-    Phi = A[i]                       # i-th eigenshape                      
-    KPhi = K.dot(Phi)                     # K.Phi                                
-    MPhi = M.dot(Phi)                     # M.Phi                                
-    
-    KPhinrm = KPhi.norm()             # Normalization scalar value           
-
-    MPhi *= lam                      # (K-\lambda.M).Phi
-    KPhi -= MPhi
-    
-    MapdlAcc[i] = KPhi.norm()/KPhinrm      # We compute the residual
-    print( "[",i,"] : Freq = ",F,"\tHz, \t Residual = ",MapdlAcc[i])
-
-    
-###############################################################################
-# Ask SciPy to solve the same problem
+# Solve the eigenproblem using PyMapdl with APDLMath.
 #
-# First get MAPDL sparse matrices into SciPy world
-# 
-pK = K.asarray()
-pM = M.asarray()
+nev = 10
+A = mm.mat(k.nrow, nev)
 
-#get_ipython().run_line_magic('matplotlib', 'inline')
+t1 = time.time()
+ev = mm.eigs(nev, k, M, phi=A, fmin=1.)
+t2 = time.time()
+mapdl_elapsed_time = t2-t1
+print('\nElapsed time to solve this problem : ', mapdl_elapsed_time)
+
+###############################################################################
+# Print eigenfrequencies and accuracy.
+#
+# Accuracy : :math:`\frac{||(K-\lambda.M).\phi||_2}{||K.\phi||_2}`
+#
+mapdl_acc = np.empty(nev)
+
+for i in range(nev):
+    f = ev[i]                            # Eigenfrequency (Hz)
+    omega = 2*np.pi*f                    # omega = 2.pi.Frequency
+    lam = omega**2                       # lambda = omega^2
+
+    phi = A[i]                           # i-th eigenshape
+    kphi = k.dot(phi)                    # K.Phi
+    mphi = M.dot(phi)                    # M.Phi
+
+    kphi_nrm = kphi.norm()               # Normalization scalar value
+
+    mphi *= lam                          # (K-\lambda.M).Phi
+    kphi -= mphi
+
+    mapdl_acc[i] = kphi.norm()/kphi_nrm  # compute the residual
+    print(f"[{i}] : Freq = {f:8.2f} Hz\t Residual = {mapdl_acc[i]:.5}")
+
+
+###############################################################################
+# Use SciPy to Solve the same Eigenproblem
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# First get MAPDL sparse matrices into the Python memory as SciPy
+# matrices.
+#
+pk = k.asarray()
+pm = M.asarray()
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.suptitle('K and M Matrix profiles')
-ax1.spy( pK, markersize=0.01)
+ax1.spy(pk, markersize=0.01)
 ax1.set_title('K Matrix')
-ax2.spy( pM, markersize=0.01)
+ax2.spy(pm, markersize=0.01)
 ax2.set_title('M Matrix')
 plt.show(block=True)
 
 
 ###############################################################################
-# Need to Unsymetrize the sparse matrices for SciPy
-#  ( -> Not good for memory usage)
+# Make the sparse matrices for SciPy unsymmetric as symmetric matrices in SciPy
+# are memory inefficient.
 #
-# $K = K + K^T - diag(K)$
+# :math:`K = K + K^T - diag(K)`
 #
-pKd = scipy.sparse.diags(pK.diagonal())
-pK = pK + pK.transpose() - pKd
-pMd = scipy.sparse.diags(pM.diagonal())
-pM = pM + pM.transpose() - pMd
+pkd = scipy.sparse.diags(pk.diagonal())
+pK = pk + pk.transpose() - pkd
+pmd = scipy.sparse.diags(pm.diagonal())
+pm = pm + pm.transpose() - pmd
 
 
 ###############################################################################
 # Plot Matrices
 #
-#get_ipython().run_line_magic('matplotlib', 'inline')
-
 fig, (ax1, ax2) = plt.subplots(1, 2)
 fig.suptitle('K and M Matrix profiles')
-ax1.spy( pK, markersize=0.01)
+ax1.spy(pk, markersize=0.01)
 ax1.set_title('K Matrix')
-ax2.spy( pM, markersize=0.01)
+ax2.spy(pm, markersize=0.01)
 ax2.set_title('M Matrix')
 plt.show(block=True)
 
 
 ###############################################################################
-# Run the Eigensolve
-# 
-print( '\nCalling SCIPY to solve the eigenproblem .. \n')
+# Solve the eigenproblem
+#
 t3 = time.time()
-vals, vecs = eigsh( A=pK, M=pM, k=10, sigma=1, which='LA')
+vals, vecs = eigsh(A=pK, M=pm, k=10, sigma=1, which='LA')
 t4 = time.time()
-SciPyElapsedTime = t4-t3
-print( '\nElapsed time to solve this problem : ', SciPyElapsedTime)
+scipy_elapsed_time = t4 - t3
+print('\nElapsed time to solve this problem : ', scipy_elapsed_time)
 
 
 ###############################################################################
-# We convert Lambda values to Frequency values:
-# $freq = \frac{\sqrt(\lambda)}{2.\pi}$
+# Convert Lambda values to Frequency values:
+# :math:`freq = \frac{\sqrt(\lambda)}{2.\pi}`
 #
-Freqs = np.sqrt(vals) / (2*math.pi)
+freqs = np.sqrt(vals) / (2*math.pi)
 
 
 ###############################################################################
-# We compute residual errors:
-# $Err=\frac{||(K-\lambda.M).\phi||_2}{||K.\phi||_2}$
+# Compute the residual error for SciPy.
 #
-SciPyAcc = np.zeros(nev)
+# :math:`Err=\frac{||(K-\lambda.M).\phi||_2}{||K.\phi||_2}`
+#
+scipy_acc = np.zeros(nev)
 
-for i in range(0, nev):
-    lam = vals[i]                      # i-th eigenvalue
-    Phi = vecs.T[i]                    # i-th eigenshape
-    
-    KPhi = pK*Phi.T                    # K.Phi
-    MPhi = pM*Phi.T                    # M.Phi
-    
-    KPhinrm = np.linalg.norm(KPhi,2)             # Normalization scalar value
+for i in range(nev):
+    lam = vals[i]                       # i-th eigenvalue
+    phi = vecs.T[i]                     # i-th eigenshape
 
-    MPhi *= lam                      # (K-\lambda.M).Phi
-    KPhi -= MPhi
-    
-    SciPyAcc[i] = np.linalg.norm(KPhi,2)/KPhinrm      # We compute the residual
-    print( "[",i,"] : Freq = ", Freqs[i], "\tHz, \t Residual = ", SciPyAcc[i])
+    kphi = pk*phi.T                     # K.Phi
+    mphi = pm*phi.T                     # M.Phi
+
+    kphi_nrm = np.linalg.norm(kphi, 2)  # Normalization scalar value
+
+    mphi *= lam                         # (K-\lambda.M).Phi
+    kphi -= mphi
+
+    scipy_acc[i] = 1 - np.linalg.norm(kphi, 2)/kphi_nrm  # compute the residual
+    print(f"[{i}] : Freq = {freqs[i]:8.2f} Hz\t Residual = {scipy_acc[i]:.5}")
 
 
 ###############################################################################
-# How MAPDL is more accurate than SciPy
+# MAPDL is more accurate than SciPy.
 #
-fig = plt.figure(figsize=(12,10))
+fig = plt.figure(figsize=(12, 10))
 ax = plt.axes()
 x = np.linspace(1, 10, 10)
-plt.title('Residual Error (%)')
+plt.title('Residual Error')
 plt.yscale('log')
-plt.xlabel('Frequency #')
-plt.ylabel('Errors (%)')
-ax.bar(x, SciPyAcc, label='SciPy Results')
-ax.bar(x, MapdlAcc, label='MAPDL Results')
+plt.xlabel('Mode')
+plt.ylabel('% Error')
+ax.bar(x, scipy_acc, label='SciPy Results')
+ax.bar(x, mapdl_acc, label='MAPDL Results')
 plt.legend(loc='lower right')
-
+plt.show()
 
 ###############################################################################
-# How MAPDL is faster than SciPy
-# 
-Ratio = SciPyElapsedTime/MapdlElapsedTime
-print( "Mapdl is ",Ratio," times faster")
+# MAPDL is faster than SciPy.
+#
+ratio = scipy_elapsed_time/mapdl_elapsed_time
+print(f"Mapdl is {ratio:.3} times faster")
