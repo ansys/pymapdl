db231a3f4c514030489d9b17038acaeb6412856f
Alex Kaszynski <akascap@gmail.com>
2021-01-19 20:13:52 -0700

    version bump to 0.57.3

--- ansys/mapdl/core/_version.py
+++ ansys/mapdl/core/_version.py
@@ -1,7 +1,7 @@
 """Version of ansys-mapdl-core module."""
 
 # major, minor, patch
-version_info = 0, 57, 2
+version_info = 0, 57, 3
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
