04cc6a41d19124cf71bda345ddd37abc8b930cf4
Alex Kaszynski <akascap@gmail.com>
2021-01-26 22:03:52 -0700

    add ability to ignore errors

--- ansys/mapdl/core/_version.py
+++ ansys/mapdl/core/_version.py
@@ -1,7 +1,7 @@
 """Version of ansys-mapdl-core module."""
 
 # major, minor, patch
-version_info = 0, 57, 4
+version_info = 0, 57, 5
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
