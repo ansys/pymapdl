aec7a47f6f21f748c30f032725b43cf4b35ea0ed
Alex Kaszynski <akascap@gmail.com>
2021-05-22 15:31:25 -0600

    fix tests

--- tests/test_element.py
+++ tests/test_element.py
@@ -3,7 +3,7 @@ import pytest
 import numpy as np
 
 from ansys.mapdl.core import examples
-from ansys.mapdl.core._commands.element import parse_e
+from ansys.mapdl.core.mapdl_functions import parse_e
 from ansys.mapdl.core._commands.preproc.element_type import parse_et
 
 
