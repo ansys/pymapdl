64ae63f82921bf969372d2b899b37001fdecec95
Alex Kaszynski <akascap@gmail.com>
2021-06-10 00:12:22 -0600

    Add matplotlib to imports (#475)
    * add matplotlib as dependency
    
    * fix license
    
    * bump reader version req

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ with io_open(version_file, mode='r') as fd:
     exec(fd.read())
 
 install_requires = [
+    'matplotlib>=3.0.0'  # for colormaps for pyvista
     'numpy>=1.14.0',
     'pyvista>=0.30.1',
     'appdirs>=1.4.0',
@@ -23,7 +24,7 @@ install_requires = [
     'google-api-python-client',
     'grpcio>=1.30.0',  # tested up to grpcio==1.35
     'ansys-grpc-mapdl==0.4.0',  # supports at least 2020R2 - 2021R2
-    'ansys-mapdl-reader>=0.50.0',
+    'ansys-mapdl-reader>=0.50.15',
     'ansys-corba',  # pending depreciation to ansys-mapdl-corba
     'protobuf>=3.1.4',  # had an issue with gRPC health checking with this version
 ]
