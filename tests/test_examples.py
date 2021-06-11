bbfd1840bfe338825c2d17d376fba8d2e4010191
Alex Kaszynski <akascap@gmail.com>
2021-06-11 12:01:43 -0600

    add in download testing

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,3 +1,4 @@
+import re
 import os
 
 from ansys.mapdl.core import examples
@@ -6,3 +7,16 @@ from ansys.mapdl.core import examples
 def test_load_verif():
     for filename in examples.vmfiles.values():
         assert os.path.isfile(filename)
+
+
+def test_bracket(mapdl, cleared):
+    # note that this method just returns a file path
+    bracket_file = examples.download_bracket()
+
+    assert os.path.isfile(bracket_file)
+
+    # load the bracket and then print out the geometry
+    mapdl.aux15()
+    out = mapdl.igesin(bracket_file)
+    n_ent = re.findall(r'TOTAL NUMBER OF ENTITIES \s*=\s*(\d*)', out)
+    assert int(n_ent[0]) > 0
