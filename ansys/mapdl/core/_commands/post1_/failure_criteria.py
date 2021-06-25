f738599383e38a729b21d0b8b9baac4e46a72044
jgd10 <jamesgordonderrick@gmail.com>
2021-06-25 10:59:56 +0100

    refactored post1 files

--- ansys/mapdl/core/_commands/post1_/failure_criteria.py
+++ ansys/mapdl/core/_commands/post1_/failure_criteria.py
@@ -1,209 +1,207 @@
-def fc(self, mat="", lab1="", lab2="", data1="", data2="", data3="",
-       data4="", data5="", data6="", **kwargs):
-    """Provides failure criteria information and activates a data
+class FailureCriteria:
 
-    APDL Command: FC
-    table to input temperature-dependent stress and strain limits.
+    def fc(self, mat="", lab1="", lab2="", data1="", data2="", data3="",
+           data4="", data5="", data6="", **kwargs):
+        """Provides failure criteria information and activates a data
 
-    Parameters
-    ----------
-    mat
-        Material reference number. You can define failure criteria
-        for up to 250 different materials.
+        APDL Command: FC
+        table to input temperature-dependent stress and strain limits.
 
-    lab1
-        Type of data.
+        Parameters
+        ----------
+        mat
+            Material reference number. You can define failure criteria
+            for up to 250 different materials.
 
-        TEMP - Temperatures. Each of the materials you define can
-        have a different set of temperatures to define the failure
-        criteria.
+        lab1
+            Type of data.
 
-        EPEL - Strains.
+            TEMP - Temperatures. Each of the materials you define can
+            have a different set of temperatures to define the failure
+            criteria.
 
-        S - Stresses.
+            EPEL - Strains.
 
-    lab2
-        Specific criteria. Not used if Lab1 = TEMP.
+            S - Stresses.
 
-        XTEN - Allowable tensile stress or strain in the
-        x-direction. (Must be positive.)
+        lab2
+            Specific criteria. Not used if Lab1 = TEMP.
 
-        XCMP - Allowable compressive stress or strain in the
-        x-direction. (Defaults to negative of XTEN.)
+            XTEN - Allowable tensile stress or strain in the
+            x-direction. (Must be positive.)
 
-        YTEN - Allowable tensile stress or strain in the
-        y-direction. (Must be positive.)
+            XCMP - Allowable compressive stress or strain in the
+            x-direction. (Defaults to negative of XTEN.)
 
-        YCMP - Allowable compressive stress or strain in the
-        y-direction. (Defaults to negative of YTEN.)
+            YTEN - Allowable tensile stress or strain in the
+            y-direction. (Must be positive.)
 
-        ZTEN - Allowable tensile stress or strain in the
-        z-direction. (Must be positive.)
+            YCMP - Allowable compressive stress or strain in the
+            y-direction. (Defaults to negative of YTEN.)
 
-        ZCMP - Allowable compressive stress or strain in the
-        z-direction. (Defaults to negative of ZTEN.)
+            ZTEN - Allowable tensile stress or strain in the
+            z-direction. (Must be positive.)
 
-        XY - Allowable XY stress or shear strain. (Must be
-        positive.)
+            ZCMP - Allowable compressive stress or strain in the
+            z-direction. (Defaults to negative of ZTEN.)
 
-        YZ - Allowable YZ stress or shear strain. (Must be
-        positive.)
+            XY - Allowable XY stress or shear strain. (Must be
+            positive.)
 
-        XZ - Allowable XZ stress or shear strain. (Must be
-        positive.)
+            YZ - Allowable YZ stress or shear strain. (Must be
+            positive.)
 
-        XYCP - XY coupling coefficient (Used only if Lab1 =
-        S). Defaults to -1.0. [1]
+            XZ - Allowable XZ stress or shear strain. (Must be
+            positive.)
 
-        YZCP - YZ coupling coefficient (Used only if Lab1 =
-        S). Defaults to -1.0. [1]
+            XYCP - XY coupling coefficient (Used only if Lab1 =
+            S). Defaults to -1.0. [1]
 
-        XZCP - XZ coupling coefficient (Used only if Lab1 =
-        S). Defaults to -1.0. [1]
+            YZCP - YZ coupling coefficient (Used only if Lab1 =
+            S). Defaults to -1.0. [1]
 
-    data1, data2, data3, . . . , data6
-        Description of DATA1 through DATA6.
+            XZCP - XZ coupling coefficient (Used only if Lab1 =
+            S). Defaults to -1.0. [1]
 
-        T1, T2, T3, T4, T5, T6 - Temperature at which limit data
-        is input. Used only when Lab1 = TEMP.
+        data1, data2, data3, . . . , data6
+            Description of DATA1 through DATA6.
 
-        V1, V2, V3, V4, V5, V6 - Value of limit stress or strain
-        at temperature T1 through T6. Used only when Lab1 = S or
-        EPEL.
+            T1, T2, T3, T4, T5, T6 - Temperature at which limit data
+            is input. Used only when Lab1 = TEMP.
 
-    Notes
-    -----
-    The data table can be input in either PREP7 or POST1. This
-    table is used only in POST1. When you postprocess failure
-    criteria results defined via the FC command (PLESOL, PRESOL,
-    PLNSOL, PRNSOL, PRRSOL, etc.), the active coordinate system
-    must be the coordinate system of the material being
-    analyzed. You do this using RSYS, SOLU. For layered
-    applications, you also use the LAYER command. See the specific
-    element documentation in the Element Reference for information
-    on defining your coordinate system for layers.
+            V1, V2, V3, V4, V5, V6 - Value of limit stress or strain
+            at temperature T1 through T6. Used only when Lab1 = S or
+            EPEL.
 
-    Some plotting and printing functions will not support Failure
-    Criteria for your PowerGraphics displays. This could result in
-    minor changes to other data when Failure Criteria are
-    applied. See the appropriate plot or print command
-    documentation for more information.
-    """
-    command = "FC,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (str(mat), str(lab1), str(
-        lab2), str(data1), str(data2), str(data3), str(data4), str(data5), str(data6))
-    return self.run(command, **kwargs)
+        Notes
+        -----
+        The data table can be input in either PREP7 or POST1. This
+        table is used only in POST1. When you postprocess failure
+        criteria results defined via the FC command (PLESOL, PRESOL,
+        PLNSOL, PRNSOL, PRRSOL, etc.), the active coordinate system
+        must be the coordinate system of the material being
+        analyzed. You do this using RSYS, SOLU. For layered
+        applications, you also use the LAYER command. See the specific
+        element documentation in the Element Reference for information
+        on defining your coordinate system for layers.
 
+        Some plotting and printing functions will not support Failure
+        Criteria for your PowerGraphics displays. This could result in
+        minor changes to other data when Failure Criteria are
+        applied. See the appropriate plot or print command
+        documentation for more information.
+        """
+        command = "FC,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (str(mat), str(lab1), str(
+            lab2), str(data1), str(data2), str(data3), str(data4), str(data5), str(data6))
+        return self.run(command, **kwargs)
 
-def fccheck(self, **kwargs):
-    """Checks both the strain and stress input criteria for all materials.
+    def fccheck(self, **kwargs):
+        """Checks both the strain and stress input criteria for all materials.
 
-     APDL Command: FCCHECK
+         APDL Command: FCCHECK
 
-    Notes
-    -----
-    Issue the FCCHECK command to check the completeness of the input during
-    the input phase.
-    """
-    command = "FCCHECK,"
-    return self.run(command, **kwargs)
+        Notes
+        -----
+        Issue the FCCHECK command to check the completeness of the input during
+        the input phase.
+        """
+        command = "FCCHECK,"
+        return self.run(command, **kwargs)
 
+    def fcdele(self, mat="", **kwargs):
+        """Deletes previously defined failure criterion data for the given
 
-def fcdele(self, mat="", **kwargs):
-    """Deletes previously defined failure criterion data for the given
+        APDL Command: FCDELE
+        material.
 
-    APDL Command: FCDELE
-    material.
+        Parameters
+        ----------
+        mat
+            Material number. Deletes all FC command input for this material.
 
-    Parameters
-    ----------
-    mat
-        Material number. Deletes all FC command input for this material.
+        Notes
+        -----
+        This command is also valid in POST1.
+        """
+        command = "FCDELE,%s" % (str(mat))
+        return self.run(command, **kwargs)
 
-    Notes
-    -----
-    This command is also valid in POST1.
-    """
-    command = "FCDELE,%s" % (str(mat))
-    return self.run(command, **kwargs)
+    def fclist(self, mat="", temp="", **kwargs):
+        """To list what the failure criteria is that you have input.
 
+        APDL Command: FCLIST
 
-def fclist(self, mat="", temp="", **kwargs):
-    """To list what the failure criteria is that you have input.
+        Parameters
+        ----------
+        mat
+             Material number (defaults to ALL for all materials).
 
-    APDL Command: FCLIST
+        temp
+            Temperature to be evaluated at (defaults to TUNIF).
 
-    Parameters
-    ----------
-    mat
-         Material number (defaults to ALL for all materials).
+        Notes
+        -----
+        This command allows you to see what you have already input for failure
+        criteria using the FC commands.
+        """
+        return self.run(f"FCLIST,{mat},,{temp}", **kwargs)
 
-    temp
-        Temperature to be evaluated at (defaults to TUNIF).
+    def fctyp(self, oper="", lab="", **kwargs):
+        """Activates or removes failure-criteria types for postprocessing.
 
-    Notes
-    -----
-    This command allows you to see what you have already input for failure
-    criteria using the FC commands.
-    """
-    return self.run(f"FCLIST,{mat},,{temp}", **kwargs)
+        APDL Command: FCTYP
 
+        Parameters
+        ----------
+        oper
+            Operation key:
 
-def fctyp(self, oper="", lab="", **kwargs):
-    """Activates or removes failure-criteria types for postprocessing.
+            ADD - Activate failure-criteria types. This option is the default behavior.
 
-    APDL Command: FCTYP
+            DELE - Remove failure-criteria types.
 
-    Parameters
-    ----------
-    oper
-        Operation key:
+        lab
+            Valid failure-criteria labels. If ALL, select all available
+            (including user-defined) failure criteria.
 
-        ADD - Activate failure-criteria types. This option is the default behavior.
+            EMAX - Maximum strain criterion (default)
 
-        DELE - Remove failure-criteria types.
+            SMAX - Maximum stress criterion (default)
 
-    lab
-        Valid failure-criteria labels. If ALL, select all available
-        (including user-defined) failure criteria.
+            TWSI  - Tsai-Wu strength index (default)
 
-        EMAX - Maximum strain criterion (default)
+            TWSR  - Inverse of Tsai-Wu strength ratio index (default)
 
-        SMAX - Maximum stress criterion (default)
+            HFIB  - Hashin fiber failure criterion
 
-        TWSI  - Tsai-Wu strength index (default)
+            HMAT  - Hashin matrix failure criterion
 
-        TWSR  - Inverse of Tsai-Wu strength ratio index (default)
+            PFIB  - Puck fiber failure criterion
 
-        HFIB  - Hashin fiber failure criterion
+            PMAT  - Puck inter-fiber (matrix) failure criterion
 
-        HMAT  - Hashin matrix failure criterion
+            L3FB - LaRc03 fiber failure criterion
 
-        PFIB  - Puck fiber failure criterion
+            L3MT - LaRc03 matrix failure criterion
 
-        PMAT  - Puck inter-fiber (matrix) failure criterion
+            L4FB - LaRc04 fiber failure criterion
 
-        L3FB - LaRc03 fiber failure criterion
+            L4MT - LaRc04 matrix failure criterion
 
-        L3MT - LaRc03 matrix failure criterion
+            USR1 through USR9  - User-defined failure criteria
 
-        L4FB - LaRc04 fiber failure criterion
+        Notes
+        -----
+        The FCTYP command modifies the list of active failure criteria.
 
-        L4MT - LaRc04 matrix failure criterion
+        By default, active failure criteria include EMAX, SMAX, TWSI, and TWSR.
 
-        USR1 through USR9  - User-defined failure criteria
+        The command affects any subsequent postprocessing listing and plotting
+        commands (such as PRESOL, PRNSOL, PLESOL, PLNSOL, and ETABLE).
 
-    Notes
-    -----
-    The FCTYP command modifies the list of active failure criteria.
-
-    By default, active failure criteria include EMAX, SMAX, TWSI, and TWSR.
-
-    The command affects any subsequent postprocessing listing and plotting
-    commands (such as PRESOL, PRNSOL, PLESOL, PLNSOL, and ETABLE).
-
-    A single FCTYP command allows up to six failure-criteria labels. If
-    needed, reissue the command to activate or remove additional failure-
-    criteria types.
-    """
-    command = f"FCTYP,{oper},{lab}"
-    return self.run(command, **kwargs)
+        A single FCTYP command allows up to six failure-criteria labels. If
+        needed, reissue the command to activate or remove additional failure-
+        criteria types.
+        """
+        command = f"FCTYP,{oper},{lab}"
+        return self.run(command, **kwargs)
