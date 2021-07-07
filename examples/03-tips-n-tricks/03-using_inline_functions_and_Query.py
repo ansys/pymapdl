6fee67cd15ab015541e8b14ec313956164663c0e
jgd10 <jamesgordonderrick@gmail.com>
2021-07-07 10:04:47 +0100

    Created example based on UX examples
    Created example based ont he examples used in the UX/Y/Z docstrings.
    
    This example is a *little* bit more detailed and includes plotting, as
    well as an example of how to use the Query class to extract data from a
    simulation.

---
+++ examples/03-tips-n-tricks/03-using_inline_functions_and_Query.py
@@ -0,0 +1,116 @@
+"""
+.. _ref_how_to_use_Query:
+
+Using inline functions (Query)
+-----------------------------
+
+Inline functions like ``UX`` have been implemented in PyMAPDL as methods
+on the ``mapdl.inline_functions.Query`` object. In this example we set
+up a simple simulation and use ``Query`` to demonstrate some of its
+functionality.
+
+We create a ``Query`` instance as below, using the ``mapdl`` instance
+as a required argument.
+"""
+
+from ansys.mapdl.core import launch_mapdl
+from ansys.mapdl.core.inline_functions import Query
+
+mapdl = launch_mapdl()
+mapdl.prep7()
+q = Query(mapdl)
+
+###############################################################################
+# Setup Mesh
+# ~~~~~~~~~~
+# - Assign element type ``SOLID5`` to element type 1
+# - Create a cuboid ``mapdl.block`` 10 x 20 x 30 in dimension
+# - Set element size to 2
+# - Mesh the block
+# - Plot the elements created
+
+mapdl.et(1, 'SOLID5')
+mapdl.block(0, 10, 0, 20, 0, 30)
+mapdl.esize(2)
+mapdl.vmesh('ALL')
+mapdl.eplot()
+
+###############################################################################
+# Setup Boundary Conditions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# - Assign an Elastic modulus in the x-direction to material 1 of 21e9
+# - Select all nodes at the ``z = 30`` end of the block
+# - Remove all degrees of freedom for all nodes in the selection
+#   - I.e. fix them in place
+# - Select all nodes at the ``z = 0`` end
+# - Apply a x-direction force of 10000 to all of these
+# - Finish preprocessing
+
+mapdl.mp('EX', 1, 210E9)
+mapdl.nsel('S', 'LOC', 'Z', 30)
+mapdl.d('ALL', 'UX')
+mapdl.d('ALL', 'UY')
+mapdl.d('ALL', 'UZ')
+mapdl.nsel('S', 'LOC', 'Z', 0)
+mapdl.f('ALL', 'FX', 10000)
+mapdl.finish()
+
+###############################################################################
+# Setup Boundary Conditions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# - Enter solution (``mapdl.slashsolu`` also works)
+# - Set the analysis type to ``STATIC``
+# - Select all nodes
+# - Solve the model
+# - Finish solution
+
+mapdl.run('/SOLU')
+mapdl.antype('STATIC')
+mapdl.nsel('ALL')
+mapdl.solve()
+mapdl.finish()
+
+###############################################################################
+# Post-Processing
+# ~~~~~~~~~~~~~~~
+# - Get the result from the ``mapdl`` instance
+# - Plot the equivalent stress results
+#   - Show the edges so that we can see the element boundaries
+#   - Use the "plasma" colormap because it is perceptually uniform
+
+result = mapdl.result
+result.plot_principal_nodal_stress(0,
+                                   'SEQV',
+                                   show_edges=True,
+                                   cmap='plasma')
+
+###############################################################################
+# Using ``Query``
+# ~~~~~~~~~~~~~~~
+# - Use ``Query`` to get the nodes nearest to (5, 0, 0) and (5, 10, 0)
+# - Use the ``Query`` instance to examine the x, y, and z displacement.
+# - Print the results in a formatted string.
+
+node1 = q.node(5., 0., 0.)
+node2 = q.node(5., 10., 0.)
+
+for node in [node1, node2]:
+    x_displacement = q.ux(node)
+    y_displacement = q.uy(node)
+    z_displacement = q.uz(node)
+
+    message = f"""
+    ************************
+    Displacement at Node {node}:
+    ************************
+    _____
+    X | {x_displacement}
+    Y | {y_displacement}
+    Z | {z_displacement}
+    
+    """
+    print(message)
+
+
+
+
