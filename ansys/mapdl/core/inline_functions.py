1fc29062c85264725280b8b8a38298f33e048bc0
jgd10 <jamesgordonderrick@gmail.com>
2021-07-08 16:55:31 +0100

    copied the nsel example into SelectionStatus

--- ansys/mapdl/core/inline_functions.py
+++ ansys/mapdl/core/inline_functions.py
@@ -11,6 +11,33 @@ class SelectionStatus(IntEnum):
     UNSELECTED = -1
     UNDEFINED = 0
     SELECTED = 1
+
+    Examples
+    --------
+    The following example is taken from ``Query.nsel`` and demonstrates
+    how `SelectionSatus` appears in PyMAPDL.
+
+    Here we create a single node and interrogate its selection
+    status.
+    >>> from ansys.mapdl.core import launch_mapdl
+    >>> from ansys.mapdl.core.inline_functions import Query
+    >>> mapdl = launch_mapdl()
+    >>> mapdl.prep7()
+    >>> n1 = mapdl.n(1, 0, 0, 0)
+    >>> n1
+    1
+    We can use ``Query.nsel`` to interrogate the selection status
+    of the node. The response is an ``enum.IntEnum`` object. If
+    you query a node that does not exist, it will return a status
+    ``SelectionStatus.UNDEFINED``.
+    >>> q = Query(mapdl)
+    >>> q.nsel(n1)
+    <SelectionStatus.SELECTED: 1>
+    >>> mapdl.nsel('NONE')
+    >>> q.nsel(n1)
+    <SelectionStatus.UNSELECTED: -1>
+    >>> q.nsel(0)
+    <SelectionStatus.UNDEFINED: 0>
     """
     UNSELECTED = -1
     UNDEFINED = 0
