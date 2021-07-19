8037c121a2d73466d4c772ff4ae33e963cfaba3a
Alex Kaszynski <akascap@gmail.com>
2021-07-18 23:52:41 -0600

    bump to 0.59.4

--- ansys/mapdl/core/_version.py
+++ ansys/mapdl/core/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 58, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 59, 3
+version_info = 0, 59, 4
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
