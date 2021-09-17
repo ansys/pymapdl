2ae465502e60f34c639985e501d30210582862d8
Alex Kaszynski <akascap@gmail.com>
2021-09-17 17:07:13 -0600

    add pdf step

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -177,7 +177,7 @@ latex_elements = {}
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "pyansys.tex", "ansys.mapdl.core Documentation", author, "manual"),
+    (master_doc, "pymapdl.tex", "ansys.mapdl.core Documentation", author, "manual"),
 ]
 
 
