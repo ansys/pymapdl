677ccdbe3c959829614225a68911552b9f963bc0
jgd10 <jamesgordonderrick@gmail.com>
2021-10-06 12:52:47 +0100

    fixed blank line problem

--- tests/test_path.py
+++ tests/test_path.py
@@ -15,6 +15,3 @@ def test_path_with_single_quote(mapdl, path_tests):
     with pytest.raises(RuntimeError):
         resp = mapdl.cwd(path_tests.path_with_single_quote)
         assert 'WARNING' not in resp
-
-
-
