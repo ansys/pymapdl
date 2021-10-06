68d3908fafe5b9610839989b8836d2e356afd5d0
jgd10 <jamesgordonderrick@gmail.com>
2021-10-06 12:46:27 +0100

    Added support for CWD to contain spaces
    the cwd command accepts directory paths with spaces when enclosed in a
    pair of single quotes/apostrophes. However, it seems that it does NOT
    support directory path's that otherwise contain single quotes. I have
    added a note to this effect to the docstring.
    
    Otherwise this commit enables the supplication of directory paths with
    spaces in as well as two tests for this. The methods could (and perhaps
    should) be extended to other path-related commands, but I'm not sure
    which these are, and would welcome input from other people.
    
    Otherwise, this commit should fix issue #641.

---
+++ tests/test_path.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def test_path_without_spaces(mapdl, path_tests):
+    resp = mapdl.cwd(path_tests.path_without_spaces)
+    assert 'WARNING' not in resp
+
+
+def test_path_with_spaces(mapdl, path_tests):
+    resp = mapdl.cwd(path_tests.path_with_spaces)
+    assert 'WARNING' not in resp
+
+
+def test_path_with_single_quote(mapdl, path_tests):
+    with pytest.raises(RuntimeError):
+        resp = mapdl.cwd(path_tests.path_with_single_quote)
+        assert 'WARNING' not in resp
+
+
+
