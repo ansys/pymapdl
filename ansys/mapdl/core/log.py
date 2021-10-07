885d5c8ff994cf6c7e6b84be8a3d23dc85cf137a
German <germanmartinezayuso@gmail.com>
2021-10-07 10:56:32 +0200

    Implementing alex comment.
    We forgot about distintion between threaded and normal mapdl execution.
    We use now instances, which has unique name.

--- ansys/mapdl/core/log.py
+++ ansys/mapdl/core/log.py
@@ -2,11 +2,13 @@ import logging
 from datetime import datetime
 import sys
 
+from ansys.mapdl.core.mapdl import _MapdlCore
+
 ## General configuration
 LOG_LEVEL = logging.DEBUG
 FILE_NAME = 'pyansys.log'
 
-## Single configuration
+## Single configuration #79
 STDOUT_MSG_FORMAT = '| %(levelname)-11s | %(module)-15s | %(funcName)-25s | %(message)s'
 FILE_MSG_FORMAT = '| %(asctime)-15s | %(levelname)-12s | %(module)-15s | %(funcName)-25s | %(message)s'
 DEFAULT_FILE_HEADER = """
@@ -34,6 +36,16 @@ NEW_SESSION_HEADER = f"""\n=====================================================
 ========================                      NEW SESSION - {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}                             ========================
 ====================================================================================================================================="""
 
+#changing the log record.
+old_factory = logging.getLogRecordFactory()
+
+def record_factory(*args, **kwargs):
+    record = old_factory(*args, **kwargs)
+    record.custom_attribute = 0xdecafbad
+    return record
+
+logging.setLogRecordFactory(record_factory)
+
 class PyansysLogger():
     """Logger used for each Pyansys logger.
     This class allows you to add handler to a file or standard output.
@@ -50,105 +62,57 @@ class PyansysLogger():
     """
 
     _file_handler = None
-    _std_out_handler = None
-    _is_pool = False
+    _std_out_handler = None 
     _level = logging.DEBUG
+    _instances = {}
 
-    def __init__(self, level=_level, filename=FILE_NAME, to_stdout=True):
+    def __init__(self, level=_level, to_file=False, to_stdout=True, filename=FILE_NAME):
 
         self._global = logging.getLogger('_Global_')
+        self._global.level = level
 
-        if not self._global.handlers:
-            self._global.level = level
-            self._global._is_pool = self._is_pool
-            self.add_handling_uncaught_expections(self._global) # Using logger to record unhandled exceptions
+        self.debug    = self._global.debug
+        self.info     = self._global.info
+        self.warning  = self._global.warning
+        self.error    = self._global.error
+        self.critical = self._global.critical
 
-            if filename:
-                self._file_handler = logging.FileHandler(filename)
-                self._file_handler.setLevel(level)
-                self._file_handler.setFormatter(logging.Formatter(FILE_MSG_FORMAT))
-                self._global.addHandler(self._file_handler)
+        if to_file or filename != FILE_NAME:
+            # We record to file
+            self._file_handler = logging.FileHandler(filename)
+            self._file_handler.setLevel(level)
+            self._file_handler.setFormatter(logging.Formatter(FILE_MSG_FORMAT))
+            self._global.addHandler(self._file_handler)
 
-                self._file_handler.stream.write(NEW_SESSION_HEADER)
-                self._file_handler.stream.write(DEFAULT_FILE_HEADER)
+            self._file_handler.stream.write(NEW_SESSION_HEADER)
+            self._file_handler.stream.write(DEFAULT_FILE_HEADER)
 
-            if to_stdout:
-                self._std_out_handler = logging.StreamHandler()
-                self._std_out_handler.setLevel(level)
-                self._std_out_handler.setFormatter(logging.Formatter(STDOUT_MSG_FORMAT))
-                self._global.addHandler(self._std_out_handler)
+        if to_stdout:
+            self._std_out_handler = logging.StreamHandler()
+            self._std_out_handler.setLevel(level)
+            self._std_out_handler.setFormatter(logging.Formatter(STDOUT_MSG_FORMAT))
+            self._global.addHandler(self._std_out_handler)
 
-                self._std_out_handler.stream.write(DEFAULT_STDOUT_HEADER)
+            self._std_out_handler.stream.write(DEFAULT_STDOUT_HEADER)
 
+        self.add_handling_uncaught_expections(self._global) # Using logger to record unhandled exceptions
+
+    def _add_instance(self, obj):
+        if isinstance(obj, str):
+            self._instances[obj] = self._global.getChild(obj)
+        elif issubclass(obj, _MapdlCore):
+            self._instances[obj._name] = self._global.getChild(obj._name)
         else:
-            self._level = self._global.level
-            self._is_pool = self._global._is_pool
-            self._file_handler = self._global.handlers[0]
-            self._std_out_handler = self._global.handlers[1]
+            raise Exception("You can only add 'str' or 'MAPDL' classes to this method.")
+        return self._instances[obj]
 
-    @property
-    def level(self):
-        return self._level
-
-    @level.setter
-    def level(self, newlevel):
-        if not isinstance(newlevel, int) and newlevel > 100 and newlevel < 0:
-            raise Exception("The log level must be an integer and being between 0 and 100.")
-
-        self._level = newlevel
-        self._global.level = newlevel
-        self._std_out_handler.setLevel(newlevel)
-        self._file_handler.setLevel(newlevel)
-
-    @property
-    def is_pool(self):
-        return self._is_pool
-
-    @is_pool.setter
-    def is_pool(self, newbool):
-        if not isinstance(newbool, bool):
-            raise Exception("'is_pool' arguments can be only booleans.")
-
-        if newbool:
-            self.debug("Switching to 'multiple thread' style logging.")
-            if self._file_handler:
-                self._file_handler.setFormatter(logging.Formatter(FILE_MSG_POOL_FORMAT))
-                self._file_handler.stream.write(DEFAULT_FILE_HEADER_POOL)
-
-            if self._std_out_handler:
-                self._std_out_handler.setFormatter(logging.Formatter(STDOUT_MSG_POOL_FORMAT))
-                self._std_out_handler.stream.write(DEFAULT_STDOUT_HEADER_POOL)
+    def __getitem__(self, key):
+        if key in self._instances.keys():
+            return self._instances[key]
         else:
-            if self._is_pool:
-                self.debug("Switching back to 'single thread' style logging.")
-
-            if self._file_handler:
-                self._file_handler.setFormatter(logging.Formatter(FILE_MSG_FORMAT))
-                self._file_handler.stream.write(DEFAULT_FILE_HEADER)
-            if self._std_out_handler:
-                self._std_out_handler.setFormatter(logging.Formatter(STDOUT_MSG_FORMAT))
-                self._std_out_handler.stream.write(DEFAULT_STDOUT_HEADER)
-
-        self._is_pool = newbool
-        self._global._is_pool = newbool
-
-    def debug(self, *args, **kwargs):
-        return self._global.debug(*args, **kwargs)
-
-    def info(self, *args, **kwargs):
-        return self._global.info(*args, **kwargs)
-
-    def warning(self, *args, **kwargs):
-        return self._global.warning(*args, **kwargs)
-
-    def error(self, *args, **kwargs):
-        return self._global.error(*args, **kwargs)
-
-    def critical(self, *args, **kwargs):
-        return self._global.critical(*args, **kwargs)
+            raise KeyError(f"There is no instances with name {key}")
 
     def add_handling_uncaught_expections(self, logger):
-
         def handle_exception(exc_type, exc_value, exc_traceback):
             if issubclass(exc_type, KeyboardInterrupt):
                 sys.__excepthook__(exc_type, exc_value, exc_traceback)
