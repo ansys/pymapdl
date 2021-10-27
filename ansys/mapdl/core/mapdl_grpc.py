a038bfb114f0dddc969bc2867549831925bcd6a6
German <germanmartinezayuso@gmail.com>
2021-10-27 18:58:49 +0200

    Added missig `log_apdl` argument passing.

--- ansys/mapdl/core/mapdl_grpc.py
+++ ansys/mapdl/core/mapdl_grpc.py
@@ -210,7 +210,7 @@ class MapdlGrpc(_MapdlCore):
         # port and ip are needed to setup the log
         self._port = port
         self._ip = ip
-        super().__init__(loglevel, log_file=log_file, **kwargs)
+        super().__init__(loglevel, log_file=log_file, log_apdl=log_apdl, ** kwargs)
 
         check_valid_ip(ip)
 
