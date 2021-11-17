f3aa12ddcee58675ba77e825b02326dd8a936d5f
German <germanmartinezayuso@gmail.com>
2021-11-17 12:30:05 +0100

    Added initial doc string.

---
+++ examples/06-verif-manual/vm-002-comosellame.py
@@ -0,0 +1,24 @@
+"""
+
+Statically Indeterminate Reaction Force Analysis
+------------------------------------------------
+
+
+Problem Description:
+ - A prismatical bar with built-in ends is loaded axially at two
+   intermediate cross sections.  Determine the reactions :math:`R_1`
+   and :math:`R_2`.
+Reference:
+ - S. Timoshenko, Strength of Materials, Part I, Elementary Theory and
+   Problems, 3rd Edition, D. Van Nostrand Co., Inc., New York, NY, 1955,
+   pg. 26, problem 10.
+Analysis Type(s):
+ - Static Analysis ``ANTYPE=0``
+Element Type(s):
+ - 3-D Spar (or Truss) Elements (LINK180)
+
+
+
+"""
+
+print('asdf')
