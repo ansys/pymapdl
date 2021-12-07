011c71e2d3132aa2b7934cfcdc8e83d97c8958d4
jgd10 <jamesgordonderrick@gmail.com>
2021-12-07 14:36:40 +0000

    Created api commands for mag, pres and temp
    Created equivalent pymapdl commands for the 3 scalar queries: MAG, PRES,
    and TEMP. Examples have only been added for one of the three and tests
    for none of them because my knowledge of APDL is not extensive enough.
    Further work will require external input.

---
+++ ansys/mapdl/core/inline_functions/scalar_queries.py
@@ -0,0 +1,101 @@
+from .core import _QueryExecution
+
+
+class _ScalarQueries(_QueryExecution):
+    _mapdl = None
+
+    def temp(self, n: int) -> float:
+        """Returns temperature at node ``n``.
+
+        Temperature at node ``n``.
+
+        Parameters
+        ----------
+        n : int
+            Node number
+
+        Returns
+        -------
+        float
+            Temperature
+
+        Examples
+        --------
+        This example has been adapted from the example script
+        :ref:`_ref_3d_plate_thermal`. We create a block
+        of solid material, and apply a temperature to one end
+        of the block, and then solve.
+
+        Then we can use ``queries.temp`` to get the temperature
+        at the first node on the deformed.
+
+        >>> from ansys.mapdl.core import launch_mapdl
+        >>> mapdl = launch_mapdl()
+        >>> mapdl.clear()
+        >>> mapdl.prep7()
+        >>> mapdl.mp("kxx", 1, 45)
+        >>> mapdl.et(1, 'SOLID70')
+        >>> mapdl.block(-0.3, 0.3, -0.46, 1.34, -0.2, -0.2 + 0.02)
+        >>> mapdl.esize(0.01)
+        >>> mapdl.vmesh(1)
+        >>> mapdl.asel("S", vmin=3)
+        >>> mapdl.nsla()
+        >>> mapdl.d("all", "temp", 5)
+        >>> mapdl.nsel('ALL')
+        >>> mapdl.run("/SOLU")
+        >>> mapdl.solve()
+        >>> mapdl.queries.temp(1)
+        4.99999994201101
+        """
+        return self._run_query(f"TEMP({n})", integer=False)
+
+    def pres(self, n: int) -> float:
+        """Returns pressure at node ``n``.
+
+        Pressure at node ``n``.
+
+        Parameters
+        ----------
+        n : int
+            Node number
+
+        Returns
+        -------
+        float
+            Pressure
+        """
+        return self._run_query(f"PRES({n})", integer=False)
+
+    def volt(self, n: int) -> float:
+        """Returns electric potential at node ``n``.
+
+        Electric potential at node ``n``.
+
+        Parameters
+        ----------
+        n : int
+            Node number
+
+        Returns
+        -------
+        float
+            Electric potential
+        """
+        return self._run_query(f"VOLT({n})", integer=False)
+
+    def mag(self, n: int) -> float:
+        """Returns magnetic scalar potential at node ``n``.
+
+        Magnetic scalar potential at node ``n``.
+
+        Parameters
+        ----------
+        n : int
+            Node number
+
+        Returns
+        -------
+        float
+            Magnetic scalar potential
+        """
+        return self._run_query(f"VOLT({n})", integer=False)
