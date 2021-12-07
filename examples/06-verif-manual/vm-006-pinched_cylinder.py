f9aae7eed8e33d395d4dd0e1346becc095fd2387
Roman Ilchenko <romailch1308@gmail.com>
2021-12-07 21:56:09 +0100

    VM6 Second Penultimate Commit

--- examples/06-verif-manual/vm-006-pinched_cylinder.py
+++ examples/06-verif-manual/vm-006-pinched_cylinder.py
@@ -1,54 +1,50 @@
 r"""
 .. _ref_vm6_example:
 
-Beam Stresses and Deflections
------------------------------
+Pinched Cylinder
+----------------
 Problem Description:
- - A standard 30 inch WF beam, with a cross-sectional area :math:`A`,
-   is supported as shown below and loaded on the overhangs by a
-   uniformly distributed load :math:`w`. Determine the maximum bending
-   stress, :math:`\sigma_max`, in the middle portion of the beam and
-   the deflection, :math:`\delta`, at the middle of the beam.
+ - A thin-walled cylinder is pinched by a force F at the middle
+   of the cylinder length. Determine the radial displacement :math:`\delta`
+   at the point where the force :math:`F` is applied.
+   The ends of the cylinder are free edges.
 
 Reference:
- -  S. Timoshenko, Strength of Material, Part I, Elementary Theory and
-   Problems, 3rd Edition, D. Van Nostrand Co., Inc., New York, NY, 1955,
-   pg. 98, problem 4.
+ - R. D. Cook, Concepts and Applications of Finite Element Analysis, 2nd Edition,
+   John Wiley and Sons, Inc., New York, NY, 1981, pp. 284-287.
+   H. Takemoto, R. D. Cook, "Some Modifications of an Isoparametric Shell
+   Element", International Journal for Numerical Methods in Engineering, Vol.7
+   No. 3, 1973.
 
 Analysis Type(s):
  - Static Analysis ``ANTYPE=0``
 
 Element Type(s):
- - 3-D 2 Node Beam (BEAM188)
+ - 4-Node Finite Strain Shell Elements (SHELL181)
+ - 8-Node Finite Strain Shell Elements (SHELL281)
 
-.. image:: ../../_static/vm2_setup.png
+.. image:: ../../_static/vm6_setup.png
    :width: 400
-   :alt: VM2 Problem Sketch
+   :alt: VM6 Pinched Cylinder Problem Sketch
 
 Material Properties
- - :math:`E = 30 \cdot 10^6 psi`
+ - :math:`E = 10.5 \cdot 10^6 psi`
+ :math:`Nu = 0.3125`
 
 Geometric Properties:
- - :math:`a = 120 in`
- - :math:`l = 240 in`
- - :math:`h = 30 in`
- - :math:`A = 50.65 in^2`
- - :math:`I_z = 7892 in^4`
+ - :math:`l = 10.35  in`
+ - :math:`r = 4.953  in`
+ - :math:`t = 0.094  in`
 
 Loading:
- - :math:`w = (10000/12) lb/in`
+ - :math:`F = 100  lb`
 
-Analytical Equations:
- - :math:`M` is the bending moment for the middle portion of the beam:
-   :math:`M = 10000 \cdot 10 \cdot 60 = 6 \cdot 10^6 lb \cdot in`
- - Determination of the maximum stress in the middle portion of the beam is:
-   :math:`\sigma_max = \frac{M h}{2 I_z}`
- - The deflection, :math:`\delta`, at the middle of the beam can be defined
-   by the formulas of the transversally loaded beam:
-   :math:`\delta = 0.182 in`
+Analysis Assumptions and Modeling Notes:
+ - A one-eighth symmetry model is used. One-fourth of the load is applied
+   due to symmetry.
 
 """
-# sphinx_gallery_thumbnail_path = '_static/vm2_setup.png'
+# sphinx_gallery_thumbnail_path = '_static/vm6_setup.png'
 
 ###############################################################################
 # Start MAPDL
@@ -68,23 +64,23 @@ mapdl.prep7()
 ###############################################################################
 # Define Element Type
 # ~~~~~~~~~~~~~~~~~~~
-# Set up the element type (a beam-type).
+# Set up the element type (a shell-type).
 
 # Type of analysis: Static.
 mapdl.antype("STATIC")
 
-# Element type: BEAM188.
-mapdl.et(1, "SHELL181")
+# Define element number.
+elem_num = 1
 
-# Special Features are defined by keyoptions of beam element:
+# Element type: SHELL181.
+mapdl.et(elem_num, "SHELL181")
+
+# Special Features are defined by keyoptions of shell element:
 
 # KEYOPT(3)
 # Integration option:
 # Full integration with incompatible modes.
-mapdl.keyopt(1, 3, 2)  # Cubic shape function
-
-
-###############################################################################
+mapdl.keyopt(elem_num, 3, 2)  # Cubic shape function
 
 
 ###############################################################################
@@ -92,234 +88,282 @@ mapdl.keyopt(1, 3, 2)  # Cubic shape function
 # ~~~~~~~~~~~~~~~
 # Set up the material.
 
-mapdl.mp("EX", 1, 10.5E6)
-mapdl.mp("NUXY", 1, 0.3125)
+# Define material number.
+mat_num = 1
+
+# Define material properties.
+mapdl.mp("EX", mat_num, 10.5E6)
+mapdl.mp("NUXY", mat_num, 0.3125)
 print(mapdl.mplist())
 
 
 ###############################################################################
 # Define Section
 # ~~~~~~~~~~~~~~
-# Set up the cross-section properties for a beam element.
+# Set up the cross-section properties for a shell element.
 
-w_f = 1.048394965
-w_w = 0.6856481
+# Parametrization of the shell thickness.
+t = 0.094
+
+# Define cross-section number.
 sec_num = 1
-mapdl.sectype(secid=sec_num, type_="SHELL", name='shell181')
-mapdl.secdata(0.094,1,0,5)
+
+# Define shell cross-section.
+mapdl.sectype(secid=sec_num, type_="SHELL", name="shell181")
+mapdl.secdata(t, mat_num, 0, 5)
+
+
+###############################################################################
+# Define Geometry
+# ~~~~~~~~~~~~~~~
+# Set up the keypoints and area. Define line division of the lines, then
+# mesh the area with shell elements.
+
+# Define global cylindrical coordinate system.
+mapdl.csys(1)
+
+# Define keypoints by coordinates.
+mapdl.k(1, 4.953)
+mapdl.k(2, 4.953, "", 5.175)
+
+# Generates additional keypoints from a pattern of keypoints.
+mapdl.kgen(2, 1, 2, 1, "", 90)
+
+# Create an area through keypoints.
+mapdl.a(1, 2, 4, 3)
+
+# Specify the default number of line divisions.
+mapdl.esize("", 8)
+
+# Mesh the area.
+mapdl.amesh(1)
+
+# Define global cartesian coordinate system.
+mapdl.csys(0)
+
+
+###############################################################################
+# Define Boundary Conditions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Application of boundary conditions (BC).
+
+# Select nodes by location and apply BC.
+mapdl.nsel("S", "LOC", "X", 0)
+mapdl.dsym("SYMM", "X", 0)
+mapdl.nsel("S", "LOC", "Y", 0)
+mapdl.dsym("SYMM", "Y", 0)
+mapdl.nsel("S", "LOC", "Z", 0)
+mapdl.dsym("SYMM", "Z", 0)
+mapdl.nsel("ALL")
+
+
+###############################################################################
+# Define Distributed Loads
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# Apply the force of :math:`F = (100/4) lb` in the y-direction.
+
+# Parametrization of the :math:`F` load for the quarter of the model.
+F = 100/4
+
+# Application of the load to the model.
+mapdl.fk(3, "FY", -F)
+mapdl.finish()
+
+
+###############################################################################
+# Solve
+# ~~~~~
+# Enter solution mode and solve the system. Print the solver output.
+
+mapdl.run("/SOLU")
+out = mapdl.solve()
+mapdl.finish()
+print(out)
 
 #
 # ###############################################################################
-# CSYS,1
-# K,1,4.953 ! DEFINE MODEL GEOMETRY
-# K,2,4.953,,5.175
-# KGEN,2,1,2,1,,90
-# A,1,2,4,3
-# ESIZE,,8
-# AMESH,1
-# CSYS,0
-# ###############################################################################
-#
-#
-# ###############################################################################
-# NSEL,S,LOC,X,0
-# DSYM,SYMM,X,0
-# NSEL,S,LOC,Y,0
-# DSYM,SYMM,Y,0
-# NSEL,S,LOC,Z,0
-# DSYM,SYMM,Z,0
-# NSEL,ALL
-# ###############################################################################
-#
-#
-# ###############################################################################
-# FK,3,FY,-25
-# FINISH
-# ###############################################################################
-#
-#
-# ###############################################################################
-# /SOLU
-# SOLVE
-# FINISH
-# ###############################################################################
-#
-#
-# ###############################################################################
-# /POST1
-# NSEL,S,LOC,Y,4.953 ! SELECT NODE AT LOAD APPLICATION
-# NSEL,R,LOC,Z,0
-# NSEL,R,LOC,X,0
-# PRNSOL,U,COMP ! PRINT DISPLACEMENTS AND VECTOR SUM
-# ###############################################################################
-#
-#
-# ###############################################################################
-# TOP_NODE = NODE (4.953,90,0)
-# ###############################################################################
-#
-#
-# ###############################################################################
-# *GET,DISP,NODE,TOP_NODE,U,Y
-# ###############################################################################
-#
-#
-# ###############################################################################
-# *DIM,LABEL,CHAR,1
-# LABEL(1) = 'DEF_IN'
-# ###############################################################################
-#
-#
-# ###############################################################################
-# *DIM,VALUE,,1,3
-# *VFILL,VALUE(1,1),DATA,0.1139
-# *VFILL,VALUE(1,2),DATA,ABS(DISP)
-# *VFILL,VALUE(1,3),DATA,ABS( DISP /0.1139 )
-# ###############################################################################
-#
-#
-# ###############################################################################
-# SAVE,TABLE_1
-# FINISH
-# ###############################################################################
+# # Post-processing
+# # ~~~~~~~~~~~~~~~
 #
 #
 #
+# mapdl.run("/POST1")
 #
+# mapdl.nsel("S", "LOC", "Y", 4.953)  # SELECT NODE AT LOAD APPLICATION
 #
+# mapdl.nsel("R", "LOC", "Z", 0)
 #
-# ###############################################################################
-# ! VM6 with element SHELL281
-# /CLEAR,NOSTART
-# /TITLE, VM6, PINCHED CYLINDER
-# C*** USING SHELL281 ELEMENTS
-# ###############################################################################
+# mapdl.nsel("R", "LOC", "X", 0)
 #
+# mapdl.prnsol("U", "COMP")  # PRINT DISPLACEMENTS AND VECTOR SUM
 #
-# ###############################################################################
-# /PREP7
-# ANTYPE,STATIC
-# ET,1,SHELL281
-# SECTYPE,1,SHELL
-# SECDATA,0.094,1,0,5
-# ###############################################################################
+# mapdl.run("TOP_NODE = NODE (4.953,90,0)")
 #
+# mapdl.get("DISP", "NODE", "TOP_NODE", "U", "Y")
 #
-# ###############################################################################
-# MP,EX,,10.5E6
-# MP,NUXY,,.3125
-# ###############################################################################
+# mapdl.run("*DIM,LABEL,CHAR,1")
 #
+# mapdl.run("*DIM,VALUE,,1,3")
 #
-# ###############################################################################
-# CSYS,1
-# K,1,4.953 ! DEFINE MODEL GEOMETRY
-# K,2,4.953,,5.175
-# KGEN,2,1,2,1,,90
-# A,1,2,4,3
-# ESIZE,,8
-# AMESH,1
-# CSYS,0
-# ###############################################################################
+# mapdl.run("LABEL(1) = 'DEF_IN'")
 #
+# mapdl.run("*VFILL,VALUE(1,1),DATA,0.1139")
 #
-# ###############################################################################
-# NSEL,S,LOC,X,0
-# DSYM,SYMM,X,0
-# NSEL,S,LOC,Y,0
-# DSYM,SYMM,Y,0
-# NSEL,S,LOC,Z,0
-# DSYM,SYMM,Z,0
-# NSEL,ALL
-# ###############################################################################
+# mapdl.run("*VFILL,VALUE(1,2),DATA,ABS(DISP)")
 #
+# mapdl.run("*VFILL,VALUE(1,3),DATA,ABS( DISP /0.1139 )")
 #
-# ###############################################################################
-# FK,3,FY,-25
-# FINISH
-# ###############################################################################
+# mapdl.save("TABLE_1")
 #
+# mapdl.finish()
 #
-# ###############################################################################
-# /SOLU
-# SOLVE
-# FINISH
-# ###############################################################################
+# mapdl.run("/CLEAR,NOSTART")
 #
+# mapdl.title("VM6 PINCHED CYLINDER")
 #
-# ###############################################################################
-# /POST1
-# NSEL,S,LOC,Y,4.953 ! SELECT NODE AT LOAD APPLICATION
-# NSEL,R,LOC,Z,0
-# NSEL,R,LOC,X,0
-# PRNSOL,U,COMP ! PRINT DISPLACEMENTS AND VECTOR SUM
-# ###############################################################################
+# mapdl.run("C*** USING SHELL281 ELEMENTS")
 #
+# mapdl.prep7()
 #
-# ###############################################################################
-# TOP_NODE = NODE (4.953,90,0)
-# ###############################################################################
+# mapdl.run("SMRT,OFF")
 #
+# mapdl.antype("STATIC")
 #
-# ###############################################################################
-# *GET,DISP,NODE,TOP_NODE,U,Y
-# ###############################################################################
+# mapdl.et(1, "SHELL281")
 #
+# mapdl.sectype(1, "SHELL")
 #
-# ###############################################################################
-# *DIM,LABEL,CHAR,1
-# LABEL(1) = 'DEF_IN'
-# ###############################################################################
+# mapdl.secdata(0.094, 1, 0, 5)
 #
+# mapdl.mp("EX", "", 10.5E6)
 #
-# ###############################################################################
-# *DIM,VALUE,,1,3
-# *VFILL,VALUE(1,1),DATA,0.1139
-# *VFILL,VALUE(1,2),DATA,ABS(DISP)
-# *VFILL,VALUE(1,3),DATA,ABS( DISP /0.1139 )
-# SAVE,TABLE_2
-# ###############################################################################
+# mapdl.mp("NUXY", "", .3125)
 #
+# mapdl.csys(1)
 #
-# ###############################################################################
-# RESUME,TABLE_1
-# ###############################################################################
+# mapdl.k(1, 4.953)  # DEFINE MODEL GEOMETRY
 #
+# mapdl.k(2, 4.953, "", 5.175)
 #
-# ###############################################################################
-# /COM
-# /OUT,vm6,vrt
-# /COM,------------------- VM6 RESULTS COMPARISON ---------------------
-# /COM,
-# /COM, | TARGET | Mechanical APDL | RATIO
-# /COM,
-# /COM,
-# /COM,
-# /COM,SHELL181
-# /COM,
-# *VWRITE,LABEL(1),VALUE(1,1),VALUE(1,2),VALUE(1,3)
-# (1X,A10,' ',F10.4,' ',F14.4,' ',1F13.3)
-# /NOPR
-# RESUME,TABLE_2
-# /GOPR
-# /COM,
-# /COM,SHELL281
-# /COM,
-# *VWRITE,LABEL(1),VALUE(1,1),VALUE(1,2),VALUE(1,3)
-# (1X,A10,' ',F10.4,' ',F14.4,' ',1F13.3)
-# /COM,-----------------------------------------------------------------
-# /OUT
-# FINISH
-# ###############################################################################
+# mapdl.kgen(2, 1, 2, 1, "", 90)
 #
+# mapdl.a(1, 2, 4, 3)
 #
-# ###############################################################################
-# /DEL,TABLE_1
-# /DEL,TABLE_2
-# FINISH
-# ###############################################################################
+# mapdl.esize("", 8)
 #
+# mapdl.amesh(1)
 #
-# ###############################################################################
-# *LIST,vm6,vrt
-# ###############################################################################
+# mapdl.csys(0)
+#
+# mapdl.nsel("S", "LOC", "X", 0)
+#
+# mapdl.dsym("SYMM", "X", 0)
+#
+# mapdl.nsel("S", "LOC", "Y", 0)
+#
+# mapdl.dsym("SYMM", "Y", 0)
+#
+# mapdl.nsel("S", "LOC", "Z", 0)
+#
+# mapdl.dsym("SYMM", "Z", 0)
+#
+# mapdl.nsel("ALL")
+#
+# mapdl.fk(3, "FY", -25)
+#
+# mapdl.finish()
+#
+# mapdl.run("/SOLU")
+#
+# mapdl.solve()
+#
+# mapdl.finish()
+#
+# mapdl.run("/POST1")
+#
+# mapdl.nsel("S", "LOC", "Y", 4.953)  # SELECT NODE AT LOAD APPLICATION
+#
+# mapdl.nsel("R", "LOC", "Z", 0)
+#
+# mapdl.nsel("R", "LOC", "X", 0)
+#
+# mapdl.prnsol("U", "COMP")  # PRINT DISPLACEMENTS AND VECTOR SUM
+#
+# mapdl.run("TOP_NODE = NODE (4.953,90,0)")
+#
+# mapdl.get("DISP", "NODE", "TOP_NODE", "U", "Y")
+#
+# mapdl.run("*DIM,LABEL,CHAR,1")
+#
+# mapdl.run("*DIM,VALUE,,1,3")
+#
+# mapdl.run("LABEL(1) = 'DEF_IN'")
+#
+# mapdl.run("*VFILL,VALUE(1,1),DATA,0.1139")
+#
+# mapdl.run("*VFILL,VALUE(1,2),DATA,ABS(DISP)")
+#
+# mapdl.run("*VFILL,VALUE(1,3),DATA,ABS( DISP /0.1139 )")
+#
+# mapdl.save("TABLE_2")
+#
+# mapdl.resume("TABLE_1")
+#
+# mapdl.run("/COM")
+#
+# with mapdl.non_interactive:
+#     mapdl.run("/OUT,vm6,vrt")
+#
+#     mapdl.run("/COM,------------------- VM6 RESULTS COMPARISON ---------------------")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("/COM, | TARGET | Mechanical APDL | RATIO")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("/COM,SHELL181")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("*VWRITE,LABEL(1),VALUE(1,1),VALUE(1,2),VALUE(1,3)")
+#
+#     mapdl.run("(1X,A10,' ',F10.4,' ',F14.4,' ',1F13.3)")
+#
+#     mapdl._run("/NOPR")  # It is not recommended to use '/NOPR' in a normal PyMAPDL session.
+#
+#     mapdl.resume("TABLE_2")
+#
+#     mapdl.run("/GOPR")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("/COM,SHELL281")
+#
+#     mapdl.run("/COM,")
+#
+#     mapdl.run("*VWRITE,LABEL(1),VALUE(1,1),VALUE(1,2),VALUE(1,3)")
+#
+#     mapdl.run("(1X,A10,' ',F10.4,' ',F14.4,' ',1F13.3)")
+#
+#     mapdl.run("/COM,-----------------------------------------------------------------")
+#
+# mapdl.run("/OUT")
+#
+# mapdl.run("/GOPR")
+#
+# mapdl.finish()
+#
+# mapdl.run("/DEL,TABLE_1")
+#
+# mapdl.run("/DEL,TABLE_2")
+#
+# mapdl.finish()
+#
+# mapdl.run("*LIST,vm6,vrt")
+#
+# mapdl.exit()
