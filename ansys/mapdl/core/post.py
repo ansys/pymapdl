4af9b0966b7ff71e313169289f51e08daa77d0c8
Alex Kaszynski <akascap@gmail.com>
2021-12-17 11:00:29 -0500

    Update ansys/mapdl/core/post.py

--- ansys/mapdl/core/post.py
+++ ansys/mapdl/core/post.py
@@ -170,7 +170,7 @@ class PostProcessing:
          75.03939292229019,
          75.20949687626468]
         """
-        self._mapdl.post1()
+        self._mapdl.post1(mute=True)
         list_rsp = self._mapdl.set("LIST")
         return np.genfromtxt(list_rsp.splitlines(), skip_header=3)[:, 1]
 
