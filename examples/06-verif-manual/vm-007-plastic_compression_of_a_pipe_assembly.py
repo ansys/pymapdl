33aa033409178295378176663155dabed03e7693
Roman Ilchenko <romailch1308@gmail.com>
2021-12-30 18:10:22 +0100

    Added updated all vm7

--- examples/06-verif-manual/vm-007-plastic_compression_of_a_pipe_assembly.py
+++ examples/06-verif-manual/vm-007-plastic_compression_of_a_pipe_assembly.py
@@ -27,6 +27,10 @@ Element Type(s):
    :width: 400
    :alt: VM7 Problem Sketch
 
+.. image:: ../../_static/vm7_setup_2.png
+   :width: 400
+   :alt: VM7 Finite Element Models
+
 Material Properties
  -
 
@@ -38,8 +42,23 @@ Loading:
  - 2nd Load Step: :math:`\delta = 0.050 in`
  - 3rd Load Step: :math:`\delta = 0.100 in`
 
-Analytical Equations:
- -
+Analysis Assumptions and Modeling Notes:
+ - The following tube dimensions, which provide the desired cross-sectional areas,
+   are arbitrarily chosen. Inner (steel) tube: inside radius = 1.9781692 in.,
+   wall thickness = 0.5 in. Outer (aluminum) tube: inside radius = 3.5697185 in.,
+   wall thickness = 0.5 in.
+
+ - The problem can be solved in three ways:
+   • using PIPE288 - the plastic straight pipe element
+   • using SOLID185 - the 3-D structural solid element
+   • using SHELL181 - the 4-Node Finite Strain Shell
+
+ - In the SOLID185 and SHELL181 cases, since the problem is axisymmetric, only a one
+   element -sector is modeled. A small angle  = 6° is arbitrarily chosen to
+   reasonably approximate the circular boundary with straight sided elements.
+   The nodes at the boundaries have the UX (radial) degree of freedom coupled.
+   In the SHELL181 model, the nodes at the boundaries additionally
+   have the ROTY degree of freedom coupled.
 
 """
 # sphinx_gallery_thumbnail_path = '_static/vm7_setup.png'
@@ -64,54 +83,91 @@ def start_prep7():
     mapdl.verify()
     mapdl.prep7()
 
-# Call the function.
+
 start_prep7()
+
+
+###############################################################################
+# Parameterization
+# ~~~~~~~~~~~~~~~~
+
+# Angle of the model sector.
+theta = 6
+
+# Deflection load steps.
+defl_ls1 = -0.032
+defl_ls2 = -0.05
+defl_ls3 = -0.1
+
+# Quantity of the load steps.
+ls = 3
 
 
 ###############################################################################
 # Define Element Type
 # ~~~~~~~~~~~~~~~~~~~
-# Set up the element types (as shell-type).
+# Set up the element types .
 
-#
-mapdl.et(1, "PIPE288", "", "", "", 2)
+# Element type PIPE288.
+mapdl.et(1, "PIPE288")
 
-# Special Features are defined by keyoptions of shell element:
-# KEYOPT(3) # Integration option:
-# Full integration with incompatible modes.
-mapdl.keyopt(elem_num, 3, 2)  # Cubic shape function
+# Special Features are defined by keyoptions of shell element.
+# KEYOPT(4)(2)
+# Hoop strain treatment:
+# Thick pipe theory.
+mapdl.keyopt(1, 4, 2)  # Cubic shape function
 
-#
+# Element type SOLID185.
 mapdl.et(2, "SOLID185")
 
-#
-mapdl.et(3, "SHELL181", "", "", 2)  # FULL INTEGRATION
+# Element type SHELL181.
+mapdl.et(3, "SHELL181")  # FULL INTEGRATION
+
+# Special Features are defined by keyoptions of shell element.
+# KEYOPT(3)(2)
+# Integration option:
+# Full integration with incompatible modes.
+mapdl.keyopt(3, 3, 2)
 
 
 ###############################################################################
 # Define Material
 # ~~~~~~~~~~~~~~~
 # Set up the material properties, where:
-# Young Modulus is :math:`E = 10.5 \cdot 10^6 psi`,
-# Poisson's ratio is :math:`Nu = 0.3125`.
+# Young Modulus is :math:`E = ... \cdot 10^6 psi`,
+# Poisson's ratio is :math:`Nu = ...`.
 
-# STEEL
-# Define Young's moulus and Poisson ratio for Aluminium.
+# STEEL material model.
+# Define Young's moulus and Poisson ratio for Steel.
 mapdl.mp("EX", 1, 26.875E6)
 mapdl.mp("PRXY", 1, 0.3)
-# Define non-linear material properties for aluminium.
+
+# Define non-linear material properties for Steel.
 mapdl.tb("BKIN", 1, 1)
 mapdl.tbtemp(0)
 mapdl.tbdata(1, 86000, 0)
 
-# ALUMINUM
+
+# ALUMINUM material model.
 # Define Young's moulus and Poisson ratio for Aluminium.
 mapdl.mp("EX", 2, 11E6)
 mapdl.mp("PRXY", 2, 0.3)
-# Define non-linear material properties for aluminium.
+
+# Define non-linear material properties for Aluminium.
 mapdl.tb("BKIN", 2, 1)
 mapdl.tbtemp(0)
 mapdl.tbdata(1, 55000, 0)
+
+# Plot material behaviour diagram
+
+import matplotlib.pyplot as plt
+
+
+x1 = [0, 0.032, 0.1, 0.2]
+x2 = [0, 0.005, 0.1, 0.2]
+
+y1 = [0, 86000, 86000, 86000]
+y2 = [0, 55000, 55000, 55000]
 
 
 ###############################################################################
@@ -119,18 +175,32 @@ mapdl.tbdata(1, 55000, 0)
 # ~~~~~~~~~~~~~~
 # Set up the cross-section properties for a shell element.
 
-
+# Shell cross-section for inside (STEEL) tube.
 mapdl.sectype(1, "SHELL")
-mapdl.secdata(0.5, 1, 0, 5)  # THICKNESS (SHELL181)
 
+# THICKNESS (SHELL181)
+mapdl.secdata(0.5, 1, 0, 5)
+
+
+# Shell cross-section for outside (ALUMINUM) tube.
 mapdl.sectype(2, "SHELL")
-mapdl.secdata(0.5, 2, 0, 5)  # THICKNESS (SHELL181)
 
+# THICKNESS (SHELL181)
+mapdl.secdata(0.5, 2, 0, 5)
+
+
+# Define Pipe cross-section for inside (STEEL) tube, where:
 mapdl.sectype(3, "PIPE")
-mapdl.secdata(4.9563384, 0.5)  # OUTSIDE DIA. AND WALL THICKNESS FOR INSIDE TUBE (PIPE288)
 
+# OUTSIDE DIA. AND WALL THICKNESS FOR INSIDE TUBE (PIPE288)
+mapdl.secdata(4.9563384, 0.5)
+
+
+# Pipe cross-section for outside (ALUMINUM) tube.
 mapdl.sectype(4, "PIPE")
-mapdl.secdata(8.139437, 0.5)  # OUTSIDE DIA. AND WALL THICKNESS FOR OUTSIDE TUBE (PIPE288)
+
+# OUTSIDE DIA. AND WALL THICKNESS FOR OUTSIDE TUBE (PIPE288)
+mapdl.secdata(8.139437, 0.5)
 
 
 ###############################################################################
@@ -138,50 +208,67 @@ mapdl.secdata(8.139437, 0.5)  # OUTSIDE DIA. AND WALL THICKNESS FOR OUTSIDE TUBE
 # ~~~~~~~~~~~~~~~
 # Set up the keypoints and create the area through the keypoints.
 
-mapdl.n(1)  # GENERATE NODES AND ELEMENTS FOR PIPE288
-mapdl.n(2, "", "", 10)
+# GENERATE NODES AND ELEMENTS FOR PIPE288
+mapdl.n(1, x=0, y=0, z=0)
+mapdl.n(2, x=0, y=0, z=10)
 
+# Create element for steel(inside) tube cross-section.
 mapdl.mat(1)
-mapdl.secnum(3)  # STEEL (INSIDE) TUBE
+mapdl.secnum(3)
 mapdl.e(1, 2)
 
+# Create element for aluminium(outside) tube cross-section.
 mapdl.mat(2)
-mapdl.secnum(4)  # ALUMINUM (OUTSIDE) TUBE
+mapdl.secnum(4)
 mapdl.e(1, 2)
 
+# Activate the global cylindrical coordinate system.
 mapdl.csys(1)
 
-mapdl.n(101, 1.9781692)  # GENERATE NODES AND ELEMENTS FOR SOLID185
-mapdl.n(102, 2.4781692)
-mapdl.n(103, 3.5697185)
-mapdl.n(104, 4.0697185)
-mapdl.n(105, 1.9781692, "", 10)
-mapdl.n(106, 2.4781692, "", 10)
-mapdl.n(107, 3.5697185, "", 10)
-mapdl.n(108, 4.0697185, "", 10)
-mapdl.ngen(2, 10, 101, 108, "", "", "THETA")  # GENERATE 2ND SET OF NODES TO FORM A THETA DEGREE SLICE
-mapdl.nrotat(101, 118, 1)
+# GENERATE NODES AND ELEMENTS FOR SOLID185
+mapdl.n(node=101, x=1.9781692)
+mapdl.n(node=102, x=2.4781692)
+mapdl.n(node=103, x=3.5697185)
+mapdl.n(node=104, x=4.0697185)
+mapdl.n(node=105, x=1.9781692, z=10)
+mapdl.n(node=106, x=2.4781692, z=10)
+mapdl.n(node=107, x=3.5697185, z=10)
+mapdl.n(node=108, x=4.0697185, z=10)
+mapdl.ngen(itime=2, inc=10, node1=101, node2=108, dy=theta)  # GENERATE 2ND SET OF NODES TO FORM A THETA DEGREE SLICE
+mapdl.nrotat(node1=101, node2=118, ninc=1)
 
+# INSIDE (STEEL) TUBE
 mapdl.type(2)
-mapdl.mat(1)  # INSIDE (STEEL) TUBE
+mapdl.mat(1)
 mapdl.e(101, 102, 112, 111, 105, 106, 116, 115)
 
-
-mapdl.mat(2)  # OUTSIDE (ALUMINUM) TUBE
+# OUTSIDE (ALUMINUM) TUBE
+mapdl.mat(2)
 mapdl.e(103, 104, 114, 113, 107, 108, 118, 117)
 
-mapdl.n(201, 2.2281692)  # GENERATE NODES AND ELEMENTS FOR SHELL181
-mapdl.n(203, 2.2281692, "", 10)
-mapdl.n(202, 3.8197185)
-mapdl.n(204, 3.8197185, "", 10)
-mapdl.ngen(2, 4, 201, 204, "", "", "THETA")  # GENERATE NODES TO FORM A THETA DEGREE SLICE
+# GENERATE NODES AND ELEMENTS FOR SHELL181
+mapdl.n(node=201, x=2.2281692)
+mapdl.n(node=203, x=2.2281692, z=10)
+mapdl.n(node=202, x=3.8197185)
+mapdl.n(node=204, x=3.8197185, z=10)
 
+# GENERATE NODES TO FORM A THETA DEGREE SLICE
+mapdl.ngen(itime=2, inc=4, node1=201, node2=204, dy=theta)
+
+# INSIDE (STEEL) TUBE
 mapdl.type(3)
-mapdl.secnum(1)  # INSIDE (STEEL) TUBE
+mapdl.secnum(1)
 mapdl.e(203, 201, 205, 207)
 
-mapdl.secnum(2)  # OUTSIDE (ALUMINUM) TUBE
+# OUTSIDE (ALUMINUM) TUBE
+mapdl.secnum(2)
 mapdl.e(204, 202, 206, 208)
+
+# plot elements
+cpos = [(19.67899462804619, 17.856836088414664, 22.644135378046194),
+        (2.03485925, 0.21270071036846988, 5.0),
+        (0.0, 0.0, 1.0)]
+mapdl.eplot(cpos=cpos)
 
 
 ###############################################################################
@@ -191,25 +278,25 @@ mapdl.e(204, 202, 206, 208)
 
 mapdl.run("C*** APPLY CONSTRAINTS TO PIPE288 MODEL")
 
-mapdl.d(1, "ALL")  # FIX ALL DOFS FOR BOTTOM END OF PIPE288
-mapdl.d(2, "UX", "", "", "", "", "UY", "ROTX", "ROTY", "ROTZ")  # ALLOW ONLY UZ DOF AT TOP END OF PIPE288 MODEL
+mapdl.d(node=1, lab="ALL")  # FIX ALL DOFS FOR BOTTOM END OF PIPE288
+mapdl.d(node=2, lab="UX", lab2="UY", lab3="ROTX", lab4="ROTY", lab5="ROTZ")  # ALLOW ONLY UZ DOF AT TOP END OF PIPE288 MODEL
 
 mapdl.run("C*** APPLY CONSTRAINTS TO SOLID185 AND SHELL181 MODELS")
 
-mapdl.cp(1, "UX", 101, 111, 105, 115)  # COUPLE NODES AT BOUNDARY IN RADIAL DIR FOR SOLID185
-mapdl.cpsgen(4, "", 1)
+mapdl.cp(nset=1, lab="UX", node1=101, node2=111, node3=105, node4=115)  # COUPLE NODES AT BOUNDARY IN RADIAL DIR FOR SOLID185
+mapdl.cpsgen(itime=4, nset1=1)
 
-mapdl.cp(5, "UX", 201, 205, 203, 20)  # COUPLE NODES AT BOUNDARY IN RADIAL DIR FOR SHELL181
-mapdl.cpsgen(2, "", 5)
+mapdl.cp(5, lab="UX", node1=201, node2=205, node3=203, node4=20)  # COUPLE NODES AT BOUNDARY IN RADIAL DIR FOR SHELL181
+mapdl.cpsgen(itime=2, nset1=5)
 
-mapdl.cp(7, "ROTY", 201, 205)  # COUPLE NODES AT BOUNDARY IN ROTY DIR FOR SHELL181
-mapdl.cpsgen(4, "", 7)
+mapdl.cp(7, lab="ROTY", node1=201, node2=205)  # COUPLE NODES AT BOUNDARY IN ROTY DIR FOR SHELL181
+mapdl.cpsgen(itime=4, nset1=7)
 
 mapdl.nsel("S", "NODE", "", 101, 212)  # SELECT ONLY NODES IN SOLID185 AND SHELL181 MODELS
 mapdl.nsel("R", "LOC", "Y", 0)  # SELECT NODES AT THETA = 0 FROM THE SELECTED SET
 mapdl.dsym("SYMM", "Y", 1)  # APPLY SYMMETRY BOUNDARY CONDITIONS
 mapdl.nsel("S", "NODE", "", 101, 212)  # SELECT ONLY NODES IN SOLID185 AND SHELL181 MODELS
-mapdl.nsel("R", "LOC", "Y", "THETA")  # SELECT NODES AT THETA FROM THE SELECTED SET
+mapdl.nsel("R", "LOC", "Y", theta)  # SELECT NODES AT THETA FROM THE SELECTED SET
 
 mapdl.dsym("SYMM", "Y", 1)  # APPLY SYMMETRY BOUNDARY CONDITIONS
 
@@ -221,179 +308,115 @@ mapdl.d("ALL", "UZ", 0)  # CONSTRAIN BOTTOM NODES IN Z DIRECTION
 mapdl.nsel("ALL")
 mapdl.finish()
 
-
 ###############################################################################
 # Solve
 # ~~~~~
 # Enter solution mode and solve the system. Print the solver output.
 
+# Start solution presederu
+mapdl.run("/SOLU")
 
-# mapdl.run("/SOLU")
-#
-# mapdl.outpr("BASIC", "LAST")  # PRINT BASIC SOLUTION AT END OF LOAD STEP
-#
-# mapdl.run("C*** APPLY DISPLACEMENT LOADS TO ALL MODELS")
-#
-# # insted
-# def DISP(ARG1='', ARG2='', ARG3='', ARG4='', ARG5='', ARG6='', ARG7='', ARG8='', ARG9='', ARG10='', ARG11='', ARG12='',
-#          ARG13='', ARG14='', ARG15='', ARG16='', ARG17='', ARG18=''):
-#
-#     mapdl.nsel("R", "LOC", "Z", 10)  # SELECT NODES AT Z = 10 TO APPLY DISPLACEMENT
-#
-#     mapdl.d("ALL", "UZ", ARG1)
-#
-#     mapdl.nsel("ALL")
-#
-#     with mapdl.non_interactive:
-#
-#         mapdl.run("/OUT,SCRATCH")
-#
-#         mapdl.solve()
-#
-# DISP(-.032)
-#
-# DISP(-.05)
-#
-# DISP(-.1)
-#
-# mapdl.finish()
-#
-#
-##############################################################################
+# Define solution function.
+def solution(deflect=None):
+    mapdl.nsel("R", "LOC", "Z", 10)
+    mapdl.d(node="ALL", lab="UZ", value=deflect)
+    mapdl.nsel("ALL")
+    mapdl.solve()
+
+
+# Run each load step to reproduce needed deflection subsequently.
+# Load Step 1
+solution(deflect=defl_ls1)
+
+# Load Step 2
+solution(deflect=defl_ls2)
+
+# Load Step 3
+solution(deflect=defl_ls3)
+mapdl.finish()
+
+
+###############################################################################
 # Post-processing
 # ~~~~~~~~~~~~~~~
-# Enter post-processing for the model with elements ``shell181``.
-# Plotting nodal displacement.
-# Get the the radial displacement at the node where force F is applied.
+# Enter post-processing.
 
-# with mapdl.non_interactive:
-#
-#     mapdl.run("/OUT,")
-#
-#     mapdl.run("/POST1")
-#
-#     mapdl.run("C*** CREATE MACRO TO GET RESULTS FOR EACH MODEL")
-#
-#
-# def GETLOAD(ARG1='', ARG2='', ARG3='', ARG4='', ARG5='', ARG6='',            ARG7='', ARG8='', ARG9='', ARG10='', ARG11='', ARG12='',            ARG13='', ARG14='', ARG15='', ARG16='', ARG17='', ARG18=''):
-#
-#         mapdl.nsel("S", "NODE", "", 1, 2)  # SELECT NODES IN PIPE288 MODEL
-#
-#         mapdl.nsel("R", "LOC", "Z", 0)
-#
-#         mapdl.run("/OUT,SCRATCH")
-#
-#         mapdl.fsum()  # FZ IS TOTAL LOAD FOR PIPE288 MODEL
-#
-#         mapdl.run("*GET,LOAD_288,FSUM,,ITEM,FZ")
-#
-#         mapdl.nsel("S", "NODE", "", 101, 118)  # SELECT NODES IN SOLID185 MODEL
-#
-#         mapdl.nsel("R", "LOC", "Z", 0)
-#
-#         mapdl.fsum()
-#
-#         mapdl.run("*GET,ZFRC,FSUM,0,ITEM,FZ")
-#
-#         mapdl.run("LOAD=ZFRC*360/THETA                 ")  # MULTIPLY BY 360/THETA FOR FULL 360 DEGREE RESULTS
-#
-#         mapdl.run("*STATUS,LOAD")
-#
-#         mapdl.run("LOAD_185 = LOAD")
-#
-#         mapdl.nsel("S", "NODE", "", 201, 212)  # SELECT NODES IN SHELL181 MODEL
-#
-#         mapdl.nsel("R", "LOC", "Z", 0)
-#
-#         mapdl.fsum()
-#
-#         mapdl.run("/OUT,")
-#
-#         mapdl.run("*GET,ZFRC,FSUM,0,ITEM,FZ")
-#
-#         mapdl.run("LOAD=ZFRC*360/THETA                 ")  # MULTIPLY BY 360/THETA FOR FULL 360 DEGREE RESULTS
-#
-#         mapdl.run("*STATUS,LOAD")
-#
-#         mapdl.run("LOAD_181 = LOAD")
-#
-#         mapdl.run("*VFILL,VALUE_288(1,1),DATA,1024400,1262000,1262000")
-#
-#         mapdl.run("*VFILL,VALUE_288(I,2),DATA,ABS(LOAD_288)")
-#
-#         mapdl.run("*VFILL,VALUE_288(I,3),DATA,ABS(LOAD_288)/(VALUE_288(I,1))")
-#
-#         mapdl.run("*VFILL,VALUE_185(1,1),DATA,1024400,1262000,1262000")
-#
-#         mapdl.run("*VFILL,VALUE_185(J,2),DATA,ABS(LOAD_185)")
-#
-#         mapdl.run("*VFILL,VALUE_185(J,3),DATA,ABS(LOAD_185)/(VALUE_185(J,1))")
-#
-#         mapdl.run("*VFILL,VALUE_181(1,1),DATA,1024400,1262000,1262000")
-#
-#         mapdl.run("*VFILL,VALUE_181(K,2),DATA,ABS(LOAD_181)")
-#
-#         mapdl.run("*VFILL,VALUE_181(K,3),DATA,ABS(LOAD_181)/(VALUE_181(K,1))")
-#
-#
-#     mapdl.run("C*** GET TOTAL LOAD FOR DISPLACEMENT = 0.032")
-#
-#     mapdl.run("C*** ---------------------------------------")
-#
-#     mapdl.set(1, 1)
-#
-#     mapdl.run("I = 1")
-#
-#     mapdl.run("J = 1")
-#
-#     mapdl.run("K = 1")
-#
-#     mapdl.run("*DIM,LABEL,CHAR,3,2")
-#
-#     mapdl.run("*DIM,VALUE_288,,3,3")
-#
-#     mapdl.run("*DIM,VALUE_185,,3,3")
-#
-#     mapdl.run("*DIM,VALUE_181,,3,3")
-#
-#
-# GETLOAD()
-#
-#     mapdl.run("C*** GET TOTAL LOAD FOR DISPLACEMENT = 0.05")
-#
-#     mapdl.run("C*** --------------------------------------")
-#
-#     mapdl.set(2, 1)
-#
-#     mapdl.run("I = I + 1")
-#
-#     mapdl.run("J = J + 1")
-#
-#     mapdl.run("K = K + 1")
-#
-# GETLOAD()
-#
-#     mapdl.run("C*** GET TOTAL LOAD FOR DISPLACEMENT = 0.1")
-#
-#     mapdl.run("C*** -------------------------------------")
-#
-#     mapdl.set(3, 1)
-#
-#     mapdl.run("I = I +1")
-#
-#     mapdl.run("J = J + 1")
-#
-#     mapdl.run("K = K + 1")
-#
-# GETLOAD()
-#
-#     mapdl.run("LABEL(1,1) = 'LOAD, lb','LOAD, lb','LOAD, lb'")
-#
-#     mapdl.run("LABEL(1,2) = ' UX=.032',' UX=0.05',' UX=0.10'")
-#
-#     mapdl.finish()
-#
-#
+# Enter the post-processing routine
+mapdl.run("/OUT,")
+mapdl.post1()
+
+
+###############################################################################
+# Define loads.
+# ~~~~~~~~~~~~~
+# Set up the function.
+
+def getload():
+    ####
+    # Select the nodes in the PIPE288 element model.
+    mapdl.nsel("S", "NODE", "", 1, 2)
+    mapdl.nsel("R", "LOC", "Z", 0)
+    mapdl.run("/OUT,SCRATCH")
+
+    # Sum the nodal force contributions of elements.
+    mapdl.fsum()
+
+    # Extrapolation of the force results in the full 360 (deg) model.
+    load_288 = mapdl.get_value("FSUM", 0, "ITEM", "FZ")
+
+    ####
+    # Select the nodes in the SOLID185 element model.
+    mapdl.nsel("S", "NODE", "", 101, 118)
+    mapdl.nsel("R", "LOC", "Z", 0)
+    mapdl.fsum()
+
+    # Get the force value of the simplified model.
+    load_185_theta = mapdl.get_value("FSUM", 0, "ITEM", "FZ")
+
+    # Extrapolation of the force results in the full 360 (deg) model.
+    load_185 = load_185_theta * 360 / theta
+    mapdl.run("*STATUS,LOAD")
+
+    ####
+    # Select the nodes in the SHELL181 element model.
+    mapdl.nsel("S", "NODE", "", 201, 212)  # SELECT NODES IN SHELL181 MODEL
+    mapdl.nsel("R", "LOC", "Z", 0)
+
+    # Sum the nodal force contributions of elements.
+    mapdl.fsum()
+    mapdl.run("/OUT,")
+
+    # Get the force value of the simplified model.
+    load_181_theta = mapdl.get_value("FSUM", 0, "ITEM", "FZ")
+
+    # Extrapolation of the force results in the full 360 (deg) model.
+    load_181 = load_181_theta * 360 / theta
+    mapdl.run("*STATUS,LOAD")
+
+    # Return load results of each element model.
+    return load_288, load_185, load_181
+
+
+###############################################################################
+# Define loads for load step 1
+
+for i in range(1, ls + 1):
+    mapdl.set(1, 1)
+    load_288, load_185, load_181 = getload()
+    results_load_deflection = f"""
+    LOAD STEP {i}
+    PIPE288_LS{i}= {abs(round(load_288,0))}
+    SOLID185_LS{i} = {abs(round(load_185,0))}
+    SHELL181_LS{i} = {abs(round(load_181,0))}
+    
+    """
+    print(results_load_deflection)
+
+
+###############################################################################
+# Check Results with Pandas
+
+
 # ###############################################################################
 # # Check Results
 # # ~~~~~~~~~~~~~
