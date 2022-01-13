205d4bc8139bca1f41dc5de1dffd4b28e9655af7
Roman Ilchenko <romailch1308@gmail.com>
2022-01-13 17:21:56 +0100

    check kp distances and use updates of kdist

--- examples/06-verif-manual/vm-008-parametric_calculation_of_point_to_point_distances.py
+++ examples/06-verif-manual/vm-008-parametric_calculation_of_point_to_point_distances.py
@@ -36,69 +36,6 @@ class Create:
                     font_size=26)
         return dist_kp
 
-    def create_node_method(self):
-        node1 = mapdl.n(x=self.x1, y=self.y1, z=self.z1)
-        node2 = mapdl.n(x=self.x2, y=self.y2, z=self.z2)
-        dist_xyz_n = mapdl.ndist(node1, node2)
-        return dist_xyz_n
-        # dist_n = math.sqrt(dist_xyz_n[0] ** 2 + dist_xyz_n[1] ** 2 + dist_xyz_n[2] ** 2)
-        # print(dist_n)
-
-
 kp = Create(1, 2, 3, 10, 20, 30)
 kp_dist = kp.create_kp_method()
 print(f"Distance between keypoints is: {kp_dist}")
-print(f"========================================")
-node = Create(1, 2, 3, 10, 20, 30)
-print(node.create_node_method())
-node_dist = node.create_node_method()
-print(node_dist)
-# print(f"Distance between nodes is: {node_dist}")
-
-
-
-
-
-
-
-
-###############################################################################
-# CHANGES FOR NDIST
-
-string_out ="""
-Distance between node is: 33.67491648096547
-========================================
-Define Distance
-The distance between nodes       1 and       2 in coordinate system     0 is:
- DIST              DX (ND2-ND1)      DY (ND2-ND1)      DZ (ND2-ND1)
-  33.67491648       9.000000000       18.00000000       27.00000000
-"""
-#################################
-print(string_out.split())
-#################################
-
-
-#################################
-ndsit_x = string_out.split()[-1]
-ndsit_y = string_out.split()[-2]
-ndsit_z = string_out.split()[-3]
-ndsit_DIST = string_out.split()[-4]
-print(f"DX: {ndsit_x}\n"
-      f"DY: {ndsit_y}\n"
-      f"DZ: {ndsit_z}\n"
-      f"DIST: {ndsit_DIST}")
-#################################
-print("\n\n\n")
-
-#################################
-dist_str = string_out.split()[-4]
-dist = float(string_out.split()[-4])
-print(f"type of the splited string: {type(dist_str)},\n"
-      f"where the full automatically calculated distance between is: {dist_str},\n"
-      f"convertation str to flot: {type(dist)},\n"
-      f"where the distance is: {dist}")
-
-#################################
-
-
-
