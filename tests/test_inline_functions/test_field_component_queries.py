425fc47732ef4704352523c3dde414c020611ca7
jgd10 <jamesgordonderrick@gmail.com>
2022-01-14 10:12:54 +0000

    refactored test setup into a fixture

--- tests/test_inline_functions/test_field_component_queries.py
+++ tests/test_inline_functions/test_field_component_queries.py
@@ -1,19 +1,6 @@
 class TestFieldComponentValueGetter:
-    def build_setup(self, mapdl):
-        mapdl.prep7()
-        mapdl.mp("kxx", 1, 45)
-        mapdl.mp("ex", 1, 2e10)
-        mapdl.mp("perx", 1, 1)
-        mapdl.mp("murx", 1, 1)
-        mapdl.et(1, 'SOLID70')
-        mapdl.et(2, 'CPT215')
-        mapdl.et(3, 'SOLID122')
-        mapdl.et(4, 'SOLID96')
-        mapdl.block(0, 1, 0, 1, 0, 1)
-        mapdl.esize(0.5)
-
-    def test_temp(self, mapdl):
-        self.build_setup(mapdl)
+    def test_temp(self, box_with_fields):
+        mapdl = box_with_fields
         mapdl.type(1)
         mapdl.vmesh(1)
         mapdl.d("all", "temp", 5.0)
@@ -22,8 +9,8 @@ class TestFieldComponentValueGetter:
         temp_value = mapdl.queries.temp(1)
         assert temp_value is not None
 
-    def test_pressure(self, mapdl):
-        self.build_setup(mapdl)
+    def test_pressure(self, box_with_fields):
+        mapdl = box_with_fields
         mapdl.type(2)
         mapdl.vmesh(1)
         mapdl.d("all", "pres", 5.0)
@@ -33,8 +20,8 @@ class TestFieldComponentValueGetter:
         pres_value = mapdl.queries.pres(1)
         assert pres_value is not None
 
-    def test_volt(self, mapdl):
-        self.build_setup(mapdl)
+    def test_volt(self, box_with_fields):
+        mapdl = box_with_fields
         mapdl.type(3)
         mapdl.vmesh(1)
         mapdl.d("all", "volt", 5.0)
@@ -43,8 +30,8 @@ class TestFieldComponentValueGetter:
         volt_value = mapdl.queries.volt(1)
         assert volt_value is not None
 
-    def test_mag(self, mapdl):
-        self.build_setup(mapdl)
+    def test_mag(self, box_with_fields):
+        mapdl = box_with_fields
         mapdl.type(4)
         mapdl.vmesh(1)
         mapdl.d("all", "mag", 5.0)
