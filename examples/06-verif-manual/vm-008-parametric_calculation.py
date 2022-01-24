ae776ba13164bc997660e57f356f93b6f8f19576
Roman Ilchenko <romailch1308@gmail.com>
2022-01-24 23:45:55 +0100

    VM8 is rebuilt with decorator property and ready to review

--- examples/06-verif-manual/vm-008-parametric_calculation.py
+++ examples/06-verif-manual/vm-008-parametric_calculation.py
@@ -73,7 +73,7 @@ mapdl = launch_mapdl()
 
 mapdl.clear()
 mapdl.verify()
-_ = mapdl.prep7()
+mapdl.prep7(mute=True)
 
 
 ###############################################################################
@@ -84,20 +84,16 @@ _ = mapdl.prep7()
 # and nodes.
 
 class Create:
-    def __init__(self, x1, y1, z1, x2, y2, z2):
-        # Coordinate Attributes.
-        self.x1 = x1
-        self.y1 = y1
-        self.z1 = z1
-        self.x2 = x2
-        self.y2 = y2
-        self.z2 = z2
+    def __init__(self, p1, p2):
+        # Points Attributes.
+        self.p1 = p1
+        self.p2 = p2
 
     def kp_distances(self):
 
         # Define keypoints by coordinates.
-        kp1 = mapdl.k(npt=3, x=self.x1, y=self.y1, z=self.z1)
-        kp2 = mapdl.k(npt=4, x=self.x2, y=self.y2, z=self.z2)
+        kp1 = mapdl.k(npt=3, x=self.p1[0], y=self.p1[1], z=self.p1[2])
+        kp2 = mapdl.k(npt=4, x=self.p2[0], y=self.p2[1], z=self.p2[2])
 
         # Get the distance between keypoints.
         dist_kp, kx, ky, kz = mapdl.kdist(kp1, kp2)
@@ -113,8 +109,8 @@ class Create:
     def node_distances(self):
 
         # Define nodes by coordinates.
-        node1 = mapdl.n(node=1, x=self.x1, y=self.y1, z=self.z1)
-        node2 = mapdl.n(node=2, x=self.x2, y=self.y2, z=self.z2)
+        node1 = mapdl.n(node=1, x=self.p1[0], y=self.p1[1], z=self.p1[2])
+        node2 = mapdl.n(node=2, x=self.p2[0], y=self.p2[1], z=self.p2[2])
 
         # Get the distance between nodes.
         dist_node, node_x, node_y, node_z = mapdl.ndist(node1, node2)
@@ -127,6 +123,35 @@ class Create:
                     font_size=26)
         return dist_node
 
+    @property
+    def p1(self):
+        # Getting value
+        return self._p1
+
+    @p1.setter
+    def p1(self, new_value):
+        # Check the data type:
+        if not isinstance(new_value, list):
+            raise ValueError("The coordinates should be implemented by the list!")
+        # Check the quantity of items:
+        if len(new_value) != 3:
+            raise ValueError("The coordinates should have three items in the list as [X, Y, Z]")
+        self._p1 = new_value
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, new_value):
+        # Check the data type:
+        if not isinstance(new_value, list):
+            raise ValueError("The coordinates should be implemented by the list!")
+        # Check the quantity of items:
+        if len(new_value) != 3:
+            raise ValueError("The coordinates should have three items in the list as [X, Y, Z]")
+        self._p2 = new_value
+
 
 ###############################################################################
 # Distance between keypoints
@@ -135,9 +160,11 @@ class Create:
 # and print out an output. The keypoints have got next coordinates:
 #
 # * :math:`K_{\mathrm{3(x,y,z)}} = 100, 0, 30`
-# * :math:`K_{\mathrm{4(x,y,z)}} = -200,25,80`
+# * :math:`K_{\mathrm{4(x,y,z)}} = -200, 25,80`
 
-kp = Create(100, 0, 30, -200, 25, 80)
+kp1 = [100, 0, 30]
+kp2 = [-200, 25, 80]
+kp = Create(kp1, kp2)
 kp_dist = kp.kp_distances()
 print(f"Distance between keypoint is: {kp_dist:.2f}\n\n")
 
@@ -154,7 +181,9 @@ print(mapdl.klist())
 # * :math:`N_{\mathrm{1(x,y,z)}} = 1.5, 2.5, 3.5`
 # * :math:`N_{\mathrm{2(x,y,z)}} = -3.7, 4.6, -3`
 
-nodes = Create(1.5, 2.5, 3.5, -3.7, 4.6, -3)
+node1 = [1.5, 2.5, 3.5]
+node2 = [-3.7, 4.6, -3]
+nodes = Create(node1, node2)
 node_dist = nodes.node_distances()
 print(f"Distance between nodes is: {node_dist:.2f}\n\n")
 
