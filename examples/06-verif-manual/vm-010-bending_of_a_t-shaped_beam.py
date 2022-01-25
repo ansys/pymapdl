3548e0c58fef4f630833f62aec7e9624c54ff40f
Roman Ilchenko <romailch1308@gmail.com>
2022-01-25 18:02:41 +0100

    Aded modifications from German's review, ready to be checked

--- examples/06-verif-manual/vm-010-bending_of_a_t-shaped_beam.py
+++ examples/06-verif-manual/vm-010-bending_of_a_t-shaped_beam.py
@@ -4,23 +4,27 @@ r"""
 Bending of a Tee-Shaped Beam
 ----------------------------
 Problem Description:
- - Find the maximum tensile :math:`\sigma_{\mathrm{(BEND)(BOT)}}` and compressive :math:`\sigma_{\mathrm{(BEND)(TOP)}}`
+ - Find the maximum tensile :math:`\sigma_{\mathrm{(B,Bot)}}` and compressive :math:`\sigma_{\mathrm{(B,Top)}}`
    bending stresses in an unsymmetrical `T-beam` subjected to uniform bending :math:`M_z`,
    with dimensions and geometric properties as shown below.
 
 Reference:
- - S. H. Crandall, N. C. Dahl, An Introduction to the Mechanics of Solids,
+ - S. H. Crandall, N. C. Dahl, *An Introduction to the Mechanics of Solids*,
    McGraw-Hill Book Co., Inc., New York, NY, 1959, pg. 294, ex. 7.2.
 
 Analysis Type(s):
- - Static Analysis (`ANTYPE = 0`)
+ - Static Analysis (``ANTYPE = 0``)
 
 Element Type(s):
- - 3-D 2 Node Beam (BEAM188)
+ - 3-D 2 Node Beam (``BEAM188``)
 
-.. image:: ../../_static/vm10_setup_1.png
-   :width: 300
-   :alt: VM10 Geometry of Beam188 and Element Model
+.. figure:: ../../_static/vm10_setup_1.png
+    :align: center
+    :width: 300
+    :alt:  VM10 Geometry of Beam188 and Element Model
+    :figclass: align-center
+
+    **Figure 1: VM10 Geometry of Beam188 and Element Model**
 
 Material Properties
  - :math:`E = 30 \cdot 10^6 psi`
@@ -32,17 +36,17 @@ Geometric Properties:
  - :math:`b = 8\,in`
 
 Loading:
- - :math:`M_z = 100,000\,in-lb`
+ - :math:`M_z = 100,000\,in\,lb`
 
 .. image:: ../../_static/vm10_setup.png
    :width: 400
    :alt: VM10 Problem Sketch
 
 Analysis Assumptions and Modeling Notes:
- - A length (:math:`l = 100 in`) is arbitrarily selected since the bending moment is constant.
-   A `T-section` beam is modeled using flange width (:math:`6b`),
-   flange thickness (:math:`\frac{h}{2}`), overall depth (:math:`2h + \frac{h}{2}`), and
-   stem thickness (:math:`b`), input using :meth:`Mapdl.secdata <ansys.mapdl.core.Mapdl.secdata>`.
+ - A length :math:`(l = 100 in)` is arbitrarily selected since the bending moment is constant.
+   A `T-section` beam is modeled using flange width :math:`(6b)`,
+   flange thickness :math:`(\frac{h}{2})`, overall depth :math:`(2h + \frac{h}{2})`, and
+   stem thickness :math:`(b)`, input using :meth:`Mapdl.secdata <ansys.mapdl.core.Mapdl.secdata>`.
 
 """
 
@@ -69,7 +73,7 @@ mapdl = launch_mapdl()
 
 mapdl.clear()
 mapdl.verify()
-_ = mapdl.prep7()
+mapdl.prep7(mute=True)
 
 
 ###############################################################################
@@ -116,9 +120,9 @@ print(mapdl.mplist())
 # ~~~~~~~~~~~~~~
 # Set up the cross-section properties for a beam elements, where:
 #
-# * :math:`w_1 = 6b = 6 * 1.5 = 9\,in` - flange width.
-# * :math:`w_2 = 2h + \frac{h}{2} = 2 * 8 + \frac{8}{2} = 20\,in` - overall depth.
-# * :math:`t_1 = \frac{h}{2} = \frac{8}{2} = 4\,in` - flange thickness.
+# * :math:`w_1 = 6b = 6 \cdot 1.5 = 9\,in` - flange width.
+# * :math:`w_2 = 2h + h/2 = 2 \cdot 8 + 8/2 = 20\,in` - overall depth.
+# * :math:`t_1 = h/2 = 8/2 = 4\,in` - flange thickness.
 # * :math:`t_2 = b = 1.5\,in` - stem thickness.
 
 # Parameterization of the cross-section dimensions.
@@ -138,8 +142,7 @@ print(mapdl.slist())
 ###############################################################################
 # Define Geometry
 # ~~~~~~~~~~~~~~~
-# Set up the nodes and elements. Create nodes then create elements
-# between nodes.
+# Set up the nodes and elements. Create nodes between elements.
 
 # Define nodes for the beam element.
 mapdl.n(1, x=0, y=0)
@@ -167,10 +170,11 @@ cpos = [(162.20508123980457, 109.41124535475498, 112.95887397446565),
         (50.0, 0.0, 0.0),
         (-0.4135015240403764, -0.4134577015789461, 0.8112146563156641)]
 
-_ = mapdl.eplot(show_node_numbering=True,
-                line_width=5,
-                cpos=cpos,
-                font_size=40)
+mapdl.eplot(show_node_numbering=True,
+            line_width=5,
+            cpos=cpos,
+            font_size=40,
+            mute=True)
 
 
 ###############################################################################
@@ -178,21 +182,21 @@ _ = mapdl.eplot(show_node_numbering=True,
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Application of boundary conditions (BC).
 
-_ = mapdl.d(node=1, lab="ALL")
-_ = mapdl.d(node="ALL", lab="UZ", lab2="ROTX", lab3="ROTY")
+mapdl.d(node=1, lab="ALL", mute=True)
+mapdl.d(node="ALL", lab="UZ", lab2="ROTX", lab3="ROTY", mute=True)
 
 
 ###############################################################################
 # Define Distributed Loads
 # ~~~~~~~~~~~~~~~~~~~~~~~~
-# Apply a bending moment :math:`bending_{\mathrm{(M_z)}}= 100000\,in-lb`.
+# Apply a bending moment :math:`\mathrm{M_{z}}= 100000\,in\,lb`.
 
 # Parametrization of the bending moment.
 bending_mz = 100000
 
 # Application of the surface load to the beam element.
 mapdl.f(node=2, lab="MZ", value=bending_mz)
-_ = mapdl.finish()
+mapdl.finish(mute=True)
 
 
 ###############################################################################
@@ -205,7 +209,7 @@ mapdl.slashsolu()
 
 # Define the number of substeps to be taken this load step.
 mapdl.nsubst(1)
-_ = mapdl.solve()
+mapdl.solve(mute=True)
 
 
 ###############################################################################
@@ -214,7 +218,7 @@ _ = mapdl.solve()
 # Enter post-processing.
 
 # Enter the post-processing routine.
-_ = mapdl.post1()
+mapdl.post1(mute=True)
 
 
 ###############################################################################
@@ -248,8 +252,8 @@ strss_bot_tens = mapdl.get_value(entity="ELEM",
 # compressive bending stresses, which can be compared with expected target
 # values:
 #
-# - maximum tensile bending stress :math:`\sigma_{\mathrm{(BEND)(BOT)}} = 300\,psi`.
-# - maximum compressive bending stress :math:`\sigma_{\mathrm{(BEND)(TOP)}} = -700\,psi`.
+# - maximum tensile bending stress :math:`\sigma_{\mathrm{(B,Bot)}} = 300\,psi`.
+# - maximum compressive bending stress :math:`\sigma_{\mathrm{(B,Top)}} = -700\,psi`.
 #
 # For better representation of the results we can use ``pandas`` dataframe
 # with following settings below:
