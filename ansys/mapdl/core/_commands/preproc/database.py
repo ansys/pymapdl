f373a2c9118fd6a14dac1fb0da60b00d79dd55cf
German <germanmartinezayuso@gmail.com>
2022-02-08 19:30:35 +0100

    Fixing missing space

--- ansys/mapdl/core/_commands/preproc/database.py
+++ ansys/mapdl/core/_commands/preproc/database.py
@@ -57,7 +57,7 @@ class Database:
         fname
             File name and directory path (248 characters maximum, including the
             characters needed for the directory path).
-            An unspecified directory path defaults to the working directory; 
+            An unspecified directory path defaults to the working directory;
             in this case, you can use all 248 characters for the file name.
 
         ext
