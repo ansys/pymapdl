cd78d58fe0e04ef5bd03e1780417b0d856552f82
Pernelle Marone-Hitz <pernelle.maronehitz@ansys.com>
2022-03-03 17:55:44 +0100

    Correcting formatting issues

--- examples/07-technology-showcase-examples/20-example-technology-showcase-dynamic-simulation-PCB.py
+++ examples/07-technology-showcase-examples/20-example-technology-showcase-dynamic-simulation-PCB.py
@@ -24,16 +24,18 @@ Chapter 20 of the Mechanical APDL Technology Showcase Manual.
 
 # start MAPDL as a service
 import os
+
 from ansys.mapdl.core import launch_mapdl
+
 mapdl = launch_mapdl(run_location=os.getcwd())
 print(mapdl)
 
 # read model of single circuit board
 mapdl.prep7()
-mapdl.cdread('COMB', 'Electronics_PCB', 'cdb')
+mapdl.cdread("COMB", "Electronics_PCB", "cdb")
 mapdl.allsel()
 mapdl.eplot()
-mapdl.cmsel('all')
+mapdl.cmsel("all")
 
 ###############################################################################
 # Creating the complete layered model
@@ -43,51 +45,51 @@ mapdl.cmsel('all')
 
 # duplicate single PCB to get three layers
 #  get the maximum node number for the single layers PCB in the input file
-max_nodenum = mapdl.get('max_nodenum', 'node', ' ', 'num', 'max')
+max_nodenum = mapdl.get("max_nodenum", "node", " ", "num", "max")
 # generate additional PCBs offset by 20 mm in the -y direction
-mapdl.egen('3', max_nodenum, 'all', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '0', '-20', '0')
+mapdl.egen("3", max_nodenum, "all", " ", " ", " ", " ", " ", " ", " ", "0", "-20", "0")
 
 
 # bind the three layers together
 # select components of interest
-mapdl.cmsel('s', 'N_JOINT_BOARD')
-mapdl.cmsel('a', 'N_JOINT_LEGS')
-mapdl.cmsel('a', 'N_BASE')
+mapdl.cmsel("s", "N_JOINT_BOARD")
+mapdl.cmsel("a", "N_JOINT_LEGS")
+mapdl.cmsel("a", "N_BASE")
 # get number of currently selected nodes
 nb_selected_nodes = mapdl.mesh.n_node
 current_node = 0
 queries = mapdl.queries
 # also select similar nodes for copies of the single PCB
 # and couple all dofs at the interface
-for node_id in range(1, nb_selected_nodes+1):
+for node_id in range(1, nb_selected_nodes + 1):
     current_node = queries.ndnext(current_node)
-    mapdl.nsel('a', 'node', ' ', current_node + max_nodenum)
-    mapdl.nsel('a', 'node', ' ', current_node + 2*max_nodenum)
-mapdl.cpintf('all')
+    mapdl.nsel("a", "node", " ", current_node + max_nodenum)
+    mapdl.nsel("a", "node", " ", current_node + 2 * max_nodenum)
+mapdl.cpintf("all")
 
 # define fixed support boundary condition
 # get max coupled set number
-cp_max = mapdl.get('cp_max', 'cp', '0', 'max')
+cp_max = mapdl.get("cp_max", "cp", "0", "max")
 # unselect nodes scoped in CP equations
-mapdl.nsel('u', 'cp', ' ', '1', 'cp_max')
+mapdl.nsel("u", "cp", " ", "1", "cp_max")
 # create named selection for base excitation
-mapdl.cm('n_base_excite', 'node')
+mapdl.cm("n_base_excite", "node")
 # fix displacement for base excitation nodes
-mapdl.d('all', 'all')
+mapdl.d("all", "all")
 
 # select all and plot model
-mapdl.allsel('all')
-mapdl.cmsel('all')
-mapdl.graphics('power')
-mapdl.rgb('index', '100', '100', '100', '0')
-mapdl.rgb('index', '80', '80', '80', '13')
-mapdl.rgb('index', '60', '60', '60', '14')
-mapdl.rgb('index', '0', '0', '0', '15')
-mapdl.triad('rbot')
-mapdl.pnum('type', '1')
-mapdl.number('1')
-mapdl.hbc('1', 'on')
-mapdl.pbc('all', ' ', '1')
+mapdl.allsel("all")
+mapdl.cmsel("all")
+mapdl.graphics("power")
+mapdl.rgb("index", "100", "100", "100", "0")
+mapdl.rgb("index", "80", "80", "80", "13")
+mapdl.rgb("index", "60", "60", "60", "14")
+mapdl.rgb("index", "0", "0", "0", "15")
+mapdl.triad("rbot")
+mapdl.pnum("type", "1")
+mapdl.number("1")
+mapdl.hbc("1", "on")
+mapdl.pbc("all", " ", "1")
 mapdl.view(1, 1, 1, 1)
 mapdl.eplot(vtk=False)
 mapdl.eplot(vtk=True)
@@ -99,11 +101,11 @@ mapdl.eplot(vtk=True)
 
 # enter solution processor and define analysis settings
 mapdl.slashsolu()
-mapdl.antype('modal')
+mapdl.antype("modal")
 # set number of modes to extract
 nb_modes = 300
 # use Block Lanzos to extract specified number of modes
-mapdl.modopt('lanb', nb_modes)
+mapdl.modopt("lanb", nb_modes)
 mapdl.mxpand(nb_modes)
 output = mapdl.solve()
 print(output)
@@ -120,7 +122,7 @@ print(output)
 # using MAPDL methods
 mapdl.post1()
 mapdl.set(1, 1)
-mapdl.plnsol('u', 'sum')
+mapdl.plnsol("u", "sum")
 
 # using PyMAPDL result reader
 mapdl_result = mapdl.result
@@ -151,29 +153,29 @@ mapdl_result.plot_nodal_displacement(0)
 # ~~~~~~~~~~~~~
 # The response spectrum analysis is defined, solved and post-processed
 
-#define PSD analysis with input spectrum
+# define PSD analysis with input spectrum
 mapdl.slashsolu()
-mapdl.antype('spectr')
+mapdl.antype("spectr")
 # power spectral density
-mapdl.spopt('psd')
+mapdl.spopt("psd")
 # use input table 1 with acceleration spectrum in terms of acceleration due to gravity
-mapdl.psdunit(1, 'accg', 9.81*1000)
+mapdl.psdunit(1, "accg", 9.81 * 1000)
 # define the frequency points in the input table 1
-mapdl.psdfrq(1, ' ', 1, 40, 50, 70.71678, 100, 700, 900)
+mapdl.psdfrq(1, " ", 1, 40, 50, 70.71678, 100, 700, 900)
 # define the PSD values in the input table 1
-mapdl.psdval(1, .01, .01, 0.1, 1, 10, 10, 1)
+mapdl.psdval(1, 0.01, 0.01, 0.1, 1, 10, 10, 1)
 # set the damping ratio as 5%
 mapdl.dmprat(0.05)
 # apply base excitation on the set of nodes N_BASE_EXCITE in the y-direction from table 1
-mapdl.d('N_BASE_EXCITE', 'uy', 1)
+mapdl.d("N_BASE_EXCITE", "uy", 1)
 # calculate the participation factor for PSD with base excitation from input table 1
-mapdl.pfact(1, 'base')
+mapdl.pfact(1, "base")
 # write the displacent solution relative to the base excitation to the results file from the PSD analysis
-mapdl.psdres('disp', 'rel')
+mapdl.psdres("disp", "rel")
 # write the absolute velocity solution to the results file from the PSD analysis
-mapdl.psdres('velo', 'abs')
+mapdl.psdres("velo", "abs")
 # write the absolute acceleration solution to the results file from the PSD analysis
-mapdl.psdres('acel', 'abs')
+mapdl.psdres("acel", "abs")
 # combine only those modes whose significance level exceeds 0.0001
 mapdl.psdcom
 mapdl.solve()
@@ -183,49 +185,50 @@ mapdl.solve()
 # Using MAPDL methods in POST1
 mapdl.post1()
 mapdl.set(1, 1)
-mapdl.plnsol('u', 'sum')
-mapdl.set('last')
-mapdl.plnsol('u', 'sum')
+mapdl.plnsol("u", "sum")
+mapdl.set("last")
+mapdl.plnsol("u", "sum")
 
 # Using MAPDL methods in POST26 (time-history post-processing)
 mapdl.post26()
 # allow storage for 200 variables
 mapdl.numvar(200)
-mapdl.cmsel('s', 'MY_MONITOR')
+mapdl.cmsel("s", "MY_MONITOR")
 monitored_node = mapdl.queries.ndnext(0)
-mapdl.store('psd')
+mapdl.store("psd")
 # store the psd analysis u_y data for the node MYMONITOR as the reference no 2
-mapdl.nsol(2, monitored_node, 'u', 'y')
+mapdl.nsol(2, monitored_node, "u", "y")
 # compute the response power spectral density for displacement associated with variable 2
 mapdl.rpsd(3, 2)
-mapdl.show('png')
+mapdl.show("png")
 # plot the variable 3
 mapdl.plvar(3)
 # print the variable 3
 mapdl.prvar(3)
 # x-axis is set for Log X scale
-mapdl.gropt('logx', 1)
+mapdl.gropt("logx", 1)
 # y-axis is set for Log X scale
-mapdl.gropt('logy', 1)
+mapdl.gropt("logy", 1)
 # plot the variable 3
 mapdl.plvar(3)
-mapdl.show('close')
+mapdl.show("close")
 
 # Using python libraries to construct plots
 # store MAPDL results to python variables
-mapdl.dim('frequencies', 'array', 4000, 1)
-mapdl.dim('response', 'array', 4000, 1)
-mapdl.vget('frequencies(1)', 1)
-mapdl.vget('response(1)', 3)
-frequencies = mapdl.parameters['frequencies']
-response = mapdl.parameters['response']
+mapdl.dim("frequencies", "array", 4000, 1)
+mapdl.dim("response", "array", 4000, 1)
+mapdl.vget("frequencies(1)", 1)
+mapdl.vget("response(1)", 3)
+frequencies = mapdl.parameters["frequencies"]
+response = mapdl.parameters["response"]
 
 # use Matplotlib to create graph
 import matplotlib.pyplot as plt
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.xscale('log')
-plt.yscale('log')
+plt.xscale("log")
+plt.yscale("log")
 ax.plot(frequencies, response)
 ax.set_xlabel("Frequencies")
 ax.set_ylabel("Response power spectral density")
