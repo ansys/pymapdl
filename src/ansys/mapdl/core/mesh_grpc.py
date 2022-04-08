b5d487341bfb4fcc8bad2b96d3239241edb44a26
German <germanmartinezayuso@gmail.com>
2022-04-08 10:40:32 +0200

    add 2

--- src/ansys/mapdl/core/mesh_grpc.py
+++ src/ansys/mapdl/core/mesh_grpc.py
@@ -394,6 +394,7 @@ class MeshGrpc(Mesh):
         offset = np.hstack((elem_off_raw - n_elem, lst_value))
 
         elems_ = deepcopy(elem_raw)  # elem_raw is only-read
+        elems_ = elems_[n_elem:]  # elem_raw is only-read
 
         return elem_raw[n_elem:], offset
 
