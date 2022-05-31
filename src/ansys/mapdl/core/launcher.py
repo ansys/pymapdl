e6473dcf8683c46a7ba4cf4a9aa4f4f91754b85c
German <germanmartinezayuso@gmail.com>
2022-05-31 20:21:33 +0200

    taking advantage of short circuit in logical operations

--- src/ansys/mapdl/core/launcher.py
+++ src/ansys/mapdl/core/launcher.py
@@ -1257,12 +1257,9 @@ def launch_mapdl(
 
     # Start MAPDL with PyPIM if the environment is configured for it
     # and the user did not pass a directive on how to launch it.
-    if _HAS_PIM:
-        if pypim.is_configured() and exec_file is None:
-            LOG.info(
-                "Starting MAPDL remotely. The startup configuration will be ignored."
-            )
-            return launch_remote_mapdl(cleanup_on_exit=cleanup_on_exit)
+    if _HAS_PIM and exec_file is None and pypim.is_configured():
+        LOG.info("Starting MAPDL remotely. The startup configuration will be ignored.")
+        return launch_remote_mapdl(cleanup_on_exit=cleanup_on_exit)
 
     # connect to an existing instance if enabled
     if start_instance is None:
