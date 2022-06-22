6e3a6b53ccc06ad01a19e38a17be9a9fdc9160a2
German <germanmartinezayuso@gmail.com>
2022-06-22 12:34:39 +0200

    removing shell script

--- .ci/start_mapdl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker pull $MAPDL_IMAGE
-docker run -e ANSYSLMD_LICENSE_FILE=1055@$LICENSE_SERVER --restart always --name mapdl -p $PYMAPDL_PORT:50052 -p $PYMAPDL_DB_PORT:50055 $MAPDL_IMAGE -smp > log.txt &
-grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
-# python -c "from ansys.mapdl.core import launch_mapdl; print(launch_mapdl())"
