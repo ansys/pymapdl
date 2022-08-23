3454b2f1a4d4c2fc6bb1d7ee941fb758d5016ea5
German <germanmartinezayuso@gmail.com>
2022-08-23 13:16:02 +0200

    Adding better version control checks to database.

--- src/ansys/mapdl/core/database/__init__.py
+++ src/ansys/mapdl/core/database/__init__.py
@@ -1,3 +1,8 @@
 """The mapdl database module, allowing the access to the MAPDL database from Python."""
 
-from .database import DBDef, MapdlDb, check_mapdl_db_is_alive  # noqa: F401
+from .database import (  # noqa: F401
+    VALID_MAPDL_VERSIONS,
+    DBDef,
+    MapdlDb,
+    check_mapdl_db_is_alive,
+)
