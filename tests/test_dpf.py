7b56469ba7b23ddbe654f55e89973b82724852a3
German <germanmartinezayuso@gmail.com>
2022-09-03 23:39:15 +0200

    Implementation in unit tests

---
+++ tests/test_dpf.py
@@ -0,0 +1,11 @@
+# Test the DPF implementation
+from ansys.dpf import core as dpf_core
+
+
+def test_dpf_connection():
+    # uses 127.0.0.1 and port 50054 by default
+    try:
+        dpf_core.connect_to_server()
+        assert True
+    except OSError:
+        assert False
