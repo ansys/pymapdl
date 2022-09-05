38d28c0fc57226f3c5608b342cb02fb52983a88f
German <germanmartinezayuso@gmail.com>
2022-09-05 12:06:12 +0200

    Asserting conneciton is live, and specifing port.

--- tests/test_dpf.py
+++ tests/test_dpf.py
@@ -1,11 +1,16 @@
 """Test the DPF implementation"""
+import os
+
 from ansys.dpf import core as dpf_core
+
+DPF_PORT = os.environ.get("DPF_PORT", 21002)
 
 
 def test_dpf_connection():
     # uses 127.0.0.1 and port 50054 by default
     try:
-        dpf_core.connect_to_server()
+        grpc_con = dpf_core.connect_to_server(port=DPF_PORT)
+        assert grpc_con.live
         assert True
     except OSError:
         assert False
