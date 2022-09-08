7cf34b05814d576fb38898c7aa4a42e803b60be8
German <germanmartinezayuso@gmail.com>
2022-09-08 14:18:08 +0200

    Adding CLI Adding doc about exec_file argument in converter. Only raise an error if using default file name.

--- src/ansys/mapdl/core/convert.py
+++ src/ansys/mapdl/core/convert.py
@@ -104,7 +104,11 @@ def convert_script(
         ``True``.
 
     line_ending : str, optional
-        When None, automatically determined by OS being used.
+        When None, automatically is ``\n.``
+
+    exec_file : str, optional
+        Specify the location of the ANSYS executable and include
+        it in the converter output ``launch_mapdl`` call.
 
     macros_as_functions : bool, optional
         Attempt to convert MAPDL macros to python functions.
@@ -179,6 +183,10 @@ def convert_script(
 
     if filename_out is None:
         filename_out = os.path.splitext(filename_in)[0] + ".py"
+        if os.path.exists(filename_out):
+            raise FileExistsError(
+                f"The predefined output file '{filename_out}' already exists."
+            )
 
     translator = _convert(
         apdl_strings=apdl_strings,
@@ -195,9 +203,6 @@ def convert_script(
         header=header,
         print_com=print_com,
     )
-
-    if os.path.exists(filename_out):
-        raise FileExistsError(f"The output file '{filename_out}' already exists.")
 
     translator.save(filename_out)
     return translator.lines
@@ -527,7 +532,7 @@ class FileTranslator:
         self.lines.append(f"from {core_module} import launch_mapdl")
 
         if exec_file:
-            exec_file_parameter = f'"{exec_file}", '
+            exec_file_parameter = f'exec_file="{exec_file}", '
         else:
             exec_file_parameter = ""
 
@@ -945,23 +950,128 @@ import click
 
 
 @click.command()
-@click.option("-i", help="Name of the input MAPDL script file.")
+@click.argument("filename_in")
 @click.option("-o", default=None, help="Name of the output Python script.")
+@click.option("--filename_out", default=None, help="Name of the output Python script.")
 @click.option(
-    "--auto-exit",
+    "--loglevel",
+    default="WARNING",
+    help="Logging level of the ansys object within the script.",
+)
+@click.option(
+    "--auto_exit",
     default=True,
     help="Adds a line to the end of the script to exit MAPDL. Default ``True``",
 )
+@click.option("--line_ending", default=None, help="When None, automatically is ``\n.``")
+@click.option(
+    "--exec_file",
+    default=None,
+    help="Specify the location of the ANSYS executable and include it in the converter output ``launch_mapdl`` call.",
+)
+@click.option(
+    "--macros_as_functions",
+    default=True,
+    help="Attempt to convert MAPDL macros to python functions.",
+)
+@click.option(
+    "--use_function_names",
+    default=True,
+    help="Convert MAPDL functions to ansys.mapdl.core.Mapdl class methods.  When ``True``, the MAPDL command ``K`` will be converted to ``mapdl.k``.  When ``False``, it will be converted to ``mapdl.run('k')``.",
+)
+@click.option(
+    "--show_log",
+    default=False,
+    help="Print the converted commands using a logger (from ``logging`` Python module).",
+)
+@click.option(
+    "--add_imports",
+    default=True,
+    help='If ``True``, add the lines ``from ansys.mapdl.core import launch_mapdl`` and ``mapdl = launch_mapdl(loglevel="WARNING")`` to the beginning of the output file. This option is useful if you are planning to use the output script from another mapdl session. See examples section. This option overrides ``auto_exit``.',
+)
+@click.option(
+    "--comment_solve",
+    default=False,
+    help='If ``True``, it will pythonically comment the lines that contain ``"SOLVE"`` or ``"/EOF"``.',
+)
+@click.option(
+    "--cleanup_output",
+    default=True,
+    help="If ``True`` the output is formatted using ``autopep8`` before writing the file or returning the string. This requires ``autopep8`` to be installed.",
+)
+@click.option(
+    "--header",
+    default=True,
+    help="If ``True``, the default header is written in the first line of the output. If a string is provided, this string will be used as header.",
+)
+@click.option(
+    "--print_com",
+    default=True,
+    help="Print command ``/COM`` arguments to python console. Defaults to ``True``.",
+)
 def cli(
-    i,
+    filename_in,
     o,
+    filename_out,
+    loglevel,
     auto_exit,
+    line_ending,
+    exec_file,
+    macros_as_functions,
+    use_function_names,
+    show_log,
+    add_imports,
+    comment_solve,
+    cleanup_output,
+    header,
+    print_com,
 ):
     """PyMAPDL CLI tool for converting MAPDL scripts to PyMAPDL scripts.
 
-    Example
-    =======
+    Usage
+    =====
 
-    $ convertscript -i mapdl.dat -o python.py
+    $ convertscript mapdl.dat -o python.py
+    File mapdl.dat successfully converted to python.py.
+
+    The output argument is completely optional, in that case, it will just change the extension to "py".
+
+    $ convertscript mapdl.dat
+    File mapdl.dat successfully converted to mapdl.py.
+
+    You can use any option from ``ansys.mapdl.core.convert.convert_script`` function.
+
+    $ convertscript mapdl.dat --auto-exit False
+    File mapdl.dat successfully converted to mapdl.py.
+
+    $ convertscript.exe mapdl.dat --filename_out mapdl.out --add_imports False
+    File mapdl.dat successfully converted to mapdl.out.
+
+
     """
-    convert_script(filename_in=i, filename_out=o, auto_exit=auto_exit)
+    if o:
+        filename_out = o
+
+    convert_script(
+        filename_in,
+        filename_out,
+        loglevel,
+        auto_exit,
+        line_ending,
+        exec_file,
+        macros_as_functions,
+        use_function_names,
+        show_log,
+        add_imports,
+        comment_solve,
+        cleanup_output,
+        header,
+        print_com,
+    )
+
+    if filename_out:
+        print(f"File {filename_in} successfully converted to {filename_out}.")
+    else:
+        print(
+            f"File {filename_in} successfully converted to {os.path.splitext(filename_in)[0] + '.py'}."
+        )
