fd59166b1ff2786f2641e7fd4189e866224e8bd0
German <germanmartinezayuso@gmail.com>
2022-09-30 13:08:09 +0200

    Fixing test failing locally

--- tests/test_licensing.py
+++ tests/test_licensing.py
@@ -167,7 +167,7 @@ def test_license_checker(tmpdir, license_checker):
 @skip_no_lic_bin
 def test_check_license_file(tmpdir):
     timeout = 15
-    checker = licensing.LicenseChecker(verbose=True, timeout=timeout)
+    checker = licensing.LicenseChecker(timeout=timeout)
     # start the license check in the background
     checker.start(checkout_license=False)
 
