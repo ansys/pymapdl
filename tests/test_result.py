0e136c15c9f789ca5a5aea5fc5955bb2a49e5370
German <germanmartinezayuso@gmail.com>
2022-10-15 00:04:48 +0200

    Implementing unit tests

--- tests/test_result.py
+++ tests/test_result.py
@@ -17,6 +17,7 @@ Notes
 import os
 import tempfile
 
+from ansys.dpf.gate.errors import DPFServerException
 from ansys.mapdl.reader import read_binary
 import numpy as np
 import pytest
@@ -26,6 +27,7 @@ from ansys.mapdl.core.examples import (
     elongation_of_a_solid_bar,
     piezoelectric_rectangular_strip_under_pure_bending_load,
     pinched_cylinder,
+    transient_response_of_a_ball_impacting_a_flexible_surface,
     transient_thermal_stress_in_a_cylinder,
 )
 
@@ -192,8 +194,7 @@ class TestStaticThermocoupledExample(TestExample):
         mapdl.set(1, set_)
         post_values = post.element_stress(COMPONENTS[comp])
 
-        ids, result_values = result.element_stress(set_)
-        result_values = result_values[np.argsort(ids)][:, comp]
+        result_values = result.element_stress(set_)[1][:, comp]
 
         # Reader returns a list of arrays. Each element of the list is the array (nodes x stress) for each element
         reader_values = reader.element_stress(set_ - 1)[1]  # getting data
@@ -242,6 +243,17 @@ class TestStaticThermocoupledExample(TestExample):
             result.nodal_temperature(set_)[1][node], np.array([69.9990463256836])
         )
 
+    def test_parse_step_substep(self, mapdl, result):
+        # Int based
+        assert result.parse_step_substep(0) == 0
+        with pytest.raises(DPFServerException):
+            assert result.parse_step_substep(1)  # Only one step
+
+        # tuple/list
+        for each in range(10):
+            assert result.parse_step_substep((0, each)) == each
+            assert result.parse_step_substep([0, each]) == each
+
 
 class TestElectroThermalCompliantMicroactuator(TestExample):
     """Class to test the Electro-Thermal-Compliant Microactuator VM223 example."""
@@ -283,8 +295,7 @@ class TestElectroThermalCompliantMicroactuator(TestExample):
         mapdl.set(1, set_)
         post_values = post.element_stress(COMPONENTS[comp])
 
-        ids, result_values = result.element_stress(set_)
-        result_values = result_values[np.argsort(ids)][:, comp]
+        result_values = result.element_stress(set_)[1][:, comp]
 
         # Reader returns a list of arrays. Each element of the list is the array (nodes x stress) for each element
         reader_values = reader.element_stress(set_ - 1)[1]  # getting data
@@ -320,8 +331,7 @@ class TestSolidStaticPlastic(TestExample):
         # It does not filter by selection neither.
         post_values = post.element_stress(COMPONENTS[comp])
 
-        ids, result_values = result.element_stress(set_)
-        result_values = result_values[np.argsort(ids)][:, comp]
+        result_values = result.element_stress(set_)[1][:, comp]
 
         # Reader returns a list of arrays. Each element of the list is the array (nodes x stress) for each element
         reader_values = reader.element_stress(set_ - 1)[1]  # getting data
@@ -399,17 +409,13 @@ class TestPiezoelectricRectangularStripUnderPureBendingLoad(TestExample):
         mapdl.set(1, set_)
         post_values = post.element_stress(COMPONENTS[comp])
 
-        ids, result_values = result.element_stress(set_)
-        result_values = result_values[np.argsort(ids)][:, comp]
+        result_values = result.element_stress(set_)[1][:, comp]
 
         reader_values = reader.element_stress(set_ - 1)[1]
         reader_values = np.array([each[comp][0] for each in reader_values])
 
         validate(result_values, reader_values, post_values)  # Reader results are broken
 
-    @pytest.mark.xfail(
-        reason="DPF shows different results with respect to Post and Reader. Interpolation between nodes?"
-    )
     @pytest.mark.parametrize("comp", [0, 1, 2], scope="class")
     def test_compatibility_nodal_elastic_strain(
         self, mapdl, reader, post, result, comp
@@ -419,9 +425,14 @@ class TestPiezoelectricRectangularStripUnderPureBendingLoad(TestExample):
         post_values = post.nodal_elastic_component_strain(COMPONENTS[comp])
         result_values = result.nodal_elastic_strain(set_)[1][:, comp]
         reader_values = reader.nodal_elastic_strain(set_ - 1)[1][:, comp]
+
+        # Overwrite the midside nodes. It seems that DPF either return them interpolated or not
+        # return them at all. This hack will allow partial validation.
+        post_values[np.isnan(reader_values)] = 0
+        result_values[np.isnan(reader_values)] = 0
         reader_values[np.isnan(reader_values)] = 0  # Overwriting NaNs with zeros
 
-        validate(result_values, reader_values, post_values)  # Reader results are broken
+        validate(result_values, reader_values, post_values)
 
     def test_selection_nodes(self, mapdl, result, post):
         set_ = 1
@@ -474,20 +485,17 @@ class TestPinchedCylinderVM6(TestExample):
 
         validate(result_values, reader_values, post_values)  # Reader results are broken
 
-    @pytest.mark.xfail(
-        reason="The shell elements return TOP/BOTTOM in PRESOL, but we don't know about DPF."
-    )
     @pytest.mark.parametrize("comp", [0, 1, 2, 3, 4, 5], scope="class")
     def test_compatibility_element_stress(self, mapdl, reader, post, result, comp):
         set_ = 1
         mapdl.set(1, set_)
+        mapdl.shell("mid")  # DPF returns the middle layer value.
 
         # Post returns the elements always ordered, because of the ETAB.
         # It does not filter by selection neither.
         post_values = post.element_stress(COMPONENTS[comp])
 
-        ids, result_values = result.element_stress(set_)
-        result_values = result_values[np.argsort(ids)][:, comp]
+        result_values = result.element_stress(set_)[1][:, comp]
 
         # Reader returns a list of arrays. Each element of the list is the array (nodes x stress) for each element
         reader_values = reader.element_stress(set_ - 1)[1]  # getting data
@@ -497,17 +505,117 @@ class TestPinchedCylinderVM6(TestExample):
         )
 
         validate(result_values, reader_values, post_values)
+        mapdl.shell()  # Back to default
 
-    @pytest.mark.xfail(
-        reason="The shell elements return TOP/BOTTOM in PRESOL, but we don't know about DPF."
-    )
-    def test_nodal_coordinate_system(self, mapdl, result, post):
+    def test_compatibility_nodal_displacement(self, mapdl, reader, post, result):
+        mapdl.set(1, 1)
+        post_values = post.nodal_displacement("all")[:, :3]
+        result_values = result.nodal_displacement(1)[1]
+        reader_values = reader.nodal_displacement(0)[1][:, :3]
+
+        validate(result_values, reader_values, post_values)  # Reader results are broken
+
+    @pytest.mark.parametrize("comp", [0, 1, 2, 3, 4, 5], scope="class")
+    def test_result_in_element_coordinate_system(
+        self, mapdl, result, reader, post, comp
+    ):
         set_ = 1
         mapdl.set(1, set_)
         mapdl.rsys("solu")
+        mapdl.shell("mid")  # DPF returns the middle layer value.
 
-        post_values = post.element_stress("x")
-        result_values = result.element_stress(set_, in_element_coord_sys=True)[1][:, 0]
+        post_values = post.element_stress(COMPONENTS[comp])
+        result_values = result.element_stress(set_, in_element_coord_sys=True)[1][
+            :, comp
+        ]
 
-        assert np.allclose(result_values, post_values)
-        mapdl.allsel()  # resetting selection
+        # Reader returns a list of arrays. Each element of the list is the array (nodes x stress) for each element
+        reader_values = reader.element_stress(set_ - 1)[1]  # getting data
+        # We are going to do the average across the element, and then retrieve the first column (X)
+        reader_values = np.array(
+            [each_element.mean(axis=0)[comp] for each_element in reader_values]
+        )
+
+        validate(result_values, reader_values, post_values)
+        mapdl.rsys(0)  # Back to default
+
+
+class TestTransientResponseOfABallImpactingAFlexibleSurfaceVM65(TestExample):
+    """Class to test Transient Response of a Ball Impacting a Flexible Surface (VM65 example).
+
+    A rigid ball of mass m is dropped through a height h onto a flexible surface of stiffness k. Determine
+    the velocity, kinetic energy, and displacement y of the ball at impact and the maximum displacement
+    of the ball.
+
+    Purposes of tests
+    =================
+    * Test multiple steps simulations
+
+    Features of test
+    ================1.
+    * Analysis Type(s): Nonlinear Transient Dynamic Analysis (ANTYPE = 4)
+    * Element Type(s):
+      * Structural Mass Elements (MASS21)
+      * 2-D/3-D Node-to-Surface Contact Elements (CONTA175)
+
+    """
+
+    example = transient_response_of_a_ball_impacting_a_flexible_surface
+    example_name = "Transient Response of a Ball Impacting a Flexible Surface"
+    apdl_code = prepare_example(example, 0)
+    example_name = title(apdl_code)
+
+    @pytest.mark.parametrize(
+        "step",
+        [((1, 10), 1), ((2, 1), 2), ((2, 2), 3), ((2, 12), 13), ((2, 21), 22)],
+        scope="class",
+    )
+    def test_compatibility_nodal_displacement(self, mapdl, reader, post, result, step):
+        """This test is particularly problematic because the steps start at the ldstep 1 and substep 10, there is nothing before,
+        hence the cumulative (which seems a sumation of substeps + 1, do not match the set.
+
+        DPF does index according to set as well as reader.
+        To get the same results in post we need to do ``mapdl.set(nset=SET)``
+
+        >>> mapdl.set("list")
+        *****  INDEX OF DATA SETS ON RESULTS FILE  *****
+
+          SET   TIME/FREQ    LOAD STEP   SUBSTEP  CUMULATIVE
+            1 0.10000E-02         1        10        10
+            2 0.20000E-02         2         1        11
+            3 0.30000E-02         2         2        12
+            4 0.40000E-02         2         3        13
+            5 0.50000E-02         2         4        14
+        """
+        loadstep = step[0]
+        set_ = step[1]
+
+        mapdl.set(*loadstep)
+        assert mapdl.post_processing.step == set_
+
+        post_values = post.nodal_displacement("all")[:, :3]
+        result_values = result.nodal_displacement(set_)[1]
+        assert np.allclose(post_values, result_values)
+
+        post_values = post_values[:, :2]
+        result_values = result_values[:, :2]
+        reader_values = reader.nodal_displacement(set_)[
+            1
+        ]  # surprisingly here the array only has two columns
+
+        validate(result_values, reader_values, post_values)
+
+    def test_parse_step_substep(self, result):
+        assert result.parse_step_substep(0) == 0
+        assert result.parse_step_substep(1) == 1
+        with pytest.raises(DPFServerException):
+            assert result.parse_step_substep(2)  # Only two step
+
+        assert result.parse_step_substep((0, 1)) == -1
+        assert result.parse_step_substep((1, 0)) == 1
+        assert result.parse_step_substep((1, 1)) == 2
+        assert result.parse_step_substep((1, 2)) == 3
+        assert result.parse_step_substep((1, 3)) == 4
+        assert result.parse_step_substep((1, 4)) == 5
+        assert result.parse_step_substep((1, 5)) == 6
+        assert result.parse_step_substep((1, 10)) == 11
