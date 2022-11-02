6fb508e58fafac01eaf35ecb75ed0d91b3851875
German <28149841+germa89@users.noreply.github.com>
2022-11-02 12:54:36 +0100

    Improving docstrings in post. (#1584)
    * Improving docstrings in post.
    
    Added also unit tests.
    
    * fixing docstring
    
    * Fixing some docstring format
    
    * more docstring format fixing

--- src/ansys/mapdl/core/_commands/post1_/setup.py
+++ src/ansys/mapdl/core/_commands/post1_/setup.py
@@ -519,35 +519,42 @@ class Setup:
         lstep
             Load step number of the data set to be read (defaults to 1):
 
-            N - Read load step N.
+            N
+              Read load step N.
 
-            FIRST - Read the first data set (Sbstep and TIME are ignored).
+            FIRST
+              Read the first data set (``Sbstep`` and ``TIME`` are ignored).
 
-            LAST - Read the last data set (Sbstep and TIME are ignored).
+            LAST
+              Read the last data set (``Sbstep`` and ``TIME`` are ignored).
 
-            NEXT - Read the next data set (Sbstep and TIME are ignored).  If at the last data set,
-                   the first data set will be read as the next.
+            NEXT
+              Read the next data set (``Sbstep`` and ``TIME`` are ignored). If at the last data set,
+              the first data set will be read as the next.
 
-            PREVIOUS - Read the previous data set (Sbstep and TIME are ignored).  If at the first data
-                       set, the last data set will be read as the previous.
+            PREVIOUS
+              Read the previous data set (``Sbstep`` and ``TIME`` are ignored). If at the first data
+              set, the last data set will be read as the previous.
 
-            NEAR - Read the data set nearest to TIME (Sbstep is ignored).  If TIME is blank, read
-                   the first data set.
+            NEAR
+              Read the data set nearest to ``TIME`` (``Sbstep`` is ignored). If ``TIME`` is blank, read
+              the first data set.
 
-            LIST - Scan the results file and list a summary of each load step.  (KIMG, TIME,
-                   ANGLE, and NSET are ignored.)
+            LIST
+              Scan the results file and list a summary of each load step.  (``KIMG``, ``TIME``,
+              ``ANGLE``, and ``NSET`` are ignored.)
 
-                   .. versionchanged:: 0.64
-                      From version 0.64 you can use the methods ``to_list`` and
-                      ``to_array`` on the object returning from ``mapdl.set("list")``.
+              .. versionchanged:: 0.64
+                From version 0.64 you can use the methods ``to_list`` and
+                ``to_array`` on the object returning from ``mapdl.set("list")``.
 
         sbstep
             Substep number (within Lstep). Defaults to the last substep of the
             load step (except in a buckling or modal analysis). For a buckling
-            (ANTYPE,BUCKLE) or modal (ANTYPE,MODAL) analysis, Sbstep
-            corresponds to the mode number. Specify Sbstep = LAST to store the
+            (``ANTYPE,BUCKLE``) or modal (``ANTYPE,MODAL``) analysis, ``Sbstep``
+            corresponds to the mode number. Specify ``Sbstep = LAST`` to store the
             last substep for the specified load step (that is, issue a
-            SET,Lstep,LAST command).
+            ``SET,Lstep,LAST`` command).
 
         fact
             Scale factor applied to data read from the file. If zero (or
@@ -559,14 +566,18 @@ class Setup:
             Used only with complex results (harmonic and complex modal
             analyses).
 
-            0 or REAL - Store the real part of complex solution (default).
+            0 or REAL
+              Store the real part of complex solution (default).
 
-            1, 2 or IMAG - Store the imaginary part of a complex solution.
+            1, 2 or IMAG
+              Store the imaginary part of a complex solution.
 
-            3 or AMPL - Store the amplitude
+            3 or AMPL
+              Store the amplitude
 
-            4 or PHAS - Store the phase angle. The angle value, expressed in degrees, will be between
-                        -180°  and +180°.
+            4 or PHAS
+              Store the phase angle. The angle value, expressed in degrees,
+              will be between -180°  and +180°.
 
         time
             Time-point identifying the data set to be read.  For a harmonic
@@ -579,41 +590,43 @@ class Setup:
 
         nset
             Data set number of the data set to be read.  If a positive value
-            for NSET is entered, Lstep, Sbstep, KIMG, and TIME are ignored.
-            Available set numbers can be determined by SET,LIST.
+            for ``NSET`` is entered, ``Lstep``, ``Sbstep``, ``KIMG``, and ``TIME`` are ignored.
+            Available set numbers can be determined by ``SET,LIST``.
 
         order
             Key to sort the harmonic index results. This option applies to
             cyclic symmetry buckling and modal analyses only, and is valid only
-            when Lstep = FIRST, LAST, NEXT, PREVIOUS, NEAR or LIST.
+            when ``Lstep = FIRST, LAST, NEXT, PREVIOUS, NEAR or LIST``.
 
-            ORDER  - Sort the harmonic index results in ascending order of eigenfrequencies or
-                     buckling load multipliers.
+            ORDER
+              Sort the harmonic index results in ascending order of eigenfrequencies or
+              buckling load multipliers.
 
-            (blank)  - No sorting takes place.
+            (blank)
+              No sorting takes place.
 
         Notes
         -----
         Defines the data set to be read from the results file into the
         database.  Various operations may also be performed during the read
         operation.  The database must have the model geometry available (or use
-        the RESUME command before the SET command to restore the geometry from
-        Jobname.DB).  Values for applied constraints [D] and loads [F] in the
+        the ``RESUME`` command before the ``SET`` command to restore the geometry from
+        ``<Jobname.DB>``).  Values for applied constraints [``D``] and loads [``F``] in the
         database will be replaced by their corresponding values on the results
-        file, if available. (See the description of the OUTRES command.)  In a
+        file, if available. (See the description of the ``OUTRES`` command.)  In a
         single load step analysis, these values are usually the same, except
         for results from harmonic elements. (See the description of the ANGLE
         value above.)
 
-        In an interactive run, the sorted list (ORDER option) is also available
+        In an interactive run, the sorted list (``ORDER`` option) is also available
         for results-set reading via a GUI pick option.
 
-        You can postprocess results without issuing a SET command if the
-        solution results were saved to the database file (Jobname.DB).
+        You can postprocess results without issuing a ``SET`` command if the
+        solution results were saved to the database file (``<Jobname.DB>``).
         Distributed ANSYS, however, can only postprocess using the results file
-        (for example, Jobname.RST) and cannot use the Jobname.DB file since no
+        (for example, Jobname.RST) and cannot use the ``<Jobname.DB>`` file since no
         solution results are written to the database. Therefore, you must issue
-        a SET command or a RESCOMBINE command before postprocessing in
+        a ``SET`` command or a ``RESCOMBINE`` command before postprocessing in
         Distributed ANSYS.
         """
         command = f"SET,{lstep},{sbstep},{fact},{kimg},{time},{angle},{nset},{order}"
@@ -639,27 +652,33 @@ class Setup:
         lstep
             Load step number of the data set to be read (defaults to 1):
 
-            N - Read load step N.
+            N
+              Read load step N.
 
-            FIRST - Read the first data set (SBSTEP and TIME are ignored).
+            FIRST
+              Read the first data set (``SBSTEP`` and ``TIME`` are ignored).
 
-            LAST - Read the last data set (SBSTEP and TIME are ignored).
+            LAST
+              Read the last data set (``SBSTEP`` and ``TIME`` are ignored).
 
-            NEXT - Read the next data set (SBSTEP and TIME are ignored).  If at the last data set,
-                   the first data set will be read as the next.
+            NEXT
+              Read the next data set (``SBSTEP`` and ``TIME`` are ignored).  If at the last data set,
+              the first data set will be read as the next.
 
-            NEAR - Read the data set nearest to TIME (SBSTEP is ignored).  If TIME is blank, read
-                   the first data set.
+            NEAR
+              Read the data set nearest to ``TIME`` (``SBSTEP`` is ignored).  If ``TIME`` is blank, read
+              the first data set.
 
-            LIST - Scan the results file and list a summary of each load step.  (FACT, KIMG, TIME
-                   and ANGLE are ignored.)
+            LIST
+              Scan the results file and list a summary of each load step.  (``FACT``, ``KIMG``, ``TIME``
+              and ``ANGLE`` are ignored.)
 
         sbstep
-            Substep number (within Lstep).   For the buckling (ANTYPE,BUCKLE)
-            analysis or the modal (ANTYPE,MODAL) analysis, the substep
+            Substep number (within Lstep).   For the buckling (``ANTYPE,BUCKLE``)
+            analysis or the modal (``ANTYPE,MODAL``) analysis, the substep
             corresponds to the mode number.  Defaults to last substep of load
-            step (except for ANTYPE,BUCKLE or MODAL).  If Lstep = LIST, SBSTEP
-            = 0 or 1 lists the basic step information, whereas SBSTEP = 2 also
+            step (except for ``ANTYPE,BUCKLE or MODAL``).  If ``Lstep = LIST, SBSTEP
+            = 0 or 1`` lists the basic step information, whereas ``SBSTEP = 2`` also
             lists the load step title, and labels imaginary data sets if they
             exist.
 
@@ -667,30 +686,32 @@ class Setup:
             Scale factor applied to data read from the file.  If zero (or
             blank), a value of 1.0 is used.  Harmonic velocities or
             accelerations may be calculated from the displacement results from
-            a modal (ANTYPE,MODAL) or harmonic (ANTYPE,HARMIC) analyses.  If
-            FACT = VELO, the harmonic velocities (v) are calculated from the
+            a modal (``ANTYPE,MODAL``) or harmonic (``ANTYPE,HARMIC``) analyses.  If
+            ``FACT = VELO``, the harmonic velocities (v) are calculated from the
             displacements (d) at a particular frequency (f) according to the
-            relationship v = 2 πfd.  Similarly, if FACT = ACEL, the harmonic
+            relationship v = 2 πfd.  Similarly, if ``FACT = ACEL``, the harmonic
             accelerations (a) are calculated as a = (2 πf)2d.
 
         kimg
             Used only with results from complex analyses:
 
-            0 - Store real part of complex solution
+            0
+              Store real part of complex solution
 
-            1 - Store imaginary part.
+            1
+              Store imaginary part.
 
         time
             Time-point identifying the data set to be read.  For harmonic
             analyses, time corresponds to the frequency.  For the buckling
             analysis, time corresponds to the load factor.  Used only in the
-            following cases:  If Lstep is NEAR, read the data set nearest to
-            TIME.   If both Lstep and SBSTEP are zero (or blank), read data set
-            at time = TIME.  If TIME is between two solution time points on the
+            following cases:  If Lstep is ``NEAR``, read the data set nearest to
+            ``TIME``.   If both Lstep and ``SBSTEP`` are zero (or blank), read data set
+            at time = ``TIME``.  If ``TIME`` is between two solution time points on the
             results file, a linear interpolation is done between the two data
             sets.  Solution items not written to the results file [OUTRES] for
             either data set will result in a null item after data set
-            interpolation.  If TIME is beyond the last time point on the file,
+            interpolation.  If ``TIME`` is beyond the last time point on the file,
             use the last time point.
 
         angle
@@ -698,27 +719,27 @@ class Setup:
             circumferential location for the harmonic calculations used when
             reading from the results file.  The harmonic factor (based on the
             circumferential angle) is applied to the harmonic elements
-            (PLANE25, PLANE75, PLANE78, PLANE83, and SHELL61) of the load case.
+            (``PLANE25``, ``PLANE75``, ``PLANE78``, ``PLANE83``, and ``SHELL61``) of the load case.
             See the Mechanical APDL Theory Reference for details.  Note that
             factored values of applied constraints and loads will overwrite any
             values existing in the database.
 
         nset
             Data set number of the data set to be read.  If a positive value
-            for NSET is entered, Lstep, SBSTEP, KIMG, and TIME are ignored.
+            for ``NSET`` is entered, ``Lstep``, ``SBSTEP``, ``KIMG``, and ``TIME`` are ignored.
             Available set numbers can be determined by ``*SET,LIST``.
 
         Notes
         -----
         Reads a data set from the results file into the database for the
         selected portions of the model only.  Data that has not been specified
-        for retrieval from the results file by the INRES command will be listed
-        as having a zero value.  Each time that the SUBSET command is issued,
+        for retrieval from the results file by the ``INRES`` command will be listed
+        as having a zero value.  Each time that the ``SUBSET`` command is issued,
         the data currently in the database will be overwritten with a new set
         of data.  Various operations may also be performed during the read
         operation.  The database must have the model geometry available (or
-        used the RESUME command before the SUBSET command to restore the
-        geometry from File.DB).
+        used the RESUME command before the ``SUBSET`` command to restore the
+        geometry from ``File.DB``).
         """
         command = f"SUBSET,{lstep},{sbstep},{fact},{kimg},{time},{angle},{nset}"
         return self.run(command, **kwargs)
