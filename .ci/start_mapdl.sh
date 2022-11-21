2f9e0f4bd5cf5c3fde8ff993c77a7ead11dda661
German <germanmartinezayuso@gmail.com>
2022-11-21 18:15:29 +0100

    using np

--- .ci/start_mapdl.sh
+++ .ci/start_mapdl.sh
@@ -13,6 +13,6 @@ docker run \
     -p $PYMAPDL_PORT:50052 \
     -p $PYMAPDL_DB_PORT:50055 \
     $MAPDL_IMAGE \
-    -smp > log.txt &
+    -smp -np 2> log.txt &
 grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
 # python -c "from ansys.mapdl.core import launch_mapdl; print(launch_mapdl())"
