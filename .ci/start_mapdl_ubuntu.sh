9392c8af871e3c6ddcb78fe695215ec732c6ead6
German <28149841+germa89@users.noreply.github.com>
2022-11-22 15:21:21 +0100

    Adding Ubuntu docker image as remote (#1636)
    * fixing function to work in remote.
    
    * Updating tests
    
    * Cleaning test before running.
    
    * fixing style
    
    * testing cicd
    
    * fixing some style issues.
    
    * Adding ubuntu to unit testing on remote
    
    * fix
    
    * fixing condition
    
    * using np
    
    * using np
    
    * using np
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * testing
    
    * fixing collect steps

--- .ci/start_mapdl_ubuntu.sh
+++ .ci/start_mapdl_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker pull $MAPDL_IMAGE
 docker run \
+    --entrypoint "/bin/bash" \
     --name mapdl \
     --restart always \
     --health-cmd="ps aux | grep \"[/]ansys_inc/.*ansys\.e.*grpc\" -q && echo 0 || echo 1" \
@@ -12,7 +13,6 @@ docker run \
     -e ANSYS_LOCK="OFF" \
     -p $PYMAPDL_PORT:50052 \
     -p $PYMAPDL_DB_PORT:50055 \
-    $MAPDL_IMAGE \
-    -smp -np 2 > log.txt &
+    $MAPDL_IMAGE /ansys_inc/v222/ansys/bin/mapdl -grpc -dir /jobs -smp -np 2 > log.txt &
 grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
 # python -c "from ansys.mapdl.core import launch_mapdl; print(launch_mapdl())"
