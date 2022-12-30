398dd3cdf89d14da762adb405bc7daf461e00852
German <germanmartinezayuso@gmail.com>
2022-12-30 21:21:42 +0100

    fixing test

--- tests/test_math.py
+++ tests/test_math.py
@@ -38,7 +38,7 @@ def cube_with_damping(mapdl, cleared):
     mapdl.prep7()
     db = os.path.join(lib_path, "model_damping.db")
     mapdl.upload(db)
-    mapdl.resume(db)
+    mapdl.resume("model_damping.db")
     mapdl.mp("dens", 1, 7800 / 0.5)
 
     mapdl.slashsolu()
