80f9d881328650c73dd6624198916c5f3e284609
German <germanmartinezayuso@gmail.com>
2023-01-31 17:00:49 +0000

    Reducing string lenght

--- src/ansys/mapdl/core/mapdl_grpc.py
+++ src/ansys/mapdl/core/mapdl_grpc.py
@@ -1634,10 +1634,10 @@ class MapdlGrpc(_MapdlCore):
         >>> with open('myinput.inp','w').write("/finish\\n/prep7\\n/com, my commands")
         >>> with open('inputtrigger.inp','w').write("/input,myinput,inp")
         >>> mapdl.upload("myinput.inp")
-        Uploading myinput.inp: 100%|██████████████████████████████████████████████████████████████████████████████████████████████████████████| 26.0/26.0 [00:00<00:00, 5.86kB/s]
+        Uploading myinput.inp: 100%|█████████████████████████████████████████████████| 26.0/26.0 [00:00<00:00, 5.86kB/s]
         'myinput.inp'
         >>> mapdl.upload("inputtrigger.inp")
-        Uploading inputtrigger.inp: 100%|█████████████████████████████████████████████████████████████████████████████████████████████████████| 32.0/32.0 [00:00<00:00, 8.92kB/s]
+        Uploading inputtrigger.inp: 100%|████████████████████████████████████████████| 32.0/32.0 [00:00<00:00, 8.92kB/s]
         'inputtrigger.inp'
         >>> with mapdl.non_interactive:
                 mapdl.run("/input,inputtrigger,inp") # This inputs 'myinput.inp'
