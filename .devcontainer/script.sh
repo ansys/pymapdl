46d2095f827286719d5f33fd19dbe9f8518ccd05
German <germanmartinezayuso@gmail.com>
2023-02-05 12:47:58 +0100

    Cleaning script.sh

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
-echo "Activating local environment"
-FILE=./.venv/bin/activate
-if test -f "$FILE"; then
-    echo "Virtual environment ($FILE) exists."
-    echo "Activating..."
-    source $FILE
-    echo "Virtual environment activated"
-else
-    echo "Virtual environment was not found."
-fi
-
-source ./.venv/bin/activate
 
 echo "Checking if the ANSYSLMD_LICENSE_FILE variable is defined"
 
 if [[ -z "${ANSYSLMD_LICENSE_FILE}" ]]; then
-  # MY_SCRIPT_VARIABLE="Some default value because ANSYSLMD_LICENSE_FILE is undefined"
-  echo "ANSYSLMD_LICENSE_FILE is not defined"
+  echo "ANSYSLMD_LICENSE_FILE is not defined."
+
+  # Read port
   read -p "Enter port (default is 1055): " port
   port=${port:-1055}
+
+  # Read license server ip
   read -p "Enter server (i.e. XXX.XX.XX.XX): " server
+
+  # Set env var for this terminal
   export ANSYSLMD_LICENSE_FILE="${server}:${port}"
+
+  # Storing env var in .bashrc file so it is automatically loaded.
   printf "\n%s\n" "ANSYSLMD_LICENSE_FILE=${port}@${server}" >> ~/.bashrc
-  set ANSYSLMD_LICENSE_FILE="${server}:${port}"
+
+  # Activating the new configuration
   source ~/.bashrc
 
 else
-  # MY_SCRIPT_VARIABLE="${ANSYSLMD_LICENSE_FILE}"
-  echo "ANSYSLMD_LICENSE_FILE is defined as ${ANSYSLMD_LICENSE_FILE}"
+  echo "ANSYSLMD_LICENSE_FILE was found."
+  echo "It is defined as ${ANSYSLMD_LICENSE_FILE}"
 fi
