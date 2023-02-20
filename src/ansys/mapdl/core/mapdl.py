b99f9f6052cd2f90430fefb961d5a3f59d8ef8b5
German <germanmartinezayuso@gmail.com>
2023-02-20 19:07:07 +0700

    Removing indexing

--- src/ansys/mapdl/core/mapdl.py
+++ src/ansys/mapdl/core/mapdl.py
@@ -1670,8 +1670,7 @@ class _MapdlCore(Commands):
                 # Size = number of areas.
                 # Values are random between 0 and min(256, number_areas)
                 rng = np.random.default_rng()
-                rand = rng.integers(size_, size=len(anum), dtype=np.uint8)
-                area_color = rand[anum]
+                area_color = rng.integers(size_, size=len(anum), dtype=np.uint8)
                 meshes.append({"mesh": surf, "scalars": area_color})
             else:
                 meshes.append({"mesh": surf, "color": kwargs.get("color", "white")})
