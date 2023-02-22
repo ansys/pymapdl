84ab3e7ee8062173fade7a219901052466179cd2
German <germanmartinezayuso@gmail.com>
2023-02-22 12:18:12 +0700

    Making sure we use floats.

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -499,6 +499,7 @@ def test_find_ansys(mapdl):
     assert find_ansys(version=version) is not None
 
     # Checking floats
+    assert find_ansys(version=22.2) is not None
     assert find_ansys(version=mapdl.version) is not None
 
     with pytest.raises(ValueError):
