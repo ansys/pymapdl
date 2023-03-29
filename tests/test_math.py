4491788994d4a48a9098b7342bd15ee1787ac92e
Kevin Mathew <kevin.koshy@ansys.com>
2023-03-29 19:43:36 +0530

    Update test to fix Issues

--- tests/test_math.py
+++ tests/test_math.py
@@ -260,7 +260,7 @@ def test_getitem_AnsVec(mm, dtype_):
 @pytest.mark.parametrize("dtype_", [np.double, np.complex128])
 def test_kron_product(mm, dtype_):
     mapdl_version = mm._mapdl.version
-    if mapdl_version >= 23.2:
+    if mapdl_version < 23.2:
         pytest.skip("Requires MAPDL 2023 R2 or later.")
 
     m1 = mm.rand(3, 3, dtype=dtype_)
