fbbc53770e5d34820b7f1ec0f7d83e01d5c93546
German <28149841+germa89@users.noreply.github.com>
2023-04-04 23:46:16 +0800

    Applying pyansys/actions v0.4 patch on 0.61 (#1964)
    * Applying patch
    
    * Updating ansys-sphinx-theme
    
    * Patching CI
    
    * Removing nav sidebar and pinning numpy
    
    * Updating docker registry
    
    * changing docker login
    
    * fixing docker login
    
    * fixing login
    
    * fixing dependencies

--- .ci/start_mapdl.sh
+++ .ci/start_mapdl.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $GH_PAT | docker login -u $GH_USERNAME --password-stdin docker.pkg.github.com
 docker pull $MAPDL_IMAGE
 docker run -e ANSYSLMD_LICENSE_FILE=1055@$LICENSE_SERVER --restart always --name mapdl -p $PYMAPDL_PORT:50052 -p $PYMAPDL_DB_PORT:50055 $MAPDL_IMAGE -smp > log.txt &
 grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
