b801e33b9f8757d0fe6132b077bce05cd26ca2f0
German <28149841+germa89@users.noreply.github.com>
2023-05-03 17:54:54 +0200

    Allowing iterables in other XSEL commands (#2030)
    * Allowing iterables in esel, vsel, asel, and lsel
    
    * Adding tests
    
    * using new methods instead of FLST and such
    
    * Adding comment
    
    * renaming duplicated test.
    
    * Fixing not passing arguments to original function

--- tests/test_mapdl.py
+++ tests/test_mapdl.py
@@ -804,7 +804,7 @@ def test_partial_mesh_nnum(mapdl, make_block):
     assert np.allclose(allsel_nnum_old, mapdl.mesh.nnum)
 
 
-def test_partial_mesh_nnum(mapdl, make_block):
+def test_partial_mesh_nnum2(mapdl, make_block):
     mapdl.nsel("S", "NODE", vmin=1, vmax=10)
     mapdl.esel("S", "ELEM", vmin=10, vmax=20)
     assert mapdl.mesh._grid.n_cells == 11
