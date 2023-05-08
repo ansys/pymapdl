ce66416763b4eb1bff057f9f1fef2333a4ca3c4a
German <germanmartinezayuso@gmail.com>
2023-05-08 20:46:49 +0200

    Adding tests

--- tests/test_component.py
+++ tests/test_component.py
@@ -1,8 +1,10 @@
-def test_str_rep(mapdl):
+def test_str_rep(mapdl, cleared):
+    assert len(mapdl.component.__str__().splitlines()) == 2
     mapdl.cm("cm1", "nodes")
     assert "cm1" in mapdl.components
     assert "nodes" in mapdl.components
+    assert len(mapdl.component.__str__().splitlines()) == 3
 
 
-# def test_get_item(mapdl):
-#     mapdl.
+def test_set_item(mapdl, cleared):
+    mapdl.component["mycomp"] = "node", [1, 2, 3]
