3db2e145de96c64520e50c1473d830c1a296a5d1
German <28149841+germa89@users.noreply.github.com>
2023-08-13 20:01:29 +0200

    Adding info about docker and MacOS (#2231)
    * first approach
    
    * Revert "Hiding docker (#1728)"
    
    This reverts commit 282e7d4a2ec9817c9dd5c1100c29cd9658bc9c07.
    
    * Fixing links to new org
    
    * Adding docker and macos sections
    
    * Adding info about OS in versioning
    
    * Revamping docker compose files
    
    * removing number
    
    * Fixing vale
    
    * Apply suggestions from Kathy's code review
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Apply suggestions from Robert's code review
    
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
    
    * Fixing headings
    
    * fixing bullet list
    
    * fix a reference.
    
    * Apply suggestions from Kathy's second code review
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    
    * Missing kathy's comment.
    
    * Fixing vale
    
    * Bringing change.
    
    * Using % for the indexing
    
    * Adding resizing color.
    
    * fixing bullet list
    
    * fixing pypim inventory
    
    * Fixing link
    
    * Update doc/source/getting_started/versioning.rst
    
    * Going back to normal link.
    
    * I had to remove the replaced part because it was giving an error in sphinx.
    
    * Using a new name
    
    * Getting rid of the susbstitutions
    
    * Following advice from Kathy.
    
    * Atempting to fix graphviz
    
    * Improving diagram.
    
    * Last try graphviz
    
    * Increasing timeout
    
    * Removing graphviz
    
    ---------
    
    Co-authored-by: Kathy Pippert <84872299+PipKat@users.noreply.github.com>
    Co-authored-by: Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -105,6 +105,8 @@ intersphinx_mapping = {
 suppress_warnings = ["label.*"]
 # supress_warnings = ["ref.option"]
 
+# Graphviz diagrams configuration
+graphviz_output_format = "png"
 
 # numpydoc configuration
 numpydoc_use_plots = True
@@ -284,6 +286,8 @@ htmlhelp_basename = "pymapdldoc"
 
 # -- Options for LaTeX output ------------------------------------------------
 latex_elements = {}
+
+latex_engine = "xelatex"
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
