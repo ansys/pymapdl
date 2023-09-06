e72e57e3bf6bded008922fea195a3757f74ef566
Camille <78221213+clatapie@users.noreply.github.com>
2023-09-06 17:28:05 +0200

    Apply suggestions from code review
    Co-authored-by: German <28149841+germa89@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,23 +21,18 @@ pyvista.set_error_output_file("errors.txt")
 pyvista.OFF_SCREEN = True
 
 # must be less than or equal to the XVFB window size
-try:
-    pyvista.global_theme.window_size = np.array([1024, 768])
-    # Manage errors
-    pyvista.set_error_output_file("errors.txt")
-    # Ensure that offscreen rendering is used for docs generation
-    pyvista.OFF_SCREEN = True  # Not necessary - simply an insurance policy
-    # Preferred plotting style for documentation
-    pyvista.set_plot_theme("document")
-    pyvista.global_theme.font.size = 22
-    pyvista.global_theme.font.label_size = 22
-    pyvista.global_theme.font.title_size = 22
-    pyvista.global_theme.return_cpos = False
-    pyvista.set_jupyter_backend(None)
-
-except AttributeError:
-    # for compatibility with pyvista < 0.40
-    pyvista.rcParams["window_size"] = np.array([1024, 768])
+pyvista.global_theme.window_size = np.array([1024, 768])
+# Manage errors
+pyvista.set_error_output_file("errors.txt")
+# Ensure that offscreen rendering is used for docs generation
+pyvista.OFF_SCREEN = True  # Not necessary - simply an insurance policy
+# Preferred plotting style for documentation
+pyvista.set_plot_theme("document")
+pyvista.global_theme.font.size = 22
+pyvista.global_theme.font.label_size = 22
+pyvista.global_theme.font.title_size = 22
+pyvista.global_theme.return_cpos = False
+pyvista.set_jupyter_backend(None)
 
 # Save figures in specified directory
 pyvista.FIGURE_PATH = os.path.join(os.path.abspath("./images/"), "auto-generated/")
