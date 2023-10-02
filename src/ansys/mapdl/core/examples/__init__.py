42fb77627d94bba5d5825ad69993c9567b4b5502
German <germanmartinezayuso@gmail.com>
2023-10-02 12:26:20 +0200

    Removing some start imports

--- src/ansys/mapdl/core/examples/__init__.py
+++ src/ansys/mapdl/core/examples/__init__.py
@@ -1,4 +1,2 @@
-from .downloads import *
-from .downloads import _download_rotor_tech_demo_vtk, download_cfx_mapping_example_data
-from .examples import *
+from .downloads import download_cfx_mapping_example_data
 from .verif_files import vmfiles
