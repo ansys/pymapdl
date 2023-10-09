843a811d44c19fff7659f83bbbb9cbf745cb9739
German <germanmartinezayuso@gmail.com>
2023-10-09 12:24:35 +0200

    fixing bat file internal links

--- doc/make.bat
+++ doc/make.bat
@@ -13,8 +13,8 @@ set LINKCHECKDIR=\%BUILDDIR%\linkcheck
 
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
-if "%1" == "clean-all" goto clean-all
 if "%1" == "clean-examples" goto clean-examples
+if "%1" == "clean-except-examples" goto clean-except-examples
 if "%1" == "linkcheck" goto linkcheck
 
 
@@ -35,12 +35,14 @@ if errorlevel 9009 (
 goto end
 
 :clean-except-examples
+echo Cleaning everything except examples
 rmdir /s /q %BUILDDIR% > /NUL 2>&1 
 rmdir /s /q images/auto-generated > /NUL 2>&1 
 for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
 goto end
 
 :clean
+echo Cleaning everything
 rmdir /s /q %BUILDDIR% > /NUL 2>&1 
 rmdir /s /q source\examples\gallery_examples > /NUL 2>&1 
 for /d /r %SOURCEDIR% %%d in (_autosummary) do @if exist "%%d" rmdir /s /q "%%d"
@@ -48,6 +50,7 @@ rmdir /s /q images/auto-generated > /NUL 2>&1
 goto end
 
 :clean-examples
+echo Cleaning examples
 rmdir /s /q source\examples\gallery_examples > /NUL 2>&1
 goto end
 
