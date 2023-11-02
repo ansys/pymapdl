e251109441a15d566aa89be252582f9e239a2bb9
German <germanmartinezayuso@gmail.com>
2023-11-02 15:50:10 +0100

    Moving parres and inquire

--- src/ansys/mapdl/core/mapdl.py
+++ src/ansys/mapdl/core/mapdl.py
@@ -5015,3 +5015,93 @@ class _MapdlCore(Commands):
         self.cmsel("s", "__tmp_cm__", entity=entity)
         self.components.select(cmps_names)
         return output
+
+    @wraps(Commands.inquire)
+    def inquire(self, strarray="", func="", arg1="", arg2=""):
+        """Wraps original INQUIRE function"""
+        func_options = [
+            "LOGIN",
+            "DOCU",
+            "APDL",
+            "PROG",
+            "AUTH",
+            "USER",
+            "DIRECTORY",
+            "JOBNAME",
+            "RSTDIR",
+            "RSTFILE",
+            "RSTEXT",
+            "OUTPUT",
+            "ENVNAME",
+            "TITLE",
+            "EXIST",
+            "DATE",
+            "SIZE",
+            "WRITE",
+            "READ",
+            "EXEC",
+            "LINES",
+        ]
+
+        if strarray.upper() in func_options and func.upper() not in func_options:
+            # Likely you are using the old ``_Mapdl.inquire`` implementation.
+            raise ValueError(
+                "Arguments of this method have changed. `Mapdl.inquire` now includes the optional `strarray` parameter "
+                f"as the first argument. Either use `inquire(func={strarray})`, or `inquire("
+                ", {strarray})`"
+            )
+
+        if func == "":
+            func = "DIRECTORY"
+
+        response = self.run(f"/INQUIRE,{strarray},{func},{arg1},{arg2}", mute=False)
+        if func.upper() in [
+            "ENV",
+            "TITLE",
+        ]:  # the output is multiline, we just need the last line.
+            response = response.splitlines()[-1]
+        if "=" in response:
+            return response.split("=")[1].strip()
+
+        return ""
+
+    @wraps(Commands.parres)
+    def parres(self, lab="", fname="", ext="", **kwargs):
+        """Wraps the original /PARRES function"""
+        if not fname:
+            fname = self.jobname
+
+        fname = self._get_file_name(
+            fname=fname, ext=ext, default_extension="parm"
+        )  # Although documentation says `PARM`
+
+        if self._mode == "grpc":  # grpc mode
+            if self.is_local:
+                # It must be a file!
+                if os.path.isfile(fname):
+                    # And it exist!
+                    filename = os.path.join(os.getcwd(), fname)
+                elif fname in self.list_files():  #
+                    # It exists in the Mapdl working directory
+                    filename = os.path.join(self.directory, fname)
+                elif os.path.dirname(fname):
+                    raise ValueError(
+                        f"'{fname}' appears to be an incomplete directory path rather than a filename."
+                    )
+                else:
+                    # Finally
+                    raise FileNotFoundError(f"Unable to locate filename '{fname}'")
+
+            else:
+                if not os.path.dirname(fname):
+                    # might be trying to run a local file.  Check if the
+                    # file exists remotely.
+                    if fname not in self.list_files():
+                        self.upload(fname, progress_bar=False)
+                else:
+                    self.upload(fname, progress_bar=False)
+                filename = os.path.basename(fname)
+        else:
+            filename = fname
+
+        return self.input(filename)
