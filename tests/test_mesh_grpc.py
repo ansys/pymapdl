8dc36e26ee130aedaabadaa921eb9032efffd456
German <germanmartinezayuso@gmail.com>
2023-11-02 18:45:48 +0100

    Testing mesh parts

--- tests/test_mesh_grpc.py
+++ tests/test_mesh_grpc.py
@@ -104,6 +104,20 @@ def test_empty_mesh(mapdl, cleared):
             mapdl.mesh.save("file.vtk")
 
 
+def test_element_node_components(mapdl, contact_geom_and_mesh):
+    mapdl.allsel()
+    # mapdl.cmsel("all")
+    mapdl.cm("CMNODE", "NODE")
+    mapdl.components["MYELEMCOMP"] = "ELEM", (1, 2, 3)
+
+    assert mapdl.mesh.element_components
+    assert "MYELEMCOMP" in mapdl.mesh.element_components
+
+    assert mapdl.mesh.node_components
+    assert "TN.TGT" in mapdl.mesh.node_components
+    assert "CMNODE" in mapdl.mesh.node_components
+
+
 def test_non_empty_mesh(mapdl, contact_geom_and_mesh):
     assert mapdl.mesh.n_node > 0
     assert mapdl.mesh.n_elem > 0
@@ -134,8 +148,15 @@ def test_non_empty_mesh(mapdl, contact_geom_and_mesh):
     assert mapdl.mesh.tshape_key
     assert len(mapdl.mesh.tshape_key.keys()) > 0
 
-    # assert mapdl.mesh.element_components #Not implemented
-    # assert mapdl.mesh.node_components # Not implemented
+    mapdl.allsel()
+    mapdl.cmsel("all")
+    mapdl.cm("CMNODE", "NODE")
+    mapdl.components["MYELEMCOMP"] = "ELEM", (1, 2, 3)
+
+    assert mapdl.mesh.element_components
+    assert "MYELEMCOMP" in mapdl.mesh.element_components
+    assert mapdl.mesh.node_components
+    assert "CMNODE" in mapdl.mesh.node_components
 
     # bools
     assert mapdl.mesh._has_elements
