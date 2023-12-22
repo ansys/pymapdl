4597ba56e9cdd225542743761a8ca8d571bae727
German <germanmartinezayuso@gmail.com>
2023-12-22 17:35:30 +0100

    test mpi fix

--- .ci/start_mapdl.sh
+++ .ci/start_mapdl.sh
@@ -3,12 +3,12 @@ docker pull $MAPDL_IMAGE
 docker run \
     --name mapdl \
     --restart always \
-    --health-cmd="ps aux | grep \"[/]ansys_inc/.*ansys\.e.*grpc\" -q && echo 0 || echo 1" \
-    --health-interval=0.5s \
-    --health-retries=4 \
-    --health-timeout=0.5s \
-    --health-start-period=10s \
-    -e ANSYSLMD_LICENSE_FILE=1055@$LICENSE_SERVER \
+    # --health-cmd="ps aux | grep \"[/]ansys_inc/.*ansys\.e.*grpc\" -q && echo 0 || echo 1" \
+    # --health-interval=0.5s \
+    # --health-retries=4 \
+    # --health-timeout=0.5s \
+    # --health-start-period=10s \
+    # -e ANSYSLMD_LICENSE_FILE=1055@$LICENSE_SERVER \
     -e ANSYS_LOCK="OFF" \
     -p $PYMAPDL_PORT:50052 \
     -p $PYMAPDL_DB_PORT:50055 \
