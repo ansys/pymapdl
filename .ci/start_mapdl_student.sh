0598d03f053e508d2de90e1a7cf903b6dea9d0ce
German <germanmartinezayuso@gmail.com>
2023-12-26 18:40:23 +0100

    setting number of processors

--- .ci/start_mapdl_student.sh
+++ .ci/start_mapdl_student.sh
@@ -8,5 +8,5 @@ docker run \
     -p "$PYMAPDL_DB_PORT":50055 \
     --shm-size=2gb \
     -e P_SCHEMA=/ansys_inc/v241/ansys/ac4/schema \
-    "$MAPDL_IMAGE" > log.txt &
+    "$MAPDL_IMAGE" -np 2 > log.txt &
 # grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
