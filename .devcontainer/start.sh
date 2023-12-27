667a34c4e5bbac5e10b43530f7f553be9b4c2c48
German <germanmartinezayuso@gmail.com>
2023-12-27 19:51:45 +0000

    Setting python venv

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -3,13 +3,16 @@
 
 mkdir -p /home/mapdl/pymapdl 
 
-echo "Creating virtual environment..."
 
-python3.10 -m venv /home/mapdl/pymapdl/.venv
-source /home/mapdl/pymapdl/.venv/bin/activate
-cd /home/mapdl/pymapdl
+if [ -f /home/mapdl/pymapdl/.venv/bin/activate ] then
+    echo "It seems a venv exists already. Stopping here"
+else
+    echo "Creating virtual environment..."
+    python3.10 -m venv /home/mapdl/pymapdl/.venv
+    source /home/mapdl/pymapdl/.venv/bin/activate
 
-echo "Installing PyMAPDL package and dependencies for development"
-pip install -e '.[tests,doc]
+    echo "Installing PyMAPDL package and dependencies for development"
+    pip install -e '.[tests,doc]'
 
-echo "Done! Enjoy PyMAPDL!"
+    echo "Done! Enjoy PyMAPDL!"
+fi
