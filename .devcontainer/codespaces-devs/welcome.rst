0be668aae42d28b060816700c6e075a068227342
German <28149841+germa89@users.noreply.github.com>
2023-12-28 20:44:15 +0000

    adding codespace for docs

---
+++ .devcontainer/codespaces-devs/welcome.rst
@@ -0,0 +1,5 @@
+Welcome to GitHub PyMAPDL Codespaces for Developers!!
+=====================================================
+
+This codespace has been specifically set for developing PyMAPDL.
+
--- .devcontainer/codespaces-docs/start.sh
+++ .devcontainer/codespaces-docs/start.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 # Script to activate the venv, and install the PyMAPDL
+echo "Starting Documentation codespaces"
 
 echo "Activating virtual environment..."
 source /home/mapdl/pymapdl/.venv/bin/activate
 
 echo "Installing PyMAPDL package and dependencies for development"
 # It should be fast because the image is build with the dependencies installed.
-pip install -e '.[tests]'
+pip install -e '.[doc]'
 
 echo "Setting pre-commit..."
 pre-commit install
