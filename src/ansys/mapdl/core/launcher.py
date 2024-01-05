17a5c131f76db778dda7288b7ecd6840e76440c6
German <germanmartinezayuso@gmail.com>
2024-01-05 16:22:38 +0100

    adding a better check

--- src/ansys/mapdl/core/launcher.py
+++ src/ansys/mapdl/core/launcher.py
@@ -249,11 +249,13 @@ def get_process_at_port(port) -> Optional[psutil.Process]:
     """Get the process (psutil.Process) running at the given port"""
     for proc in psutil.process_iter():
         try:
-            proc.cmdline()  # just to check if we can access the
+            connections = proc.connections(
+                kind="inet"
+            )  # just to check if we can access the
         except psutil.AccessDenied:
             continue
 
-        for conns in proc.connections(kind="inet"):
+        for conns in connections:
             if conns.laddr.port == port:
                 return proc
 
