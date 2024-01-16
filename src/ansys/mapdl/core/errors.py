c56fbdc0118783a0cb74a32a1a89b54876a4eb8f
German <germanmartinezayuso@gmail.com>
2024-01-16 17:04:50 +0100

    Rewriting classes

--- src/ansys/mapdl/core/errors.py
+++ src/ansys/mapdl/core/errors.py
@@ -49,52 +49,76 @@ TYPE_MSG = (
 )
 
 
-class ANSYSDataTypeError(ValueError):
-    """Raised when and invalid data type is sent to APDLMath"""
+## Abraham class
+class MapdlException(Exception):
+    def __init__(self, msg=""):
+        Exception.__init__(self, msg)
 
-    def __init__(self, msg=TYPE_MSG):
+
+## Main subclasses
+class MapdlValueError(MapdlException, ValueError):
+    def __init__(self, msg=""):
         ValueError.__init__(self, msg)
 
 
-class VersionError(ValueError):
-    """Raised when MAPDL is the wrong version"""
-
-    def __init__(self, msg="Invalid MAPDL version"):
-        ValueError.__init__(self, msg)
-
-
-class NoDistributedFiles(FileNotFoundError):
-    """Unable to find any distributed result files"""
-
-    def __init__(self, msg="Unable to find any distributed result files"):
+class MapdlFileNotFoundError(MapdlException, FileNotFoundError):
+    def __init__(self, msg=""):
         FileNotFoundError.__init__(self, msg)
 
 
-class MapdlRuntimeError(RuntimeError):
+class MapdlRuntimeError(MapdlException, RuntimeError):
     """Raised when MAPDL passes an error"""
 
-    pass
+    def __init__(self, msg=""):
+        RuntimeError.__init__(self, msg)
 
 
-class MapdlInvalidRoutineError(RuntimeError):
+class MapdlDeprecationError(MapdlException, DeprecationError):
+    def __init__(self, msg=""):
+        DeprecationError.__init__(self, msg)
+
+
+## Inheritated
+class ANSYSDataTypeError(MapdlValueError):
+    """Raised when and invalid data type is sent to APDLMath"""
+
+    def __init__(self, msg=TYPE_MSG):
+        MapdlValueError.__init__(self, msg)
+
+
+class VersionError(MapdlValueError):
+    """Raised when MAPDL is the wrong version"""
+
+    def __init__(self, msg="Invalid MAPDL version"):
+        MapdlValueError.__init__(self, msg)
+
+
+class NoDistributedFiles(MapdlFileNotFoundError):
+    """Unable to find any distributed result files"""
+
+    def __init__(self, msg="Unable to find any distributed result files"):
+        MapdlFileNotFoundError.__init__(self, msg)
+
+
+class MapdlInvalidRoutineError(MapdlRuntimeError):
     """Raised when MAPDL is in the wrong routine"""
 
     def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
-class MapdlCommandIgnoredError(RuntimeError):
+class MapdlCommandIgnoredError(MapdlRuntimeError):
     """Raised when MAPDL ignores a command."""
 
     def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
-class MapdlExitedError(RuntimeError):
+class MapdlExitedError(MapdlRuntimeError):
     """Raised when MAPDL has exited"""
 
     def __init__(self, msg="MAPDL has exited"):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
 class NotEnoughResources(MapdlExitedError):
@@ -108,18 +132,18 @@ class NotEnoughResources(MapdlExitedError):
         MapdlExitedError.__init__(self, msg.format(resource=resource))
 
 
-class LockFileException(RuntimeError):
+class LockFileException(MapdlRuntimeError):
     """Error message when the lockfile has not been removed"""
 
     def __init__(self, msg=LOCKFILE_MSG):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
-class MapdlDidNotStart(RuntimeError):
+class MapdlDidNotStart(MapdlRuntimeError):
     """Error when the MAPDL process does not start"""
 
     def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
 class PortAlreadyInUse(MapdlDidNotStart):
@@ -138,11 +162,11 @@ class PortAlreadyInUseByAnMAPDLInstance(PortAlreadyInUse):
         PortAlreadyInUse.__init__(self, msg.format(port=port))
 
 
-class MapdlConnectionError(RuntimeError):
+class MapdlConnectionError(MapdlRuntimeError):
     """Provides the error when connecting to the MAPDL instance fails."""
 
     def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
 class LicenseServerConnectionError(MapdlDidNotStart):
@@ -161,87 +185,15 @@ class IncorrectWorkingDirectory(OSError, MapdlRuntimeError):
         MapdlRuntimeError.__init__(self, msg)
 
 
-class DifferentSessionConnectionError(RuntimeError):
+class DifferentSessionConnectionError(MapdlRuntimeError):
     """Provides the error when connecting to the MAPDL instance fails."""
 
     def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
+        MapdlRuntimeError.__init__(self, msg)
 
 
-class DeprecationError(RuntimeError):
+class DeprecationError(MapdlRuntimeError):
     """Provides the error for deprecated commands, classes, interfaces, etc"""
-
-    def __init__(self, msg=""):
-        RuntimeError.__init__(self, msg)
-
-
-# handler for protect_grpc
-def handler(sig, frame):  # pragma: no cover
-    """Pass signal to custom interrupt handler."""
-    logger.info(
-        "KeyboardInterrupt received.  Waiting until MAPDL " "execution finishes"
-    )
-    SIGINT_TRACKER.append(True)
-
-
-def protect_grpc(func):
-    """Capture gRPC exceptions and return a more succinct error message
-
-    Capture KeyboardInterrupt to avoid segfaulting MAPDL.
-
-    This works some of the time, but not all the time.  For some
-    reason gRPC still captures SIGINT.
-
-    """
-
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        """Capture gRPC exceptions and KeyboardInterrupt"""
-
-        # capture KeyboardInterrupt
-        old_handler = None
-        if threading.current_thread().__class__.__name__ == "_MainThread":
-            if threading.current_thread().is_alive():
-                old_handler = signal.signal(signal.SIGINT, handler)
-
-        # Capture gRPC exceptions
-        try:
-            out = func(*args, **kwargs)
-        except (_InactiveRpcError, _MultiThreadedRendezvous) as error:
-            # can't use isinstance here due to circular imports
-            try:
-                class_name = args[0].__class__.__name__
-            except:
-                class_name = ""
-
-            if class_name == "MapdlGrpc":
-                mapdl = args[0]
-            elif hasattr(args[0], "_mapdl"):
-                mapdl = args[0]._mapdl
-
-            # Must close unfinished processes
-            mapdl._close_process()
-            raise MapdlExitedError("MAPDL server connection terminated") from None
-
-        if threading.current_thread().__class__.__name__ == "_MainThread":
-            received_interrupt = bool(SIGINT_TRACKER)
-
-            # always clear and revert to old handler
-            SIGINT_TRACKER.clear()
-            if old_handler:
-                signal.signal(signal.SIGINT, old_handler)
-
-            if received_interrupt:  # pragma: no cover
-                raise KeyboardInterrupt("Interrupted during MAPDL execution")
-
-        return out
-
-    return wrapper
-
-
-class MapdlException(MapdlRuntimeError):
-
-    """General MAPDL exception."""
 
     def __init__(self, msg=""):
         MapdlRuntimeError.__init__(self, msg)
@@ -320,3 +272,67 @@ class CommandDeprecated(MapdlException, DeprecationError):
 
     def __init__(self, msg=""):
         MapdlException.__init__(self, msg)
+
+
+# handler for protect_grpc
+def handler(sig, frame):  # pragma: no cover
+    """Pass signal to custom interrupt handler."""
+    logger.info(
+        "KeyboardInterrupt received.  Waiting until MAPDL " "execution finishes"
+    )
+    SIGINT_TRACKER.append(True)
+
+
+def protect_grpc(func):
+    """Capture gRPC exceptions and return a more succinct error message
+
+    Capture KeyboardInterrupt to avoid segfaulting MAPDL.
+
+    This works some of the time, but not all the time.  For some
+    reason gRPC still captures SIGINT.
+
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """Capture gRPC exceptions and KeyboardInterrupt"""
+
+        # capture KeyboardInterrupt
+        old_handler = None
+        if threading.current_thread().__class__.__name__ == "_MainThread":
+            if threading.current_thread().is_alive():
+                old_handler = signal.signal(signal.SIGINT, handler)
+
+        # Capture gRPC exceptions
+        try:
+            out = func(*args, **kwargs)
+        except (_InactiveRpcError, _MultiThreadedRendezvous) as error:
+            # can't use isinstance here due to circular imports
+            try:
+                class_name = args[0].__class__.__name__
+            except:
+                class_name = ""
+
+            if class_name == "MapdlGrpc":
+                mapdl = args[0]
+            elif hasattr(args[0], "_mapdl"):
+                mapdl = args[0]._mapdl
+
+            # Must close unfinished processes
+            mapdl._close_process()
+            raise MapdlExitedError("MAPDL server connection terminated") from None
+
+        if threading.current_thread().__class__.__name__ == "_MainThread":
+            received_interrupt = bool(SIGINT_TRACKER)
+
+            # always clear and revert to old handler
+            SIGINT_TRACKER.clear()
+            if old_handler:
+                signal.signal(signal.SIGINT, old_handler)
+
+            if received_interrupt:  # pragma: no cover
+                raise KeyboardInterrupt("Interrupted during MAPDL execution")
+
+        return out
+
+    return wrapper
