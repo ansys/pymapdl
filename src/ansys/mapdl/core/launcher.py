36ffa0414097519f8f759952d25596e20b5ca9d0
German <germanmartinezayuso@gmail.com>
2024-03-08 17:09:57 +0100

    Fixing launcher

--- src/ansys/mapdl/core/launcher.py
+++ src/ansys/mapdl/core/launcher.py
@@ -820,6 +820,11 @@ def get_start_instance(start_instance: bool = True):
         )
         return start_instance
 
+    elif start_instance is None:
+        LOG.debug(
+            f"'PYMAPDL_START_INSTANCE' is unset, and there is no supplied value, hence using default value 'True'"
+        )
+        return True  # Default is true
     else:
         raise ValueError("Only booleans are allowed as arguments.")
 
