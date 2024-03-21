d528bd850e5e222b3c89048cf44e77fd632d8991
german <gayuso@ansys.com>
2024-03-21 17:17:07 +0000

    Rising the raise exception

--- src/ansys/mapdl/core/mapdl_extended.py
+++ src/ansys/mapdl/core/mapdl_extended.py
@@ -332,7 +332,7 @@ class _MapdlCommandExtended(_MapdlCore):
         fname_ = fname + "." + ext
         if not self._local:
             if os.path.dirname(fname_):
-                raise IOError(
+                raise FileNotFoundError(
                     "Only writing files to the MAPDL working directory is allowed. "
                     f"The supplied path {fname_} is not allowed."
                 )
