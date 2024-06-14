6d98b2dc3215d149296efd0b888e6da29022ac46
afernand <alex.fernandezluces@ansys.com>
2024-06-14 08:02:06 +0200

    fix: import plotting if it has pyvista

--- src/ansys/mapdl/core/plotting/__init__.py
+++ src/ansys/mapdl/core/plotting/__init__.py
@@ -20,4 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from ansys.mapdl.core.plotting.plotting import *
+from ansys.mapdl.core import _HAS_PYVISTA
+
+if _HAS_PYVISTA:
+    from ansys.mapdl.core.plotting.plotting import *
