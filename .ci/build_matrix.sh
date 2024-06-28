0658a9accfec965e68abf53b7314464d155d5619
German <germanmartinezayuso@gmail.com>
2024-06-28 11:36:47 +0200

    test: checks if github context is accessible from shell script

--- .ci/build_matrix.sh
+++ .ci/build_matrix.sh
@@ -18,20 +18,22 @@ JSON="{\"include\":["
 
 # Loop through each version
 for version in "${versions[@]}"; do
-    echo "Processing version: $version"
-    # echo "::add-matrix name=app::%changed_path%"
-    # Place your command here
-    # For example, you can pull Docker images
-    # docker pull myimage:$version
+    
+    extended_testing="${{ github.event_name == 'schedule' || ( github.event_name == 'workflow_dispatch' && inputs.run_all_tests ) || ( github.event_name == 'push' && contains(github.ref, 'refs/tags') ) }}"
 
+    auth_user=${{ steps.is_organization_member.outputs.result == 'false' }}
 
+    echo "Processing"
+    echo "  - Version: $version"
+    echo "  - extended_testing: $extended_testing"
+    echo "  - auth_user: $auth_user"
 
     # Add build to the matrix only if it is not already included
-    JSONline="{\"mapdl-version\": \"$version\", \"extended_testing\": \"false\"},"
+    JSONline="{\"mapdl-version\": \"$version\", \"extended_testing\": \"$extended_testing\",  \"auth_user\": \"$auth_user\"},"
+
     if [[ "$JSON" != *"$JSONline"* ]]; then
         JSON="$JSON$JSONline"
     fi
-
 
     # Set output
     echo "matrix=$( echo "$JSON" )" >> $GITHUB_OUTPUT
