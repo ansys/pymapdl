8d0177c7e8ecf67703dfa5f1b97defc5ed0431e6
German <germanmartinezayuso@gmail.com>
2024-06-28 12:22:37 +0200

    feat: checking in a script which test should be generated.

--- .ci/build_matrix.sh
+++ .ci/build_matrix.sh
@@ -1,42 +1,80 @@
 # List of versions
 versions=(
-    'v22.2.1'
-    'v22.2-ubuntu'
-    'v23.1.0'
-    'v23.1-ubuntu'
-    'v23.2.0'
-    'v23.2-ubuntu'
+    'latest-ubuntu'
+    'latest-ubuntu-student'
+    'v24.2.0'
     'v24.1.0'
     'v24.1-ubuntu'
     'v24.1-ubuntu-student'
-    'v24.2.0'
-    'latest-ubuntu'
-    'latest-ubuntu-student'
+    'v23.2.0'
+    'v23.2-ubuntu'
+    'v23.1.0'
+    'v23.1-ubuntu'
+    'v22.2.1'
+    'v22.2-ubuntu'
 )
 
 JSON="{\"include\":["
 
+counter=0
 # Loop through each version
 for version in "${versions[@]}"; do
     
-    extended_testing="${{ github.event_name == 'schedule' || ( github.event_name == 'workflow_dispatch' && inputs.run_all_tests ) || ( github.event_name == 'push' && contains(github.ref, 'refs/tags') ) }}"
+    # 1 based counter
+    ((counter++))
 
-    auth_user=${{ steps.is_organization_member.outputs.result == 'false' }}
+    if [[ "${{ matrix.mapdl-version }}" == *"ubuntu"* ]]; then
+        export ON_UBUNTU=true;
+    else 
+        export ON_UBUNTU=false;
+    fi
 
-    echo "Processing"
+    if [[ "${{ matrix.mapdl-version }}" == *"student"* ]]; then
+        export ON_STUDENT=true;
+    else 
+        export ON_STUDENT=false;
+    fi
+
+    echo "Processing $counter"
     echo "  - Version: $version"
     echo "  - extended_testing: $extended_testing"
     echo "  - auth_user: $auth_user"
+    echo "  - Student: $ON_STUDENT"
+    echo "  - Ubuntu: $ON_UBUNTU"
+    echo ""
 
-    # Add build to the matrix only if it is not already included
-    JSONline="{\"mapdl-version\": \"$version\", \"extended_testing\": \"$extended_testing\",  \"auth_user\": \"$auth_user\"},"
+    if [[ $auth_user == "true"]]; then
+        if [[ $extended_testing == "true" ]]; then
+            # runs everything 
+            add_line="true"
+        else
+            # Runs only "latest" and last two versions.
+            # Last 6 registries in list above.
+            if [[ $counter -le 6 ]]; then
+                add_line="true"
+            else
+                add_line="false"
+            fi
+        fi
+    else
+        if [[ $ON_STUDENT == "true" ]]; then
+            add_line="true"
+        else
+            add_line="false"
+    fi;
 
-    if [[ "$JSON" != *"$JSONline"* ]]; then
-        JSON="$JSON$JSONline"
+    if [["$add_line" == "true" ]]; then
+        JSONline="{\"mapdl-version\": \"$version\"},"
+
+        echo "ADDED line: $JSONline"
+
+        # checks that the line is not repeated before adding it.
+        if [[ "$JSON" != *"$JSONline"* ]]; then
+            JSON="$JSON$JSONline"
+        fi
+    else
+        echo "NOT added line"
     fi
-
-    # Set output
-    echo "matrix=$( echo "$JSON" )" >> $GITHUB_OUTPUT
     
 done
 
