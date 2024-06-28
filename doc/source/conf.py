b9019fa99f26cabc24b5dca6a6f58c0347ddc6b1
German <germanmartinezayuso@gmail.com>
2024-06-28 19:24:27 +0200

    feat: adding transparent background configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,9 +23,12 @@ pyvista.OFF_SCREEN = True
 # must be less than or equal to the XVFB window size
 try:
     pyvista.global_theme.window_size = np.array([1024, 768])
+    pyvista.global_theme.transparent_background = True
+
 except AttributeError:
     # for compatibility with pyvista < 0.40
     pyvista.rcParams["window_size"] = np.array([1024, 768])
+    pyvista.rcParams["transparent_background"] = True
 
 # Save figures in specified directory
 pyvista.FIGURE_PATH = os.path.join(os.path.abspath("./images/"), "auto-generated/")
