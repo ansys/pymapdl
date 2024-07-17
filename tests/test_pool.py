0ffdbec87f6e64fcdf4e5659c55d591a76e590e9
German <28149841+germa89@users.noreply.github.com>
2024-07-17 12:54:59 +0200

    fix: removing __del__ method.

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -68,11 +68,6 @@ NPROC = 1
 
 class TestMapdlPool:
 
-    def __del__(self):
-        # making sure we are deleting everything
-        for each_mapdl in self.pool._instances:
-            each_mapdl.exit(force=True)
-
     @pytest.fixture(scope="class")
     def pool_creator(self, tmpdir_factory):
         run_path = str(tmpdir_factory.mktemp("ansys_pool"))
