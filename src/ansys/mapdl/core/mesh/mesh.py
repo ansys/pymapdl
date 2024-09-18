7272b8f079235a88e35b78b3d8497a87b625973b
German <28149841+germa89@users.noreply.github.com>
2024-09-18 13:39:27 +0200

    fix: numpy 2.0.0 raising issue.

--- src/ansys/mapdl/core/mesh/mesh.py
+++ src/ansys/mapdl/core/mesh/mesh.py
@@ -240,7 +240,7 @@ def fix_missing_midside(cells, nodes, celltypes, offset, angles, nnum):
     unique_nodes, idx_a, idx_b = unique_rows(temp_nodes[nnodes:])
 
     # rewrite node numbers
-    cells[mask] = idx_b + nnodes
+    cells[mask] = (idx_b + nnodes).ravel()
     nextra = idx_a.shape[0]  # extra unique nodes
     nodes_new = nodes_new[: nnodes + nextra]
     nodes_new[nnodes:] = unique_nodes
