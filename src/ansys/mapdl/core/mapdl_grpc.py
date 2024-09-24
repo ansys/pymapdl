594ee189139b81c3c68f4cb0a5ef1dce5781136a
German <28149841+germa89@users.noreply.github.com>
2024-09-24 12:02:34 +0200

    feat: cache directory at starting

--- src/ansys/mapdl/core/mapdl_grpc.py
+++ src/ansys/mapdl/core/mapdl_grpc.py
@@ -899,6 +899,7 @@ class MapdlGrpc(MapdlBase):
         with self.run_as_routine("POST26"):
             self.numvar(200, mute=True)
 
+        self.inquire("", "DIRECTORY")
         self.show(self._file_type_for_plots)
         self.version  # Caching version
         self.file_type_for_plots  # Setting /show,png and caching it.
