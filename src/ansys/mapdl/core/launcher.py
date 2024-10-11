9812f6835d34738d46ad7851f16d3dd34e4389a6
Camille <78221213+clatapie@users.noreply.github.com>
2024-10-11 15:09:24 +0200

    fix: removing `"` character

--- src/ansys/mapdl/core/launcher.py
+++ src/ansys/mapdl/core/launcher.py
@@ -596,7 +596,7 @@ def launch_grpc(
     # removing spaces in cells
     command = " ".join(command_parm)
     command_parm = command.split()
-    command_parm.insert(0, f'"{exec_file}"')
+    command_parm.insert(0, f"{exec_file}")
     command_parm.insert(0, "+x")
     command_parm.insert(0, "chmod")
     command = " ".join(command_parm)
