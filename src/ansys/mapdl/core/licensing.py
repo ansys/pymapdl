8304fd4e854a12ab1161e8278e94f6c30e4c74b4
Camille <78221213+clatapie@users.noreply.github.com>
2024-10-21 14:53:32 +0200

    fix: ``_checkout_license``

--- src/ansys/mapdl/core/licensing.py
+++ src/ansys/mapdl/core/licensing.py
@@ -329,7 +329,7 @@ class LicenseChecker:
 
         tstart = time.time()
         process = subprocess.Popen(
-            [f"'{ansysli_util_path}'", "-checkout", f"{lic}"],
+            [f'"{ansysli_util_path}"', "-checkout", f"{lic}"],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             env=env,
