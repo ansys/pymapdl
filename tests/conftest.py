a750e5408bc07c3456ea5b2dabc10ae0025ab800
German <28149841+germa89@users.noreply.github.com>
2024-11-20 12:05:05 +0100

    refactor: removing pytest warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -316,7 +316,7 @@ class MyReporter(TerminalReporter):
             )
 
 
-@pytest.mark.trylast
+@pytest.hookimpl(trylast=True)
 def pytest_configure(config):
     vanilla_reporter = config.pluginmanager.getplugin("terminalreporter")
     my_reporter = MyReporter(config)
