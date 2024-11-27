d7f00ed00b363ecd39948eb143d42986600156e6
German <28149841+germa89@users.noreply.github.com>
2024-11-27 18:56:00 +0000

    fix: post_processing repr

--- src/ansys/mapdl/core/post.py
+++ src/ansys/mapdl/core/post.py
@@ -138,20 +138,20 @@ class PostProcessing:
 
     @supress_logging
     def __repr__(self):
-        with self._mapdl.run_as_routine("POST1"):
-            info = "PyMAPDL PostProcessing Instance\n"
-            info += f"\tActive Result File:    {self.filename}\n"
+        info = "PyMAPDL PostProcessing Instance\n"
+        info += f"\tActive Result File:    {self.filename}\n"
 
-            # If there is no result file, this fails.
-            try:
-                nsets = int(self.nsets)
-            except MapdlRuntimeError:
-                nsets = "NA"
+        # If there is no result file, this fails.
+        try:
+            nsets = int(self.nsets)
+        except MapdlRuntimeError:
+            nsets = "NA"
 
-            info += f"\tNumber of result sets: {nsets}\n"
-            info += f"\tCurrent load step:     {self.load_step}\n"
-            info += f"\tCurrent sub step:      {self.sub_step}\n"
+        info += f"\tNumber of result sets: {nsets}\n"
+        info += f"\tCurrent load step:     {self.load_step}\n"
+        info += f"\tCurrent sub step:      {self.sub_step}\n"
 
+        if self._mapdl.parameters.routine == "POST1":
             try:
                 nlist = self._mapdl.set("LIST")
             except MapdlRuntimeError as err:
@@ -163,10 +163,9 @@ class PostProcessing:
                 else:
                     raise err
 
-            if self._mapdl.parameters.routine == "POST1":
-                info += "\n\n" + nlist
-            else:
-                info += "\n\nEnable routine POST1 to see a table of available results"
+            info += "\n\n" + nlist
+        else:
+            info += "\n\nEnable routine POST1 to see a table of available results"
 
         return info
 
