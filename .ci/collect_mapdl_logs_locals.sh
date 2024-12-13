c513f14cff5171246dc27c606135ad644582af65
German <28149841+germa89@users.noreply.github.com>
2024-12-13 18:28:24 +0100

    ci: adding logs to console

--- .ci/collect_mapdl_logs_locals.sh
+++ .ci/collect_mapdl_logs_locals.sh
@@ -2,8 +2,8 @@
 mkdir "$LOG_NAMES" && echo "Successfully generated directory $LOG_NAMES"
 
 cp *.log ./"$LOG_NAMES"/ || echo "No log files could be found"
-cp apdl.out ./"$LOG_NAMES"/ || echo "No APDL log files could be found"
-cp pymapdl.apdl ./"$LOG_NAMES"/ || echo "No PYMAPDL APDL log files could be found"
+cp *apdl.out ./"$LOG_NAMES"/ || echo "No APDL log files could be found"
+cp *pymapdl.apdl ./"$LOG_NAMES"/ || echo "No PYMAPDL APDL log files could be found"
 
 
 ls -la ./"$LOG_NAMES"
