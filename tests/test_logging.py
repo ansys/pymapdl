121c3d065effbc7d5dc27d842661ee9cf701ad12
German <28149841+germa89@users.noreply.github.com>
2024-12-18 18:00:20 +0100

    feat: debug testing  (#3594)
    * feat: using an envvar to activate testing debugging
    
    * chore: adding changelog file 3594.maintenance.md [dependabot-skip]
    
    * fix: test logging name too long
    
    * fix: detecting comments as parameters sets when they have =.
    
    * fix: apply suggestions from code review
    
    * fix: script path name
    
    * fix: test
    
    * ci: using an external file for minimal requirements
    
    * fix: tests
    
    * refactor: moving dependency to requirement file
    
    * ci: fix files names.
    
    * fix: test
    
    * ci: adding logs to console
    
    ---------
    
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -287,7 +287,12 @@ def test_log_to_file(tmpdir):
     LOG.logger.setLevel("ERROR")
     LOG.std_out_handler.setLevel("ERROR")
 
-    if not LOG.file_handler:
+    if LOG.file_handler is None:
+        old_logger = None
+        LOG.log_to_file(file_path)
+    else:
+        # the logger has been already instantiated
+        old_logger = LOG.file_handler.baseFilename
         LOG.log_to_file(file_path)
 
     LOG.error(file_msg_error)
@@ -312,6 +317,9 @@ def test_log_to_file(tmpdir):
         text = "".join(fid.readlines())
 
     assert file_msg_debug in text
+
+    if old_logger is not None:
+        LOG.log_to_file(old_logger)
 
 
 def test_log_instance_name(mapdl, cleared):
