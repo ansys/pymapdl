0c98fb249c8e8f3a4ab614ca8b5673d40166370a
German <28149841+germa89@users.noreply.github.com>
2024-12-19 14:33:57 +0100

    tests: skipping dpf on 252

--- tests/test_database.py
+++ tests/test_database.py
@@ -54,7 +54,7 @@ def db(mapdl):
         and ON_CI
     ):
         pytest.skip(
-            f"This MAPDL version ({mapdl_version}) docker image seems to not support DB, but local does."
+            f"This MAPDL version ({mapdl_version}) docker image seems to not support DB on CICD."
         )
 
     if mapdl._server_version < (0, 4, 1):  # 2021R2
