acb1dbffa70339155c40de7b141ef41345659b3b
German <28149841+germa89@users.noreply.github.com>
2025-01-20 13:41:41 +0100

    refactor: make cli testing not depending on MAPDL.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,7 +22,9 @@
 
 import re
 import subprocess
+from unittest.mock import MagicMock, patch
 
+import numpy as np
 import psutil
 import pytest
 
@@ -32,6 +34,26 @@ if VALID_PORTS:
     PORT1 = max(VALID_PORTS) + 1
 else:
     PORT1 = 50090
+
+
+def make_fake_process(pid, name, port=PORT1, ansys_process=False, n_children=0):
+    mock_process = MagicMock(spec=psutil.Process)
+    mock_process.pid = pid
+    mock_process.name.return_value = name
+    mock_process.status.return_value = psutil.STATUS_RUNNING
+    mock_process.children.side_effect = lambda *arg, **kwargs: [
+        i for i in range(n_children)
+    ]
+    mock_process.cwd.return_value = f"/cwd/of/{name}"
+
+    if ansys_process:
+        mock_process.cmdline.return_value = (
+            f"/ansys_inc/v251/ansys/bin/ansys251 -grpc -port {port}".split(" ")
+        )
+    else:
+        mock_process.cmdline.return_value = f"/path/to/process".split(" ")
+
+    return mock_process
 
 
 @pytest.fixture
@@ -44,7 +66,7 @@ def run_cli():
         from ansys.mapdl.core.cli import main
 
         if arguments:
-            args = list(arguments.split(" "))
+            args = list(arguments.strip().split(" "))
         else:
             args = []
 
@@ -58,8 +80,6 @@ def run_cli():
 
 
 @requires("click")
-@requires("local")
-@requires("nostudent")
 @pytest.mark.parametrize("start_instance", [None, True, False])
 def test_launch_mapdl_cli(monkeypatch, run_cli, start_instance):
     if start_instance is not None:
@@ -67,25 +87,163 @@ def test_launch_mapdl_cli(monkeypatch, run_cli, start_instance):
     else:
         monkeypatch.delenv("PYMAPDL_START_INSTANCE", raising=False)
 
-    # Setting a port so it does not collide with the already running instance for testing
-    output = run_cli(f"start --port {PORT1}")
+    with (
+        patch("ansys.mapdl.core.launcher.launch_mapdl") as mock_launch,
+        patch("ansys.mapdl.core.launcher.submitter") as mock_submitter,
+    ):  # test we are not calling Popen
+
+        mock_launch.side_effect = lambda *args, **kwargs: (
+            "123.45.67.89",
+            str(PORT1),
+            "123245",
+        )
+
+        # Setting a port so it does not collide with the already running instance for testing
+        output = run_cli(f"start --port {PORT1}")
 
     assert "Success: Launched an MAPDL instance " in output
     assert str(PORT1) in output
 
     # grab ips and port
+    pid = re.search(r"\(PID=(\d+)\)", output)
+    assert pid
     pid = int(re.search(r"\(PID=(\d+)\)", output).groups()[0])
+    assert pid != 0
 
-    output = run_cli(f"stop --port {PORT1}")
-    assert "success" in output.lower()
+
+@pytest.mark.parametrize(
+    "mapping",
+    ((False, True), (False, True, False), (False, False, False), (True, True, False)),
+)
+def test_pymapdl_stop_instances(run_cli, mapping):
+
+    fake_processes = [
+        make_fake_process(
+            np.random.randint(10000, 100000),
+            name="ansys251" if each else "process",
+            ansys_process=each,
+        )
+        for ind, each in enumerate(mapping)
+    ]
+
+    with (
+        patch("ansys.mapdl.core.cli.stop._kill_process") as mock_kill,
+        patch("psutil.pid_exists") as mock_pid,
+        patch("psutil.process_iter", return_value=iter(fake_processes)),
+    ):
+
+        mock_pid.return_value = lambda *args, **kwargs: True  # All process exists
+        mock_kill.side_effect = lambda *args, **kwargs: None  # avoid kill nothing
+
+        if sum(mapping) == 0:
+            output = run_cli(f"stop --port {PORT1}")
+            assert (
+                f"error: no ansys instances running on port {PORT1}" in output.lower()
+            )
+            mock_kill.assert_not_called()
+
+            output = run_cli(f"stop --all")
+            assert f"error: no ansys instances have been found." in output.lower()
+            mock_kill.assert_not_called()
+
+        elif sum(mapping) == 1:
+            output = run_cli(f"stop --port {PORT1}")
+            assert (
+                f"success: ansys instances running on port {PORT1} have been stopped"
+                in output.lower()
+            )
+            mock_kill.assert_called()
+        else:
+            output = run_cli(f"stop --all")
+            assert "success: ansys instances have been stopped." in output.lower()
+            assert mock_kill.call_count == sum(mapping)
 
 
 @requires("click")
-@requires("local")
-@requires("nostudent")
-def test_launch_mapdl_cli_config(run_cli):
-    cmds_ = ["start", f"--port {PORT1}", "--jobname myjob"]
-    cmd_warnings = [
+@pytest.mark.parametrize(
+    "arg,check",
+    (
+        ("", {}),  # default
+        ("-i", {"Is Instance": False}),
+        ("-c", {"Command line": True}),
+        ("-l", {"Command line": True, "Working directory": True}),
+        ("-cwd", {"Working directory": True}),
+        ("--instances", {"Is Instance": False}),
+        ("--cmd", {"Command line": True}),
+        ("--long", {"Command line": True, "Working directory": True}),
+        ("--location", {"Working directory": True}),
+    ),
+)
+@patch("psutil.pid_exists", lambda *args, **kwargs: True)
+def test_launch_mapdl_cli_list(run_cli, arg, check):
+
+    mapping = (False, True, False, True, False, False)
+    is_instance = (False, False, False, True, False, False)
+
+    fake_process_ = [
+        {
+            "pid": np.random.randint(10000, 100000),
+            "name": f"ansys251_{ind}" if each else "process",
+            "port": str(50052 + ind),
+            "ansys_process": each,
+            "n_children": 4 if each_ins else 1,
+        }
+        for ind, (each, each_ins) in enumerate(zip(mapping, is_instance))
+    ]
+
+    fake_processes = [make_fake_process(**each) for each in fake_process_]
+
+    checks_defaults = {
+        "Is Instance": True,
+        "Command line": False,
+        "Working directory": False,
+    }
+
+    with patch("psutil.process_iter", return_value=iter(fake_processes)):
+        output = run_cli(" ".join(["list", arg]))
+
+        assert "running" in output or "sleeping" in output
+        checks_defaults.update(check)
+
+        for each, value in checks_defaults.items():
+            if value:
+                assert each in output
+            else:
+                assert each not in output
+
+        assert len(output.splitlines()) > 2
+
+        if arg in ["-c", "--cmd"]:
+            assert "/ansys_inc/v251/ansys/bin/ansys251" in output
+
+        if arg in ["-cwd", "--location"]:
+            assert "/cwd/of/ansys251" in output
+
+        if arg in ["-i", "--instances"]:
+            assert len(output.splitlines()) == sum(is_instance) + 2
+        else:
+            assert len(output.splitlines()) == sum(mapping) + 2
+
+        if arg in ["-l", "--long"]:
+            assert "/ansys_inc/v251/ansys/bin/ansys251" in output.lower()
+
+        for ind, (each, each_ins) in enumerate(zip(mapping, is_instance)):
+            proc_options = fake_process_[ind]
+            # is ansys
+            if not each or (not each_ins and arg in ["-i", "--instances"]):
+                assert proc_options["name"] not in output
+                assert str(proc_options["pid"]) not in output
+                assert proc_options["port"] not in output
+            else:
+                assert proc_options["name"] in output
+                assert str(proc_options["pid"]) in output
+                assert proc_options["port"] in output
+
+
+@requires("click")
+@pytest.mark.parametrize(
+    "arg",
+    [
         "ip",
         "license_server_check",
         "mode",
@@ -98,93 +256,35 @@ def test_launch_mapdl_cli_config(run_cli):
         "print_com",
         "add_env_vars",
         "replace_env_vars",
-    ]
+    ],
+)
+def test_launch_mapdl_cli_config(run_cli, arg):
+    cmd = " ".join(["start", f"--port {PORT1}", "--jobname myjob", f"--{arg} True"])
 
-    cmd = " ".join(cmds_)
-    cmd_warnings_ = ["--" + each + " True" for each in cmd_warnings]
+    with (
+        patch("ansys.mapdl.core.launcher.launch_mapdl") as mock_launch,
+        patch("ansys.mapdl.core.launcher.submitter") as mock_submitter,
+    ):  # test we are not calling Popen
+        mock_launch.side_effect = lambda *args, **kwargs: (
+            "123.45.67.89",
+            str(PORT1),
+            "123245",
+        )
 
-    cmd = cmd + " " + " ".join(cmd_warnings_)
-
-    try:
         output = run_cli(cmd)
+
+        mock_submitter.assert_not_called()
+        kwargs = mock_launch.call_args_list[0].kwargs
+        assert str(kwargs["port"]) == str(PORT1)
 
         assert "Launched an MAPDL instance" in output
         assert str(PORT1) in output
 
         # assert warnings
-        for each in cmd_warnings:
-            assert (
-                f"The following argument is not allowed in CLI: '{each}'" in output
-            ), f"Warning about '{each}' not printed"
-
-        # grab ips and port
-        pid = int(re.search(r"\(PID=(\d+)\)", output).groups()[0])
-        p = psutil.Process(pid)
-        cmdline = " ".join(p.cmdline())
-
-        assert str(PORT1) in cmdline
-        assert "myjob" in cmdline
-
-    finally:
-        output = run_cli(f"stop --port {PORT1}")
-        assert "Success" in output
+        assert arg not in kwargs
         assert (
-            f"Success: Ansys instances running on port {PORT1} have been stopped"
-            in output
-        )
-
-
-@requires("click")
-@requires("local")
-@requires("nostudent")
-@pytest.mark.xfail(reason="Flaky test. See #2435")
-def test_launch_mapdl_cli_list(run_cli):
-
-    output = run_cli(f"start --port {PORT1}")
-
-    assert "Success: Launched an MAPDL instance " in output
-    assert str(PORT1) in output
-
-    output = run_cli("list")
-    assert "running" in output or "sleeping" in output
-    assert "Is Instance" in output
-    assert len(output.splitlines()) > 2
-    assert "ansys" in output.lower() or "mapdl" in output.lower()
-
-    output = run_cli("list -i")
-    assert "running" in output or "sleeping" in output
-    assert "Is Instance" not in output
-    assert len(output.splitlines()) > 2
-    assert "ansys" in output.lower() or "mapdl" in output.lower()
-
-    output = run_cli("list -c")
-    assert "running" in output or "sleeping" in output
-    assert "Command line" in output
-    assert "Is Instance" in output
-    assert len(output.splitlines()) > 2
-    assert "ansys" in output.lower() or "mapdl" in output.lower()
-
-    output = run_cli("list -cwd")
-    assert "running" in output or "sleeping" in output
-    assert "Command line" not in output
-    assert "Working directory" in output
-    assert "Is Instance" in output
-    assert len(output.splitlines()) > 2
-    assert "ansys" in output.lower() or "mapdl" in output.lower()
-
-    output = run_cli("list -l")
-    assert "running" in output or "sleeping" in output
-    assert "Is Instance" in output
-    assert "Command line" in output
-    assert len(output.splitlines()) > 2
-    assert "ansys" in output.lower() or "mapdl" in output.lower()
-
-    output = run_cli(f"stop --port {PORT1}")
-    assert "Success" in output
-    assert str(PORT1) in output
-    assert (
-        f"Success: Ansys instances running on port {PORT1} have been stopped" in output
-    )
+            f"The following argument is not allowed in CLI: '{arg}'" in output
+        ), f"Warning about '{arg}' not printed"
 
 
 @requires("click")
