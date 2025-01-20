0738ba86988642a142d85d316994ecdbec515881
German <28149841+germa89@users.noreply.github.com>
2025-01-20 19:48:23 +0100

    fix: test

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -633,7 +633,7 @@ def test_converter_cli(tmpdir, run_cli):
     with input_file.open("w") as f:
         f.write(content)
 
-    assert run_cli("-f " + str(input_file)) == 0
+    assert run_cli(f"-f {input_file} -o {output_file}") == 0
 
     assert os.path.exists(output_file)
     with output_file.open("r") as f:
