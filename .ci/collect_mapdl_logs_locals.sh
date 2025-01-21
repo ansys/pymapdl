9d62d356f85781a0a0a2345c255323da3fbe73a9
German <28149841+germa89@users.noreply.github.com>
2025-01-21 12:58:27 +0100

    fix: not attaching prof files

--- .ci/collect_mapdl_logs_locals.sh
+++ .ci/collect_mapdl_logs_locals.sh
@@ -7,7 +7,7 @@ cp *apdl.out ./"$LOG_NAMES"/ || echo "No APDL log files could be found"
 cp *pymapdl.apdl ./"$LOG_NAMES"/ || echo "No PYMAPDL APDL log files could be found"
 
 echo "Copying the profiling files..."
-cp prof ./"$LOG_NAMES"/prof || echo "No profile files could be found"
+cp -r prof ./"$LOG_NAMES"/prof || echo "No profile files could be found"
 
 
 ls -la ./"$LOG_NAMES"
