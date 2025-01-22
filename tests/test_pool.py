3c0359b5ebc4dd3f9712a50e1ee2afc1c7a7bb39
German <28149841+germa89@users.noreply.github.com>
2025-01-22 12:33:15 +0100

    ci: marking test as flacky. Ref https://github.com/ansys/pymapdl/issues/2435 #2435

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -292,6 +292,7 @@ class TestMapdlPool:
         assert all(["my_instance" in each for each in dirs_path_pool])
 
     @skip_if_ignore_pool
+    @pytest.mark.xfail(reason="Flaky test. See #2435")
     def test_directory_names_function(self, tmpdir):
         def myfun(i):
             if i == 0:
