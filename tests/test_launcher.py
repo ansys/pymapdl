103d141139485c708ba4a51445a66471606d9131
German <28149841+germa89@users.noreply.github.com>
2025-02-25 18:58:54 +0100

    refactor: pytest mark. Removing only-gui

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -403,7 +403,7 @@ def test_env_injection():
         update_env_vars(None, "asdf")
 
 
-@requires("requires_gui")
+@requires("gui")
 @pytest.mark.parametrize(
     "include_result,inplace,to_check",
     (
