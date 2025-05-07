b1944672fdd1749b9c6537ea84e11e077a05d700
German <28149841+germa89@users.noreply.github.com>
2025-05-07 13:28:51 +0200

    fix: replace cp with mv for log file handling in local and remote scripts

--- .ci/collect_mapdl_logs_locals.sh
+++ .ci/collect_mapdl_logs_locals.sh
@@ -2,15 +2,15 @@
 mkdir "$LOG_NAMES" && echo "Successfully generated directory $LOG_NAMES"
 
 echo "Copying the log files..."
-cp *.log ./"$LOG_NAMES"/ || echo "No log files could be found"
-cp *apdl.out ./"$LOG_NAMES"/ || echo "No APDL log files could be found"
-cp *pymapdl.apdl ./"$LOG_NAMES"/ || echo "No PYMAPDL APDL log files could be found"
+mv *.log ./"$LOG_NAMES"/ || echo "No log files could be found"
+mv *apdl.out ./"$LOG_NAMES"/ || echo "No APDL log files could be found"
+mv *pymapdl.apdl ./"$LOG_NAMES"/ || echo "No PYMAPDL APDL log files could be found"
 
 echo "Copying the profiling files..."
-cp -r prof ./"$LOG_NAMES"/prof || echo "No profile files could be found"
+mv -r prof ./"$LOG_NAMES"/prof || echo "No profile files could be found"
 
 echo "Copying the JSONL files..."
-cp *.jsonl ./"$LOG_NAMES"/ || echo "No JSONL files could be found"
+mv *.jsonl ./"$LOG_NAMES"/ || echo "No JSONL files could be found"
 
 ls -la ./"$LOG_NAMES"
 
