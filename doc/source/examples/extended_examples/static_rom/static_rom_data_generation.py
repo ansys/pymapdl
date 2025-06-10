8ed05f398532fd7c752fca56a218de713dea4127
EDCarman <edward.carman@ansys.com>
2025-06-10 08:27:35 +0100

    docs: remove local .db file, refactor script

--- doc/source/examples/extended_examples/static_rom/static_rom_data_generation.py
+++ doc/source/examples/extended_examples/static_rom/static_rom_data_generation.py
@@ -21,35 +21,14 @@
 # SOFTWARE.
 
 """
-.. _ref_static_rom_data_generation:
-
 Creating training data for a 3D Static reduced order model (ROM)
 ----------------------------------------------------------------
 
 This example is an extension of the example, :ref:`ref_3d_plane_stress_concentration`.
 It shows how a parametric sweep may be run and the output displacement and stress data exported into
 the format required to build a Static ROM with Ansys Twin Builder.
-
-The final data structure for the ROM building is shown in Figure 1.
-
-.. figure:: images/static_ROM_file_structure.png
-    :align: center
-    :width: 600
-    :alt:  Static ROM creation files.
-    :figclass: align-center
-
-    **Figure 1: Organization of files and directories for static ROM creation.**
-
-Additional Packages Used
-~~~~~~~~~~~~~~~~~~~~~~~~
-
-* `NumPy <https://numpy.org>`_ is used for using NumPy arrays.
-* `PyTwin <https://twin.docs.pyansys.com>`_ is used convert result data to binary snapshots.
-
-
 """
 
-# Assumes working locally. See :ref:`ref_dpf_basic_example` for other options.
 import json
 from pathlib import Path
 
@@ -57,12 +36,10 @@ from ansys.dpf import core as dpf
 import numpy as np
 from pytwin import write_binary
 
-from ansys.mapdl.core import launch_mapdl
+from ansys.mapdl.core import Mapdl, launch_mapdl
+from ansys.mapdl.core.examples.downloads import download_example_data
 
 
-###############################################################################
-# Helper functions
-# ~~~~~~~~~~~~
 def compress_id_list(id_list: np.ndarray):
     """
     Compress array of consecutive IDs.
@@ -95,7 +72,9 @@ def compress_id_list(id_list: np.ndarray):
     return result
 
 
-def write_settings(path: str | Path, field: dpf.Field, name: str, is_deformation=False):
+def write_settings(
+    path: str | Path, field: dpf.Field, name: str, is_deformation: bool = False
+):
     """Write the settings.json file."""
 
     if field.component_count == 1 or field.component_count == 3:
@@ -142,7 +121,6 @@ def get_scoping(model: dpf.Model):
 
 def write_points(model: dpf.Model, scoping: dpf.Scoping, output_folder: str | Path):
     """Write points.bin file."""
-    # Get node locations
     nodes = model.metadata.meshed_region.nodes
     scoped_node_indices, _ = nodes.map_scoping(scoping)
     points_coordinates = nodes.coordinates_field.data[scoped_node_indices]
@@ -151,7 +129,6 @@ def write_points(model: dpf.Model, scoping: dpf.Scoping, output_folder: str | Pa
 
 def write_doe_headers(output_folder: str | Path, name: str, parameters: dict):
     """Write blank doe.csv file with headers."""
-    # Write doe.csv headers
     with open(Path(output_folder).joinpath("doe.csv"), "w") as fw:
         parameter_headers = ",".join([str(key) for key in parameters.keys()])
         fw.write(f"{name},{parameter_headers}\n")
@@ -159,7 +136,6 @@ def write_doe_headers(output_folder: str | Path, name: str, parameters: dict):
 
 def write_doe_entry(output_folder: str | Path, snapshot_name: str, parameters: dict):
     """Write entry to doe.csv file."""
-    # Write doe.csv headers
     with open(Path(output_folder).joinpath("doe.csv"), "a") as fw:
         parameter_values = ",".join([str(value) for value in parameters.values()])
         fw.write(f"{snapshot_name},{parameter_values}\n")
@@ -187,8 +163,9 @@ def export_static_ROM_data(
     else:
         raise ValueError(f"Unsupported result type: {name}")
 
-    # To retrieve displacement and stress at last result set, scoped to corner nodes.
+    # Retrieve displacement and stress at last result set.
     scoped_result = result.on_last_time_freq.on_mesh_scoping(scoping)
+
     # Result must be sorted by scoping to ensure consistency across outputs.
     sorted_result = dpf.operators.logic.ascending_sort_fc(
         scoped_result, sort_by_scoping=True
@@ -208,32 +185,8 @@ def export_static_ROM_data(
     write_binary(snapshot_folder.joinpath(snap_name), result_field.data)
 
 
-###############################################################################
-# Launch MAPDL
-# ~~~~~~~~~~~~
-# First, start MAPDL as a service and disable all but error messages.
-mapdl = launch_mapdl(loglevel="ERROR")
-
-###############################################################################
-# Download and resume the 3D notch database
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# The 3D notch model was generated as shown in
-# :ref:`ref_3d_plane_stress_concentration` and a nodal component, 'load_node',
-# was created for the node to which the load is applied, using the commands
-# below:
-#
-# .. code-block:: python
-#
-#     mapdl.nsel("S", "NODE", vmin=single_node, vmax=single_node)
-#     mapdl.cm("load_node", "NODE")
-
-# notch_file = download_example_data(
-#     filename="3d_notch.db", directory="pymapdl/notch"
-# )
-notch_file = Path.cwd().joinpath(r"src\ansys\mapdl\core\examples\3d_notch.db")
-mapdl.resume(notch_file, mute=True)
-
-for idx, force_load in enumerate([250, 500, 750, 1000]):
+def solve_design_point(mapdl: Mapdl, force_load: float):
+    """Solve the MAPDL model."""
     mapdl.run("/SOLU")
     mapdl.cmsel("S", "load_node", "NODE")
     mapdl.fdele("ALL", "FX")
@@ -242,30 +195,50 @@ for idx, force_load in enumerate([250, 500, 750, 1000]):
     mapdl.antype("STATIC")
     mapdl.solve()
     mapdl.finish(mute=True)
-
-    # Define folders for output
-    rom_folder = Path(mapdl.directory).joinpath("Static_ROM")
-
     rst_path = mapdl.result_file
-    model = dpf.Model(rst_path)
-    results = model.results
-    scoping = get_scoping(model)
-    parameters = {"force[N]": force_load}
+    return rst_path
 
-    new_metadata = idx == 0
-    for name in ["displacement", "stress"]:
-        output_folder = rom_folder.joinpath(name)
-        # Ensure displacement scoping and stress scoping are the same by only taking nodes connected to
-        # elements.
-        export_static_ROM_data(
-            model,
-            scoping,
-            name,
-            output_folder,
-            parameters=parameters,
-            snap_idx=idx,
-            new_metadata=new_metadata,
-        )
 
-    print(rom_folder)
-mapdl.exit()
+def run_doe():
+    # First, start MAPDL as a service and disable all but error messages.
+    mapdl = launch_mapdl(loglevel="ERROR")
+
+    # Download the example database: notch_file is the path to the downloaded file.
+    notch_file = download_example_data(
+        filename="3d_notch.db", directory="pymapdl/static_ROM_data_generation"
+    )
+
+    mapdl.resume(notch_file, mute=True)
+
+    # Define folders for output.
+    rom_folder = Path(mapdl.directory).joinpath("Static_ROM")
+    for idx, force_load in enumerate([250]):
+        # Solve the MAPDL model
+        rst_path = solve_design_point(mapdl, force_load)
+
+        # Load the results to DPF and create scoping.
+        model = dpf.Model(rst_path)
+        scoping = get_scoping(model)
+
+        # Only create points.bin and settings.json on first design point.
+        new_metadata = idx == 0
+
+        # Set current parameters and export displacement and stress data.
+        parameters = {"force[N]": force_load}
+        for name in ["displacement", "stress"]:
+            output_folder = rom_folder.joinpath(name)
+            export_static_ROM_data(
+                model,
+                scoping,
+                name,
+                output_folder,
+                parameters=parameters,
+                snap_idx=idx,
+                new_metadata=new_metadata,
+            )
+        print(rom_folder)
+    mapdl.exit()
+
+
+if __name__ == "__main__":
+    run_doe()
