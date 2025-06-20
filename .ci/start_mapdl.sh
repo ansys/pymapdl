ebbbb2fa031adaadf177e4a73fe3971f8172ae29
German <28149841+germa89@users.noreply.github.com>
2025-06-20 13:07:56 +0200

    fix: enhance start_mapdl.sh script with detailed usage instructions and improve docker command construction

--- .ci/start_mapdl.sh
+++ .ci/start_mapdl.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
+# This script is used to start a MAPDL instance in a Docker container.
+# 
+# Usage:
+# ------
+# This script is intended to be run in a CI/CD environment where the necessary environment variables are set.
+#
+# Required environment variables:
+# -------------------------------
+#
+# - DISTRIBUTED_MODE: The mode of operation for MAPDL (e.g., "smp", "dmp").
+# - DPF_PORT: The port for the DPF service (e.g., 50055).
+# - INSTANCE_NAME: The name of the MAPDL instance (e.g., "MAPDL_0").
+# - LICENSE_SERVER: The address of the license server (e.g., "123.123.123.123").
+# - MAPDL_PACKAGE: The Docker image package name (e.g., ansys/mapdl).
+# - MAPDL_VERSION: The version of the MAPDL image to use (e.g., "v25.2-ubuntu-cicd").
+# - PYMAPDL_DB_PORT: The port for the PyMAPDL database service (e.g., 50056).
+# - PYMAPDL_PORT: The port for the PyMAPDL service (e.g., 50052).
+#
+# Example:
+# --------
+#
+#   export DISTRIBUTED_MODE="smp"
+#   export DPF_PORT=50055
+#   export INSTANCE_NAME=MAPDL_0
+#   export LICENSE_SERVER="123.123.123.123"
+#   export MAPDL_PACKAGE=ghcr.io/ansys/mapdl
+#   export MAPDL_VERSION=v25.2-ubuntu-cicd
+#   export PYMAPDL_DB_PORT=50056
+#   export PYMAPDL_PORT=50052
+#   ./start_mapdl.sh
+#   
 echo "MAPDL Instance name: $INSTANCE_NAME"
 echo "MAPDL_VERSION: $MAPDL_VERSION"
 
 export MAPDL_IMAGE="$MAPDL_PACKAGE:$MAPDL_VERSION"
 echo "MAPDL_IMAGE:   $MAPDL_IMAGE"
-docker pull "$MAPDL_IMAGE"
+# docker pull "$MAPDL_IMAGE"
 
 export MAJOR=$(echo "$MAPDL_VERSION" | head -c 3 | tail -c 2)
 export MINOR=$(echo "$MAPDL_VERSION" | head -c 5 | tail -c 1)
@@ -31,7 +62,7 @@ fi;
 
 if [[ $MAPDL_VERSION == *"cicd"* ]] ; then
     echo "It is a CICD version, binding DPF port too"
-    export DPF_ARG="-p $DPF_PORT:50055"
+    export DPF_ARG="-p ${DPF_PORT}:50055"
     export DB_INT_PORT=50056
 
     echo "DPF_ARG: $DPF_ARG"
@@ -44,27 +75,38 @@ fi;
 echo "EXEC_PATH: $EXEC_PATH"
 echo "P_SCHEMA: $P_SCHEMA"
 
-docker run \
-    --entrypoint "/bin/bash" \
-    --name "$INSTANCE_NAME" \
-    --restart always \
-    --health-cmd="ps aux | grep \"[/]ansys_inc/.*ansys\.e.*grpc\" -q && echo 0 || echo 1" \
-    --health-interval=0.5s \
-    --health-retries=4 \
-    --health-timeout=0.5s \
-    --health-start-period=10s \
-    -e ANSYSLMD_LICENSE_FILE=1055@"${LICENSE_SERVER}" \
-    -e ANSYS_LOCK="OFF" \
-    -p "$PYMAPDL_PORT":50052 \
-    -p "$PYMAPDL_DB_PORT":"${DB_INT_PORT}" \
-    "${DPF_ARG}" \
-    --shm-size=2gb \
-    -e I_MPI_SHM_LMT=shm \
-    -e P_SCHEMA="$P_SCHEMA" \
-    -w /jobs \
-    -u=0:0 \
-    --memory=6656MB \
-    --memory-swap=16896MB \
-    "$MAPDL_IMAGE" "$EXEC_PATH" -grpc -dir /jobs -"$DISTRIBUTED_MODE" -np 2 -db -5000 -m -5000 - > log.txt &
+# Building docker command
+CMD=$(cat <<-_EOT_
+run \
+  --entrypoint "/bin/bash" \
+  --name "$INSTANCE_NAME" \
+  --restart always \
+  --health-cmd="ps aux | grep \"[/]ansys_inc/.*ansys\.e.*grpc\" -q && echo 0 || echo 1" \
+  --health-interval=0.5s \
+  --health-retries=4 \
+  --health-timeout=0.5s \
+  --health-start-period=10s \
+  -e ANSYSLMD_LICENSE_FILE=1055@"${LICENSE_SERVER}" \
+  -e ANSYS_LOCK="OFF" \
+  -p ${PYMAPDL_PORT}:50052 \
+  -p ${PYMAPDL_DB_PORT}:${DB_INT_PORT} \
+  ${DPF_ARG} \
+  --shm-size=2gb \
+  -e I_MPI_SHM_LMT=shm \
+  -e P_SCHEMA="$P_SCHEMA" \
+  -w /jobs \
+  -u=0:0 \
+  --memory=6656MB \
+  --memory-swap=16896MB \
+  "$MAPDL_IMAGE" "$EXEC_PATH" -grpc -dir /jobs -"$DISTRIBUTED_MODE" -np 2 -db -5000 -m -5000 -
+_EOT_
+)
 
+echo "Running docker command: "
+echo "docker ${CMD}"
+docker ${CMD} > log.txt &
 grep -q 'Server listening on' <(timeout 60 tail -f log.txt)
+
+
+echo "Content of log.txt:"
+cat log.txt
