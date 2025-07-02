86c70d8e4417799b798702cab2a602d59e4e8e66
Camille Latapie <78221213+clatapie@users.noreply.github.com>
2025-07-02 18:19:46 +0200

    Update src/ansys/mapdl/core/mapdl_grpc.py
    Co-authored-by: German <28149841+germa89@users.noreply.github.com>

--- src/ansys/mapdl/core/mapdl_grpc.py
+++ src/ansys/mapdl/core/mapdl_grpc.py
@@ -964,7 +964,7 @@ class MapdlGrpc(MapdlBase):
     def _set_no_abort(self):
         """Do not abort MAPDL."""
         self._log.debug("Setting to no abort")
-        self.run("/NERR,,,-1", mute=True)
+        self.nerr(abort=-1, mute=True)
 
     def _run_at_connect(self):
         """Run house-keeping commands when initially connecting to MAPDL."""
