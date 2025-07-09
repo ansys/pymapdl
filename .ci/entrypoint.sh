1965eaba2046a87919ce57761ba785aeed5177e4
German <28149841+germa89@users.noreply.github.com>
2025-07-09 12:22:28 +0200

    fix: add quotes around DATAPROCESSING_DEBUG variable for consistency

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -16,7 +16,7 @@ echo "RUN_DPF_SERVER: $RUN_DPF_SERVER"
 
 if [ "$RUN_DPF_SERVER" = true ]; then
     echo "Setting DPF Debug directory..."
-    export DATAPROCESSING_DEBUG=${HOME}/dpf_logs/
+    export DATAPROCESSING_DEBUG="${HOME}/dpf_logs/"
     mkdir -p "$DATAPROCESSING_DEBUG"
 
     echo "Starting DPF server..."
