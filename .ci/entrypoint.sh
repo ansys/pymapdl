b697079ffb56895480f39ec989ea11060036da3e
German <28149841+germa89@users.noreply.github.com>
2025-07-09 17:08:15 +0200

    fix: comment out DPF Debug directory setup in entrypoint.sh

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -16,8 +16,8 @@ echo "RUN_DPF_SERVER: $RUN_DPF_SERVER"
 
 if [ "$RUN_DPF_SERVER" == "true" ]; then
     echo "Setting DPF Debug directory..."
-    export DATAPROCESSING_DEBUG=${HOME}/dpf_logs/
-    mkdir -p "$DATAPROCESSING_DEBUG"
+    # export DATAPROCESSING_DEBUG=${HOME}/dpf_logs/
+    # mkdir -p "$DATAPROCESSING_DEBUG"
 
     echo "Starting DPF server..."
     /ansys_inc/v${VERSION}/aisol/bin/linx64/Ans.Dpf.Grpc.sh --port ${DPF_PORT_INTERNAL} > log_dpf.log &
