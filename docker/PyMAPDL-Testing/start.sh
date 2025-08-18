4db251a03a55104393c56e5a2756e2892a0c8109
German <28149841+germa89@users.noreply.github.com>
2025-08-18 18:17:14 +0200

    refactor: Update Dockerfile and docker-compose.yml for improved configurations and added mylocal.ip

--- docker/PyMAPDL-Testing/start.sh
+++ docker/PyMAPDL-Testing/start.sh
@@ -8,13 +8,14 @@ echo ""
 # Checkout to the specified branch if PYMAPDL_BRANCH is set
 if [ -n "${PYMAPDL_BRANCH}" ]; then
   echo "PYMAPDL_BRANCH is set to '${PYMAPDL_BRANCH}'. Checking out this branch..."
-  git fetch origin "${PYMAPDL_BRANCH}"
+  git fetch origin "${PYMAPDL_BRANCH}" > /dev/null 2>&1
   git checkout "${PYMAPDL_BRANCH}"
 fi
 
-if [[ "${USE_LOCAL_REPO}" == "yes" ]]; then
+if [[ "${USE_LOCAL_REPO}" == "true" ]]; then
     echo "Using local PyMAPDL repository for testing."
-    python3 -m pip install '.[tests]'
+    # Uncomment if you expect changes in the dependencies.
+    # python3 -m pip install '.[tests]'
 
 else
     echo "Using cloned PyMAPDL repository for testing."
@@ -22,4 +23,5 @@ fi
 
 echo "Using pytest arguments: ${PYTEST_ARGUMENTS}"
 
-xvfb-run pytest "${PYTEST_ARGUMENTS}"
+# shellcheck disable=SC2086
+xvfb-run pytest ${PYTEST_ARGUMENTS}
