9990f1317e20b9607a6189f611638aaae24faa4c
German <28149841+germa89@users.noreply.github.com>
2025-08-19 18:23:14 +0200

    fix: correct condition for skipping InProcess tests based on environment variable

--- tests/test_inprocess.py
+++ tests/test_inprocess.py
@@ -107,7 +107,7 @@ def mapdl_inprocess(mapdl: "Mapdl", tmp_path) -> MapdlInProcessRunner:
     if not ON_LOCAL:
         pytest.skip("InProcess interface can only be tested on local machines")
 
-    if not os.getenv("TEST_INPROCESS"):
+    if not os.getenv("TEST_INPROCESS", "").lower() != "true":
         pytest.skip(
             "Skipping InProcess tests, set TEST_INPROCESS environment variable to run them"
         )
