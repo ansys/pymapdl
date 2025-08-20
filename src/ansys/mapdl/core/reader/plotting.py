b54f4d939944edbbf6d0553edad99be347b3b789
German <28149841+germa89@users.noreply.github.com>
2025-08-20 14:19:16 +0200

    feat: add plot_element_stress method for visualizing element stress results

--- src/ansys/mapdl/core/reader/plotting.py
+++ src/ansys/mapdl/core/reader/plotting.py
@@ -27,6 +27,7 @@ from ansys.mapdl.core.reader.core import NotImplementedInDPFBackend
 from ansys.mapdl.core.reader.data import DPFResultData
 from ansys.mapdl.core.reader.types import (
     ComponentsDirections,
+    Elements,
     Kwargs,
     MAPDLComponents,
     Nodes,
@@ -436,6 +437,132 @@ class DPFResultPlotting(DPFResultData):
         if show_displacement:
             # Get displacement field for deformation
             displacement_field: dpf.Field = self.nodal_displacement(rnum, nodes=nodes, return_field=True)  # type: ignore
+            deform_by = displacement_field
+
+        return self.mesh.plot(
+            field, scale_factor=displacement_factor, deform_by=deform_by, **kwargs
+        )
+
+    def plot_element_stress(
+        self,
+        rnum: Rnum,
+        comp: ComponentsDirections | int | None = None,
+        show_displacement: bool = False,
+        displacement_factor: int = 1,
+        node_components: MAPDLComponents | None = None,
+        element_components: MAPDLComponents | None = None,
+        sel_type_all: bool | None = None,
+        treat_nan_as_zero: bool | None = None,
+        nodes: Nodes = None,
+        principal: bool = False,
+        in_element_coord_sys: bool = False,
+        elements: Elements = None,
+        **kwargs: Kwargs,
+    ) -> list[float]:
+        """Plots the element stresses in the solution.
+
+        Parameters
+        ----------
+        rnum : int or list
+            Cumulative result number with zero based indexing, or a
+            list containing (step, substep) of the requested result.
+
+        comp : str, optional
+            Stress component to display.  Available options:
+            - ``"X"``
+            - ``"Y"``
+            - ``"Z"``
+            - ``"XY"``
+            - ``"YZ"``
+            - ``"XZ"``
+
+        show_displacement
+            If True, displays the displacement along with the stress
+            plot. Default is False.
+
+        displacement_factor
+            Factor by which to scale the displacement plot. Default is 1.
+
+        node_components : list, optional
+            Accepts either a string or a list strings of node
+            components to plot.  For example:
+            ``['MY_COMPONENT', 'MY_OTHER_COMPONENT]``
+
+        element_components : list, optional
+            Accepts either a string or a list strings of element
+            components to plot.  For example:
+            ``['MY_COMPONENT', 'MY_OTHER_COMPONENT]``
+
+        sel_type_all : bool, optional
+            If node_components is specified, plots those elements
+            containing all nodes of the component.  Default ``True``.
+
+        treat_nan_as_zero : bool, optional
+            Treat NAN values (i.e. stresses at midside nodes) as zero
+            when plotting.
+
+        nodes
+            Select a limited subset of nodes.  Can be a nodal
+            component or array of node numbers.  For example
+
+            * ``"MY_COMPONENT"``
+            * ``['MY_COMPONENT', 'MY_OTHER_COMPONENT]``
+            * ``np.arange(1000, 2001)``
+
+        principal : bool, optional
+            Returns principal stresses instead of component stresses.
+            Default False.
+
+        in_element_coord_sys : bool, optional
+            When ``True``, returns results in the element coordinate
+            system.  Default ``False``.
+
+        elements : str, sequence of int or str, optional
+            Select a limited subset of elements.  Can be a element
+            component or array of element numbers.  For example
+
+            * ``"MY_COMPONENT"``
+            * ``['MY_COMPONENT', 'MY_OTHER_COMPONENT]``
+            * ``np.arange(1000, 2001)``
+
+        **kwargs
+            Additional keyword arguments.  See ``help(pyvista.plot)``
+
+        Returns
+        -------
+        cpos : list
+            3 x 3 vtk camera position.
+
+        Examples
+        --------
+        Plot the X component element stress while showing displacement.
+
+        >>> mapdl.result.plot_element_stress(0, comp='x', show_displacement=True)
+        """
+        if element_components:
+            raise NotImplementedInDPFBackend(argument="element_components")
+
+        if node_components:
+            raise NotImplementedInDPFBackend(argument="node_components")
+
+        if sel_type_all:
+            raise NotImplementedInDPFBackend(argument="sel_type_all")
+
+        if treat_nan_as_zero:
+            raise NotImplementedInDPFBackend(argument="treat_nan_as_zero")
+
+        if nodes:
+            raise NotImplementedInDPFBackend(argument="nodes")
+
+        field: dpf.Field = self.element_stress(rnum, principal=principal, in_element_coord_sys=in_element_coord_sys, elements=elements, return_field=True, **kwargs)  # type: ignore
+        field = self._component_selector(fc=field, component=comp)
+
+        # If the user requested to show displacement, we will
+        # deform the mesh by the displacement field.
+        deform_by: dpf.Field | None = None
+        if show_displacement:
+            # Get displacement field for deformation
+            displacement_field: dpf.Field = self.nodal_displacement(rnum, return_field=True)  # type: ignore
             deform_by = displacement_field
 
         return self.mesh.plot(
