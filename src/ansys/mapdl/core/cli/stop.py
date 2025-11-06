c16d61f01fb706ae8088c2b0b7b5affcfc49858b
Sébastien Morais <146729917+SMoraisAnsys@users.noreply.github.com>
2025-11-06 16:00:16 +0100

    fix: add missing import and take into account review

--- src/ansys/mapdl/core/cli/stop.py
+++ src/ansys/mapdl/core/cli/stop.py
@@ -187,6 +187,7 @@ def _can_access_process(proc):
         True if we can safely access and kill the process
     """
     import getpass
+    import platform
 
     import psutil
 
@@ -194,7 +195,7 @@ def _can_access_process(proc):
         # Check if we can access basic process info and if it belongs to current user
         current_user = getpass.getuser()
         process_user = proc.username()
-        if os.name == "nt" and "\\" in process_user:
+        if platform.system() == "Windows" and "\\" in process_user:
             return current_user == process_user.split("\\")[-1]
         return process_user == current_user
     except (psutil.AccessDenied, psutil.NoSuchProcess):
