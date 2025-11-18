ef41c2daba296c42b15704d166bc36685131ded2
clatapie <78221213+clatapie@users.noreply.github.com>
2025-11-18 14:51:11 +0100

    fix: pre-commit

--- src/ansys/mapdl/core/_commands/prep7/explicit_dynamics.py
+++ src/ansys/mapdl/core/_commands/prep7/explicit_dynamics.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2016 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from typing import Optional
 
 from ansys.mapdl.core.mapdl_types import MapdlFloat, MapdlInt
@@ -1386,7 +1408,9 @@ class ExplicitDynamics:
         Distributed ANSYS Restriction: This command is not supported in
         Distributed ANSYS.
         """
-        command = f"EDNROT,{option},{cid},{cname},{dof1},{dof2},{dof3},{dof4},{dof5},{dof6}"
+        command = (
+            f"EDNROT,{option},{cid},{cname},{dof1},{dof2},{dof3},{dof4},{dof5},{dof6}"
+        )
         return self.run(command, **kwargs)
 
     def edpart(self, option="", partid="", cname="", **kwargs):
@@ -1673,9 +1697,7 @@ class ExplicitDynamics:
         Distributed ANSYS Restriction: This command is not supported in
         Distributed ANSYS.
         """
-        command = (
-            f"EDWELD,{option},{nweld},{n1},{n2},{sn},{ss},{expn},{exps},{epsf},{tfail},{nsw},{cid}"
-        )
+        command = f"EDWELD,{option},{nweld},{n1},{n2},{sn},{ss},{expn},{exps},{epsf},{tfail},{nsw},{cid}"
         return self.run(command, **kwargs)
 
     def edadapt(self, part="", key="", **kwargs):
