a10a252cc0d3b1c833707fc73e0d36596be6489c
germa89 <28149841+germa89@users.noreply.github.com>
2025-11-25 17:24:59 +0100

    refactor(tests): convert function-based tests to class-based structure for better organization

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -26,126 +26,135 @@ import pytest
 from ansys.mapdl.core.errors import MapdlRuntimeError
 
 
-def test_time_step_size(mapdl, cleared):
-    assert isinstance(mapdl.solution.time_step_size, float)
+class TestSolution:
+    @pytest.fixture(scope="class")
+    def setup(self, mapdl):
+        from conftest import clear
 
+        clear(mapdl)
+        mapdl.prep7()
 
-def test_n_cmls(mapdl, cleared):
-    assert isinstance(mapdl.solution.n_cmls, float)
+    @staticmethod
+    def test_time_step_size(setup, mapdl):
+        assert isinstance(mapdl.solution.time_step_size, float)
 
+    @staticmethod
+    def test_n_cmls(setup, mapdl):
+        assert isinstance(mapdl.solution.n_cmls, float)
 
-def test_n_cmss(mapdl, cleared):
-    assert isinstance(mapdl.solution.n_cmss, float)
+    @staticmethod
+    def test_n_cmss(setup, mapdl):
+        assert isinstance(mapdl.solution.n_cmss, float)
 
+    @staticmethod
+    def test_n_eqit(setup, mapdl):
+        assert isinstance(mapdl.solution.n_eqit, float)
 
-def test_n_eqit(mapdl, cleared):
-    assert isinstance(mapdl.solution.n_eqit, float)
+    @staticmethod
+    def test_n_cmit(setup, mapdl):
+        assert isinstance(mapdl.solution.n_cmit, float)
 
+    @staticmethod
+    def test_converged(setup, mapdl):
+        assert isinstance(mapdl.solution.converged, bool)
 
-def test_n_cmit(mapdl, cleared):
-    assert isinstance(mapdl.solution.n_cmit, float)
+    @staticmethod
+    def test_mx_dof(setup, mapdl):
+        assert isinstance(mapdl.solution.mx_dof, float)
 
+    @staticmethod
+    def test_res_frq(setup, mapdl):
+        assert isinstance(mapdl.solution.res_frq, float)
 
-def test_converged(mapdl, cleared):
-    assert isinstance(mapdl.solution.converged, bool)
+    @staticmethod
+    def test_res_eig(setup, mapdl):
+        assert isinstance(mapdl.solution.res_eig, float)
 
+    @staticmethod
+    def test_decent_parm(setup, mapdl):
+        assert isinstance(mapdl.solution.decent_parm, float)
 
-def test_mx_dof(mapdl, cleared):
-    assert isinstance(mapdl.solution.mx_dof, float)
+    @staticmethod
+    def test_force_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.force_cnv, float)
 
+    @staticmethod
+    def test_moment_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.moment_cnv, float)
 
-def test_res_frq(mapdl, cleared):
-    assert isinstance(mapdl.solution.res_frq, float)
+    @staticmethod
+    def test_heat_flow_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.heat_flow_cnv, float)
 
+    @staticmethod
+    def test_magnetic_flux_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.magnetic_flux_cnv, float)
 
-def test_res_eig(mapdl, cleared):
-    assert isinstance(mapdl.solution.res_eig, float)
+    @staticmethod
+    def test_current_segment_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.current_segment_cnv, float)
 
+    @staticmethod
+    def test_current_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.current_cnv, float)
 
-def test_decent_parm(mapdl, cleared):
-    assert isinstance(mapdl.solution.decent_parm, float)
+    @staticmethod
+    def test_fluid_flow_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.fluid_flow_cnv, float)
 
+    @staticmethod
+    def test_displacement_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.displacement_cnv, float)
 
-def test_force_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.force_cnv, float)
+    @staticmethod
+    def test_rotation_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.rotation_cnv, float)
 
+    @staticmethod
+    def test_temperature_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.temperature_cnv, float)
 
-def test_moment_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.moment_cnv, float)
+    @staticmethod
+    def test_vector_cnv(setup, mapdl):
+        assert isinstance(mapdl.solution.vector_cnv, float)
 
+    @staticmethod
+    def test_smcv(setup, mapdl):
+        assert isinstance(mapdl.solution.smcv, float)
 
-def test_heat_flow_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.heat_flow_cnv, float)
+    @staticmethod
+    def test_voltage_conv(setup, mapdl):
+        assert isinstance(mapdl.solution.voltage_conv, float)
 
+    @staticmethod
+    def test_pressure_conv(setup, mapdl):
+        assert isinstance(mapdl.solution.pressure_conv, float)
 
-def test_magnetic_flux_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.magnetic_flux_cnv, float)
+    @staticmethod
+    def test_velocity_conv(setup, mapdl):
+        assert isinstance(mapdl.solution.velocity_conv, float)
 
+    @staticmethod
+    def test_mx_creep_rat(setup, mapdl):
+        assert isinstance(mapdl.solution.mx_creep_rat, float)
 
-def test_current_segment_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.current_segment_cnv, float)
+    @staticmethod
+    def test_mx_plastic_inc(setup, mapdl):
+        assert isinstance(mapdl.solution.mx_plastic_inc, float)
 
+    @staticmethod
+    def test_n_cg_iter(setup, mapdl):
+        assert isinstance(mapdl.solution.n_cg_iter, float)
 
-def test_current_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.current_cnv, float)
+    @staticmethod
+    def test_solution_call(setup, mapdl):
+        mapdl.finish()
+        output = mapdl.solution()
+        assert "MAPDL SOLUTION ROUTINE" in output or "ANSYS SOLUTION ROUTINE" in output
 
-
-def test_fluid_flow_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.fluid_flow_cnv, float)
-
-
-def test_displacement_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.displacement_cnv, float)
-
-
-def test_rotation_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.rotation_cnv, float)
-
-
-def test_temperature_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.temperature_cnv, float)
-
-
-def test_vector_cnv(mapdl, cleared):
-    assert isinstance(mapdl.solution.vector_cnv, float)
-
-
-def test_smcv(mapdl, cleared):
-    assert isinstance(mapdl.solution.smcv, float)
-
-
-def test_voltage_conv(mapdl, cleared):
-    assert isinstance(mapdl.solution.voltage_conv, float)
-
-
-def test_pressure_conv(mapdl, cleared):
-    assert isinstance(mapdl.solution.pressure_conv, float)
-
-
-def test_velocity_conv(mapdl, cleared):
-    assert isinstance(mapdl.solution.velocity_conv, float)
-
-
-def test_mx_creep_rat(mapdl, cleared):
-    assert isinstance(mapdl.solution.mx_creep_rat, float)
-
-
-def test_mx_plastic_inc(mapdl, cleared):
-    assert isinstance(mapdl.solution.mx_plastic_inc, float)
-
-
-def test_n_cg_iter(mapdl, cleared):
-    assert isinstance(mapdl.solution.n_cg_iter, float)
-
-
-def test_solution_call(mapdl, cleared):
-    mapdl.finish()
-    output = mapdl.solution()
-    assert "MAPDL SOLUTION ROUTINE" in output or "ANSYS SOLUTION ROUTINE" in output
-
-
-def test_exited(mapdl, cleared):
-    mapdl._exited = True
-    with pytest.raises(MapdlRuntimeError):
-        parm = mapdl.solution.time_step_size
-    mapdl._exited = False
+    @staticmethod
+    def test_exited(setup, mapdl):
+        mapdl._exited = True
+        with pytest.raises(MapdlRuntimeError):
+            parm = mapdl.solution.time_step_size
+        mapdl._exited = False
