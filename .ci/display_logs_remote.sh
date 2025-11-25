29d0adec1fc6f46923211cb33339a874b37c6a08
germa89 <28149841+germa89@users.noreply.github.com>
2025-11-25 18:20:53 +0100

    fix: enhance log display in display_logs_remote.sh for better debugging

--- .ci/display_logs_remote.sh
+++ .ci/display_logs_remote.sh
@@ -8,6 +8,13 @@ echo "::group:: Display files structure" && docker exec "$MAPDL_INSTANCE" /bin/b
 
 echo "::group:: Display docker run log" && cat log.txt && echo "::endgroup::"
 
+# Displaying Docker stdout log
+FILE_PAT="./$LOG_NAMES/mapdl_launch_*.log"
+if compgen -G "$FILE_PAT" > /dev/null ;then for f in $FILE_PAT; do echo "::group:: MAPDL docker launch $f" && cat "$f" && echo "::endgroup::" ; done; fi || echo "Failed to display the 'log' files."
+
+FILE_PAT="./$LOG_NAMES/MAPDL_*.log"
+if compgen -G "$FILE_PAT" > /dev/null ;then for f in $FILE_PAT; do echo "::group:: MAPDL docker output $f" && cat "$f" && echo "::endgroup::" ; done; fi || echo "Failed to display the 'log' files."
+
 # Displaying MAPDL files
 FILE_PAT="./$LOG_NAMES/*.err"
 if compgen -G "$FILE_PAT" > /dev/null ;then for f in $FILE_PAT; do echo "::group:: Error file $f" && cat "$f" && echo "::endgroup::" ; done; fi || echo "Failed to display the 'err' files."
