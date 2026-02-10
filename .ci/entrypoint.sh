f55ce3bc16424b409f93acce35eebf736017a933
germa89 <28149841+germa89@users.noreply.github.com>
2026-02-10 18:17:27 +0100

    ci: fix command syntax in entrypoint script for MAPDL execution

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -10,39 +10,42 @@ fi
 
 RUN_DPF_SERVER=${RUN_DPF_SERVER:-false}
 
-if [ -n "${ANSYS_DPF_ACCEPT_LA}" ]; then
-    if [ "${ANSYS_DPF_ACCEPT_LA}" == "Y" ]; then
-        RUN_DPF_SERVER=true
-    fi
+if [ "${ANSYS_DPF_ACCEPT_LA}" = "Y" ]; then
+    RUN_DPF_SERVER=true
 fi
 
 
 echo "RUN_DPF_SERVER: $RUN_DPF_SERVER"
 
-if [ "$RUN_DPF_SERVER" == "true" ]; then
+if [ "$RUN_DPF_SERVER" = "true" ]; then
     echo "Starting DPF server..."
     export DPF_DEFAULT_GRPC_MODE="insecure"
-    "/ansys_inc/v${VERSION}/aisol/bin/linx64/Ans.Dpf.Grpc.sh" --port "${DPF_PORT_INTERNAL}" --address '0.0.0.0' > log_dpf.log &
-    echo "DPF server started."
+    if "/ansys_inc/v${VERSION}/aisol/bin/linx64/Ans.Dpf.Grpc.sh" --port "${DPF_PORT_INTERNAL}" --address '0.0.0.0' > log_dpf.log 2>&1 &
+    then
+        sleep 2  # Give DPF server time to start
+        echo "DPF server started."
+    else
+        echo "Failed to start DPF server"
+        exit 1
+    fi
 fi
 
-if [[ $MAPDL_IMAGE == *"cicd"* ]] ; then
+if [[ $MAPDL_IMAGE == *"cicd"* ]]; then
     echo "Using OpenMPI for CICD version"
     export MPI="-mpi openmpi"
-
 else
     echo "Using default MPI version"
     export MPI=""
-fi;
+fi
 
 echo "Starting MAPDL..."
 echo "Using executable path: ${EXEC_PATH}"
 
 CMD=$(cat <<-_EOT_
-    $EXEC_PATH -grpc -dir /jobs -"${DISTRIBUTED_MODE}" "${MPI}" -transport insecure -allowremote true -np 2 -db -6000 -m -6000
+${EXEC_PATH} -grpc -dir /jobs -${DISTRIBUTED_MODE} ${MPI} -transport insecure -allowremote true -np 2 -db -6000 -m -6000
 _EOT_
 )
 
-echo "Running command: "
+echo "Running command:"
 echo "${CMD}"
 exec ${CMD}
