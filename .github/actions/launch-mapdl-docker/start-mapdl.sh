5a1d367ec8c3801064ed386eedff9c12876aaf98
germa89 <28149841+germa89@users.noreply.github.com>
2026-02-26 14:00:50 +0100

    fix: update Docker run command to execute entrypoint script instead of listing directory

--- .github/actions/launch-mapdl-docker/start-mapdl.sh
+++ .github/actions/launch-mapdl-docker/start-mapdl.sh
@@ -165,10 +165,7 @@ docker run \
   -e OMPI_ALLOW_RUN_AS_ROOT=1 \
   -e OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1 \
   "${MAPDL_IMAGE}" \
-  ls -al
-
-
-#  "/ansys_inc/v252/ansys/bin/mapdl -grpc -dir /jobs -dmp -transport insecure -allowremote true -np 2 -db 6000 -m 6000 -mpi openmpi"
+  -c "/entrypoint.sh"
 
 echo ""
 echo "Container started. Waiting for MAPDL to initialize..."
